6fe94757a9d61e8a449be750fe75628bce03062d
NathanKell <nathankell@gmail.com>
2015-03-01 21:55:26 -0500

    Fix locking, add Avionics module
    Apply that module to a few parts, Avionics.cfg is MASSIVE TODO / FIXME.
    Now you need an avionics module that will allow sufficient mass to be
    controlled, in order to control your rocket of given mass.

---
+++ Source/ModuleAvionics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KSP;
+using UnityEngine;
+
+namespace RP0
+{
+    class ModuleAvionics : PartModule
+    {
+        [KSPField]
+        public float massLimit = float.MaxValue; // default is unlimited
+
+        public override string GetInfo()
+        {
+            string retStr = "This part allows control of vessels of ";
+            if (massLimit < float.MaxValue)
+                retStr += "up to " + massLimit.ToString("N1") + " tons.";
+            else
+                retStr += "any mass.";
+            return retStr;
+        }
+    }
+}
--- Source/ModuleScienceCore.cs
+++ Source/ModuleScienceCore.cs
@@ -7,55 +7,6 @@ using UnityEngine;
 
 namespace RP0
 {
-    [KSPAddon(KSPAddon.Startup.Flight, false)]
-    class ScienceCoreControlLocker
-    {
-        public int vParts = -1;
-        public Vessel vessel = null;
-        bool wasLocked = false;
-        const ControlTypes lockmask = ControlTypes.YAW | ControlTypes.PITCH | ControlTypes.ROLL | ControlTypes.SAS | ControlTypes.RCS;
-        const string lockID = "ModuleScienceCore";
-        
-        public bool ShouldLock()
-        {
-            if (vessel != FlightGlobals.ActiveVessel)
-            {
-                vParts = -1;
-                vessel = FlightGlobals.ActiveVessel;
-            }
-            if ((object)vessel == null)
-                return false;
-
-            if (vessel.Parts.Count != vParts)
-            {
-                vParts = vessel.Parts.Count;
-                for (int i = 0; i < vessel.Parts.Count; i++)
-                {
-                    if (vessel.Parts[i].Modules.Contains("ModuleCommand") && !vessel.Parts[i].Modules.Contains("ModuleScienceCore"))
-                        return false;
-                }
-                return true;
-            }
-            return wasLocked;
-        }
-
-        public void FixedUpdate()
-        {
-            bool doLock = ShouldLock();
-            if (doLock != wasLocked)
-            {
-                if (wasLocked)
-                    InputLockManager.RemoveControlLock(lockID);
-                else
-                    InputLockManager.SetControlLock(lockmask, lockID);
-                wasLocked = doLock;
-            }
-        }
-        public void OnDestroy()
-        {
-            InputLockManager.RemoveControlLock(lockID);
-        }
-    }
     class ModuleScienceCore : PartModule
     {
         public override string GetInfo()

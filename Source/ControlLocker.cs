f2fa900669d35bab697c813c6ca23d34d9210c8f
NathanKell <nathankell@gmail.com>
2015-03-31 20:34:28 -0400

    Refactor ControlLocker, add editor GUI
    * New class with static method that locks based on total parts
    * Ignore launch clamp mass
    * If in editor, ignore launch clamp control ability
    * If avionics part has modulecommand with minCrew > 0, only add the
    avionics if the vessel has that or more crew
    * Show a window in the editor (can be toggled by MOD + I). FIXME: Add a
    toolbar button.
    * Update lock status in editor once every 0.25 seconds (not per frame).
    * Editor window can also be set to show only if insufficient avionics
    * FIXME: Save/load configuration, window position.

--- Source/ControlLocker.cs
+++ Source/ControlLocker.cs
@@ -8,16 +8,207 @@ using UnityEngine;
 
 namespace RP0
 {
+    class ControlLockerUtils
+    {
+        public static bool ShouldLock(List<Part> parts, bool countClamps, out float maxMass, out float vesselMass)
+        {
+            int crewCount = -1;
+            maxMass = vesselMass = 0f;
+            for (int i = 0; i < parts.Count; i++)
+            {
+                // add up mass
+                float partMass = parts[i].mass + parts[i].GetResourceMass();
+
+                // get modules
+                bool cmd = false, science = false, avionics = false, clamp = false;
+                float partAvionicsMass = 0f;
+                ModuleCommand mC = null;
+                for (int j = 0; j < parts[i].Modules.Count; j++)
+                {
+                    if (parts[i].Modules[j] is ModuleCommand)
+                    {
+                        cmd = true;
+                        mC = parts[i].Modules[j] as ModuleCommand;
+                    }
+                    science |= parts[i].Modules[j] is ModuleScienceCore;
+                    if ((parts[i].Modules[j]) is ModuleAvionics)
+                    {
+                        avionics = true;
+                        partAvionicsMass += (parts[i].Modules[j] as ModuleAvionics).CurrentMassLimit;
+                    }
+                    if(parts[i].Modules[j] is LaunchClamp)
+                    {
+                        clamp = true;
+                        partMass = 0f;
+                    }
+                }
+                vesselMass += partMass; // done after the clamp check
+
+                // switch based on modules
+
+                // Do we have an unencumbered command module?
+                // if we count clamps, they can give control. If we don't, this works only if the part isn't a clamp.
+                if ((countClamps || !clamp) && cmd && !science && !avionics)
+                    return false;
+
+                if (cmd && avionics) // check if need crew
+                {
+                    bool flag = true;
+                    if (mC.minimumCrew > 0) // if we need crew
+                    {
+                        if (crewCount < 0) // see if we cached crew
+                        {
+                            if (HighLogic.LoadedSceneIsFlight) // get from vessel
+                                crewCount = parts[i].vessel.GetCrewCount();
+                            else if (HighLogic.LoadedSceneIsEditor) // or crew manifest
+                                crewCount = CMAssignmentDialog.Instance.GetManifest().GetAllCrew(false).Count;
+                            else crewCount = 0; // or assume no crew (should never trip this)
+                        }
+                        if (mC.minimumCrew < crewCount)
+                            flag = false;
+                    }
+                    if(flag)
+                        maxMass += partAvionicsMass;
+                }
+            }
+            if (maxMass >= vesselMass) // will only be reached if the best we have is avionics.
+                return false; // unlock if our max avionics mass is >= vessel mass
+            // NOTE: we don't update for fuel burnt, because avionics needs to be able to handle the size
+            // as well as the fuel.
+
+            // Otherwise, we lock yaw/pitch/roll.
+            return true;
+        }
+    }
+    /// <summary>
+    /// This class will display a warning in the editor when there is insufficient avionics
+    /// </summary>
+    [KSPAddon(KSPAddon.Startup.EditorAny, false)]
+    class ControlLockerEditor : MonoBehaviour
+    {
+        // GUI
+        static Rect windowPos = new Rect(500, 200, 0, 0);
+        static bool guiEnabled = true;
+        bool haveParts = true;
+        static bool showOnlyWarning = false;
+
+        // settings
+        //static KSP.IO.PluginConfiguration config;
+        static public KeyCode key = KeyCode.I;
+        double deltaTime = 0d;
+        const double UPDATEINTERVAL = 0.25d;
+
+        // data
+        bool isControlLocked = false;
+        float maxMass, vesselMass;
+
+        public void Start()
+        {
+            //config = KSP.IO.PluginConfiguration.CreateForType<ControlLockerEditor>
+            enabled = true;
+            //config.load();
+            //windowPos = config.GetValue<Rect>
+        }
+        public void Update()
+        {
+            // toggle visibility
+            if (GameSettings.MODIFIER_KEY.GetKey() && Input.GetKeyDown(key))
+                guiEnabled = !guiEnabled;
+
+            deltaTime += TimeWarp.deltaTime;
+            if (deltaTime > UPDATEINTERVAL)
+            {
+                deltaTime = 0;
+                List<Part> parts = EditorLogic.SortedShipList;
+                haveParts = false;
+                isControlLocked = true;
+                if (parts != null)
+                {
+                    if (parts.Count > 0)
+                    {
+                        isControlLocked = ControlLockerUtils.ShouldLock(parts, false, out maxMass, out vesselMass);
+                        haveParts = true;
+                    }
+                }
+            }
+        }
+        public void OnGUI()
+        {
+            if(guiEnabled && haveParts && (!showOnlyWarning || (showOnlyWarning && isControlLocked)))
+                windowPos = GUILayout.Window("RP0ControlLocker".GetHashCode(), windowPos, DrawWindow, "Avionics");
+        }
+        public void DrawWindow(int windowID)
+        {
+            // Enable closing of the window tih "x"
+            GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+            buttonStyle.padding = new RectOffset(5, 5, 3, 0);
+            buttonStyle.margin = new RectOffset(1, 1, 1, 1);
+            buttonStyle.stretchWidth = false;
+            buttonStyle.stretchHeight = false;
+            GUIStyle labelStyle = new GUIStyle(GUI.skin.label);
+            labelStyle.wordWrap = false;
+
+            GUILayout.BeginVertical();
+
+            GUILayout.FlexibleSpace();
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("X", buttonStyle))
+            {
+                guiEnabled = false;
+            }
+            GUILayout.EndHorizontal();
+
+            if (isControlLocked)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("WARNING", labelStyle);
+                GUILayout.EndHorizontal();
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("Insufficient avionics!", labelStyle);
+                GUILayout.EndHorizontal();
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("Supports: " + maxMass.ToString("N3") + "t", labelStyle);
+                GUILayout.EndHorizontal();
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("Vessel:   " + vesselMass.ToString("N3") + "t", labelStyle);
+                GUILayout.EndHorizontal();
+            }
+            else
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("Avionics OK!", labelStyle);
+                GUILayout.EndHorizontal();
+            }
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button(showOnlyWarning ? "Set to show always" : "Set to hide if OK", buttonStyle))
+            {
+                showOnlyWarning = !showOnlyWarning;
+            }
+            GUILayout.EndHorizontal();
+
+           
+
+            GUILayout.EndVertical();
+            GUI.DragWindow();
+        }
+
+    }
+
+    /// <summary>
+    /// This class will lock controls if and only if avionics requirements exist and are not met
+    /// </summary>
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     class ControlLocker : MonoBehaviour
     {
         public int vParts = -1;
-        public float maxMass = -1f, vesselMass = 0f;
         public Vessel vessel = null;
         bool wasLocked = false;
         const ControlTypes lockmask = ControlTypes.YAW | ControlTypes.PITCH | ControlTypes.ROLL | ControlTypes.SAS | 
             ControlTypes.RCS | ControlTypes.THROTTLE | ControlTypes.WHEEL_STEER | ControlTypes.WHEEL_THROTTLE;
         const string lockID = "RP0ControlLocker";
+        float maxMass, vesselMass;
 
         public bool ShouldLock()
         {
@@ -33,40 +224,8 @@ namespace RP0
             // Do we need to update?
             if (vessel.Parts.Count != vParts)
             {
-                maxMass = 0f;
-                vesselMass = 0f;
                 vParts = vessel.Parts.Count;
-                for (int i = 0; i < vessel.Parts.Count; i++)
-                {
-                    // add up mass
-                    if ((object)(vessel.Parts[i].rb) != null)
-                        vesselMass += vessel.Parts[i].rb.mass;
-                    else
-                        vesselMass += vessel.Parts[i].mass + vessel.Parts[i].GetResourceMass();
-
-                    // get modules
-                    bool cmd = false, science = false, avionics = false;
-                    for(int j = 0; j < vessel.Parts[i].Modules.Count; j++)
-                    {
-                        cmd = cmd || ((vessel.Parts[i].Modules[j]) is ModuleCommand);
-                        science = science || ((vessel.Parts[i].Modules[j]) is ModuleScienceCore);
-                        if ((vessel.Parts[i].Modules[j]) is ModuleAvionics)
-                        {
-                            avionics = true;
-                            maxMass += ((ModuleAvionics)(vessel.Parts[i].Modules[j])).CurrentMassLimit;
-                        }
-                    }
-                    // switch based on modules
-                    if (cmd && !science && !avionics) // unencumbered command module
-                        return false;
-                }
-                if (maxMass >= vesselMass) // will only be reached if the best we have is avionics.
-                    return false; // unlock if our max avionics mass is >= vessel mass
-                // NOTE: we don't update for fuel burnt, because avionics needs to be able to handle the size
-                // as well as the fuel.
-
-                // Otherwise, we lock yaw/pitch/roll.
-                return true;
+                return ControlLockerUtils.ShouldLock(vessel.Parts, true, out maxMass, out vesselMass);
             }
             return wasLocked;
         }
@@ -90,7 +249,8 @@ namespace RP0
                 {
                     InputLockManager.SetControlLock(lockmask, lockID);
                     vessel.Autopilot.Disable();
-                    ScreenMessages.PostScreenMessage("Insufficient Avionics, Locking Controls", 3.0f);
+                    ScreenMessages.PostScreenMessage("Insufficient Avionics, Locking Controls (supports "
+                        + maxMass.ToString("N3") + "t, have " + vesselMass.ToString("N3") + "t)", 3.0f);
                 }
                 wasLocked = doLock;
             }

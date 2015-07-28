94c5a3a19abc50cdf3768ccd0ce5f0649c60d0f1
NathanKell <nathankell@gmail.com>
2015-07-27 23:17:47 -0400

    Initial commit of entry cost modifier.

---
+++ Source/EntryCosts/EntryCostModifier.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Linq;
+using UnityEngine;
+
+namespace RP0
+{
+    [KSPScenario(ScenarioCreationOptions.AddToAllGames, new GameScenes[] { GameScenes.EDITOR, GameScenes.SPACECENTER })]
+    public class EntryCostModifier : ScenarioModule
+    {
+        #region Fields
+        protected Dictionary<string, PartEntryCostHolder> partlist;
+        #region Instance
+        private static EntryCostModifier _instance = null;
+        public static EntryCostModifier Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        #endregion
+        #endregion
+
+        #region Overrides and Monobehaviour methods
+        public override void OnAwake()
+        {
+            base.OnAwake();
+
+            if (_instance != null)
+            {
+                Object.Destroy(this);
+                return;
+            }
+            _instance = this;
+
+            partlist = new Dictionary<string, PartEntryCostHolder>();
+            FillPartList();
+            GameEvents.OnPartPurchased.Add(new EventData<AvailablePart>.OnEvent(onPartPurchased));
+        }
+        public void OnDestroy()
+        {
+            GameEvents.OnPartPurchased.Remove(new EventData<AvailablePart>.OnEvent(onPartPurchased));
+        }
+
+        public override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+            UpdatePartEntryCosts();
+        }
+        public override void OnSave(ConfigNode node)
+        {
+            base.OnSave(node);
+        }
+        #endregion
+
+        #region Methods
+        public void FillPartList()
+        {
+            // precalc our node dictionary
+            Dictionary<string, ConfigNode> partnodes = new Dictionary<string, ConfigNode>();
+            foreach (ConfigNode node in GameDatabase.Instance.GetConfigNodes("ENTRYCOSTMODS"))
+                foreach (ConfigNode n in node.GetNodes("PART"))
+                    if (n.HasValue("name"))
+                        partnodes[n.GetValue("name")] = n;
+
+            // now fill our dictionary of parts
+            if (PartLoader.Instance == null || PartLoader.LoadedPartsList == null)
+            {
+                Debug.LogError("*RP-0 EC: ERROR: Partloader instance null or list null!");
+                return;
+            }
+            for (int a = PartLoader.LoadedPartsList.Count - 1; a >= 0; --a)
+            {
+                AvailablePart ap = PartLoader.LoadedPartsList[a];
+                if (ap == null)
+                {
+                    continue;
+                }
+                Part part = ap.partPrefab;
+                if (part != null)
+                {
+                    ConfigNode partnode = null;
+                    string name = GetPartName(ap);
+                    if (partnodes.TryGetValue(name, out partnode))
+                        partlist[name] = new PartEntryCostHolder(partnode, ap);
+                }
+            }
+        }
+        public void onPartPurchased(AvailablePart ap)
+        {
+            UpdatePartEntryCosts();
+        }
+        public void UpdatePartEntryCosts()
+        {
+            for (int a = PartLoader.LoadedPartsList.Count - 1; a >= 0; --a)
+            {
+                AvailablePart ap = PartLoader.LoadedPartsList[a];
+                if (ap == null)
+                {
+                    continue;
+                }
+                Part part = ap.partPrefab;
+                if (part != null)
+                {
+                    string name = GetPartName(ap);
+                    PartEntryCostHolder ec;
+                    if (partlist.TryGetValue(name, out ec))
+                        ec.UpdateCost();
+                }
+            }
+        }
+        public double PartEntryCost(string partName)
+        {
+            PartEntryCostHolder ec = null;
+            if (partlist.TryGetValue(partName, out ec))
+                return ec.EntryCost();
+
+            Debug.LogError("*RP-0 EC: ERROR: entry cost modifier for " + partName + " does not exist!");
+            return 0d;
+        }
+
+        public bool IsUnlocked(string partname)
+        {
+            PartEntryCostHolder partEC = null;
+            if (ResearchAndDevelopment.Instance != null)
+                if (partlist.TryGetValue(partname, out partEC))
+                    return ResearchAndDevelopment.PartModelPurchased(partEC.ap);
+
+            return false;
+        }
+        #endregion
+
+        #region Helpers
+        // from RF
+        public static string GetPartName(Part part)
+        {
+            if (part.partInfo != null)
+                return GetPartName(part.partInfo);
+            return GetPartName(part.name);
+        }
+
+        public static string GetPartName(AvailablePart ap)
+        {
+            return GetPartName(ap.name);
+        }
+        public static string GetPartName(string partName)
+        {
+            partName = partName.Replace(".", "-");
+            return partName.Replace("_", "-");
+        }
+        #endregion
+    }
+}
---
+++ Source/EntryCosts/PartEntryCostHolder.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RP0
+{
+    public class PartEntryCostHolder
+    {
+        #region Fields
+        public string name;
+
+        public double entryCost = 0d;
+
+        public AvailablePart ap;
+
+        public Dictionary<string, double> entryCostMultipliers = new Dictionary<string, double>();
+        public Dictionary<string, double> entryCostSubtractors = new Dictionary<string, double>();
+        #endregion
+
+        #region Constructors
+        public PartEntryCostHolder(ConfigNode node, AvailablePart part, string Name = "")
+        {
+            Load(node);
+            if(Name != "")
+                name = Name;
+            entryCost = part.entryCost;
+            ap = part;
+        }
+        #endregion
+
+        #region Methods
+        public void LoadMultipliers(ConfigNode node)
+        {
+            if (node == null)
+                return;
+
+            double dtmp;
+            foreach (ConfigNode.Value v in node.values)
+            {
+                if (double.TryParse(v.value, out dtmp))
+                    entryCostMultipliers[v.name] = dtmp;
+            }
+        }
+        public void LoadSubtractors(ConfigNode node)
+        {
+            if (node == null)
+                return;
+
+            double dtmp;
+            foreach (ConfigNode.Value v in node.values)
+            {
+                if (double.TryParse(v.value, out dtmp))
+                    entryCostSubtractors[v.name] = dtmp;
+            }
+        }
+
+        #region ConfigNode methods
+        public void Load(ConfigNode node)
+        {
+            double dtmp;
+
+            if (node.HasValue("name"))
+                name = node.GetValue("name");
+
+            if (node.HasValue("entryCost"))
+            {
+                if (double.TryParse(node.GetValue("entryCost"), out dtmp))
+                    entryCost = dtmp;
+            }
+
+            if (node.HasNode("entryCostMultipliers"))
+                LoadMultipliers(node.GetNode("entryCostMultipliers"));
+
+            if (node.HasNode("entryCostSubtractors"))
+                LoadSubtractors(node.GetNode("entryCostSubtractors"));
+        }
+        #endregion
+
+        protected double ModCost(double cost, double subtractMultipler = 1.0d)
+        {
+            foreach (KeyValuePair<string, double> kvp in entryCostMultipliers)
+            {
+                if(EntryCostModifier.Instance.IsUnlocked(kvp.Key))
+                    cost *= kvp.Value;
+            }
+
+            foreach (KeyValuePair<string, double> kvp in entryCostSubtractors)
+            {
+                if (EntryCostModifier.Instance.IsUnlocked(kvp.Key))
+                    cost -= kvp.Value * subtractMultipler;
+            }
+            if (cost > 0d)
+                return cost;
+
+            return 0d;
+        }
+        public double EntryCost()
+        {
+            return ModCost(entryCost);
+        }
+        public void UpdateCost()
+        {
+            if (ap != null)
+                ap.entryCost = (int)EntryCost();
+        }
+        #endregion
+    }
+}

3c81d1dbcbfa85b67b962878073e632082d62971
Ryan Caskey <rcaskey@adconion.com>
2017-03-07 02:41:51 +0000

    Removed reference to ProceduralParts, now using reflection to find part size.  Reorganized code

--- Source/ModuleProceduralAvionics.cs
+++ Source/ModuleProceduralAvionics.cs
@@ -9,33 +9,47 @@ namespace RP0
 {
 	class ModuleProceduralAvionics : ModuleAvionics, IPartMassModifier, IPartCostModifier
 	{
-		[KSPField]
-		public float maxDensityOfAvionics = 1f; //metric tons / cubic meter, defaults to roughly 1/3 the density of aluminum
 
+		#region KSPFields and class variables
+		// This limits how the weight of the avionics equipment per space of volume
+		// Metric tons / cubic meter, defaults to roughly 1/3 the density of aluminum
+		[KSPField]
+		public float maxDensityOfAvionics = 1f;
+
+		// This controls how much the current part can control (in metric tons)
 		[KSPField(isPersistant = true, guiName = "Tonnage", guiActive = false, guiActiveEditor = true, guiUnits = "T"),
 		 UI_FloatEdit(scene = UI_Scene.Editor, minValue = 0f, incrementLarge = 10f, incrementSmall = 1f, incrementSlide = 0.1f, sigFigs = 1, unit = "T")]
 		public float proceduralMassLimit = 1;
 		private float oldProceduralMassLimit = 1;
 
+		// We can have multiple configurations of avionics, for example: 
+		//    boosters can have a high EC usage, but lower cost and mass
+		//    probeCores can have a higher mass, but be very power efficient (and can even be turned off)
 		[KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false, guiName = "Configuration"), UI_ChooseOption(scene = UI_Scene.Editor)]
 		public string avionicsConfigName;
 		private string oldAvionicsConfigName;
 
-		private ProceduralAvionicsConfig currentProceduralAvionicsConfig;
-
+		// The currently selected avionics config
 		public ProceduralAvionicsConfig CurrentProceduralAvionicsConfig
 		{
 			get { return currentProceduralAvionicsConfig; }
 		}
 
+		protected override float getInternalMassLimit()
+		{
+			return proceduralMassLimit;
+		}
+
+		private ProceduralAvionicsConfig currentProceduralAvionicsConfig;
 		private UI_FloatEdit proceduralMassLimitEdit;
 
 		[SerializeField]
 		public byte[] proceduralAvionicsConfigsSerialized; //public so it will persist from loading to using it
 
 		private Dictionary<string, ProceduralAvionicsConfig> proceduralAvionicsConfigs;
+		#endregion
 
-
+		#region event handlers
 		public override void OnLoad(ConfigNode node)
 		{
 			try
@@ -47,127 +61,9 @@ namespace RP0
 			}
 			catch (Exception ex)
 			{
-				UnityEngine.Debug.Log("[ProcAvi] - OnLoad exception: " + ex);
+				Log("OnLoad exception: " + ex);
 				throw;
 			}
-		}
-
-		private void DeserializeObjects()
-		{
-			if (proceduralAvionicsConfigs == null && proceduralAvionicsConfigsSerialized != null)
-			{
-				UnityEngine.Debug.Log("[ProcAvi] - deserialization needed");
-				proceduralAvionicsConfigs = new Dictionary<string, ProceduralAvionicsConfig>();
-				List<ProceduralAvionicsConfig> proceduralAvionicsConfigList = ObjectSerializer.Deserialize<List<ProceduralAvionicsConfig>>(proceduralAvionicsConfigsSerialized);
-				foreach (var item in proceduralAvionicsConfigList)
-				{
-					UnityEngine.Debug.Log("[ProcAvi] - deserialized " + item.name);
-					proceduralAvionicsConfigs.Add(item.name, item);
-				}
-				UnityEngine.Debug.Log("[ProcAvi] - deserialized " + proceduralAvionicsConfigs.Count + " configs");
-
-				BaseField avionicsConfigField = Fields["avionicsConfigName"];
-				avionicsConfigField.guiActiveEditor = true;
-				UI_ChooseOption range = (UI_ChooseOption)avionicsConfigField.uiControlEditor;
-				range.options = proceduralAvionicsConfigs.Keys.ToArray();
-
-				avionicsConfigName = proceduralAvionicsConfigs.Keys.First();
-
-				UnityEngine.Debug.Log("[ProcAvi] - Defaulted config to " + avionicsConfigName);
-			}
-		}
-
-		public void LoadAvionicsConfigs(ConfigNode node)
-		{
-			var proceduralAvionicsConfigList = new List<ProceduralAvionicsConfig>();
-			proceduralAvionicsConfigs = new Dictionary<string, ProceduralAvionicsConfig>();
-			foreach (ConfigNode tNode in node.GetNodes("AVIONICSCONFIG"))
-			{
-				ProceduralAvionicsConfig config = new ProceduralAvionicsConfig();
-				config.Load(tNode);
-				proceduralAvionicsConfigList.Add(config);
-				UnityEngine.Debug.Log("[ProcAvi] - Loaded AvionicsConfg: " + config.name);
-			}
-
-			proceduralAvionicsConfigsSerialized = ObjectSerializer.Serialize(proceduralAvionicsConfigList);
-			UnityEngine.Debug.Log("[ProcAvi] - Serialized configs");
-
-		}
-
-		private void UpdateCurrentConfig()
-		{
-			if (avionicsConfigName == oldAvionicsConfigName)
-			{
-				return;
-			}
-
-			UnityEngine.Debug.Log("[ProcAvi] - Setting config to " + avionicsConfigName);
-
-			currentProceduralAvionicsConfig = proceduralAvionicsConfigs[avionicsConfigName];
-
-			oldAvionicsConfigName = avionicsConfigName;
-		}
-
-		protected override float getInternalMassLimit()
-		{
-			return proceduralMassLimit;
-		}
-
-		private void UpdateMaxValues()
-		{
-			//update mass limit value slider
-
-			if (proceduralMassLimitEdit == null)
-			{
-				proceduralMassLimitEdit = (UI_FloatEdit)Fields["proceduralMassLimit"].uiControlEditor;
-			}
-
-			var ppModule = part.FindModuleImplementing<ProceduralParts.ProceduralPart>();
-			var maxAvionicsMass = ppModule.CurrentShape.Volume * maxDensityOfAvionics;
-			proceduralMassLimitEdit.maxValue = maxAvionicsMass;
-
-		}
-
-		private void VerifyPart()
-		{
-			if (proceduralMassLimit == oldProceduralMassLimit)
-			{
-				return;
-			}
-			UnityEngine.Debug.Log("[ProcAvi] - verifying part");
-			var ppModule = part.FindModuleImplementing<ProceduralParts.ProceduralPart>();
-
-			var maxAvionicsMass = ppModule.CurrentShape.Volume * maxDensityOfAvionics;
-			UnityEngine.Debug.Log("[ProcAvi] - new mass would be " + CalculateNewMass() + ", max avionics mass is " + maxAvionicsMass);
-			if (maxAvionicsMass < CalculateNewMass())
-			{
-				proceduralMassLimit = oldProceduralMassLimit;
-				UnityEngine.Debug.Log("[ProcAvi] - resetting part");
-			}
-			else
-			{
-				oldProceduralMassLimit = proceduralMassLimit;
-				UnityEngine.Debug.Log("[ProcAvi] - part verified");
-			}
-		}
-
-		private float CalculateNewMass()
-		{
-			if (CurrentProceduralAvionicsConfig != null)
-			{
-				return proceduralMassLimit / CurrentProceduralAvionicsConfig.tonnageToMassRatio;
-			}
-			else
-			{
-				UnityEngine.Debug.Log("[ProcAvi] - Cannot compute mass yet");
-				return 0;
-			}
-		}
-
-		private float CalculateCost()
-		{
-			//TODO: define
-			return proceduralMassLimit * 100;
 		}
 
 		public new void Update()
@@ -202,5 +98,142 @@ namespace RP0
 		{
 			return ModifierChangeWhen.FIXED;
 		}
+
+		#endregion
+
+		#region config loading and serialization
+		private void DeserializeObjects()
+		{
+			if (proceduralAvionicsConfigs == null && proceduralAvionicsConfigsSerialized != null)
+			{
+				Log("deserialization needed");
+				proceduralAvionicsConfigs = new Dictionary<string, ProceduralAvionicsConfig>();
+				List<ProceduralAvionicsConfig> proceduralAvionicsConfigList = ObjectSerializer.Deserialize<List<ProceduralAvionicsConfig>>(proceduralAvionicsConfigsSerialized);
+				foreach (var item in proceduralAvionicsConfigList)
+				{
+					Log("deserialized " + item.name);
+					proceduralAvionicsConfigs.Add(item.name, item);
+				}
+				Log("deserialized " + proceduralAvionicsConfigs.Count + " configs");
+
+				BaseField avionicsConfigField = Fields["avionicsConfigName"];
+				avionicsConfigField.guiActiveEditor = true;
+				UI_ChooseOption range = (UI_ChooseOption)avionicsConfigField.uiControlEditor;
+				range.options = proceduralAvionicsConfigs.Keys.ToArray();
+
+				avionicsConfigName = proceduralAvionicsConfigs.Keys.First();
+
+				Log("Defaulted config to " + avionicsConfigName);
+			}
+		}
+
+		public void LoadAvionicsConfigs(ConfigNode node)
+		{
+			var proceduralAvionicsConfigList = new List<ProceduralAvionicsConfig>();
+			proceduralAvionicsConfigs = new Dictionary<string, ProceduralAvionicsConfig>();
+			foreach (ConfigNode tNode in node.GetNodes("AVIONICSCONFIG"))
+			{
+				ProceduralAvionicsConfig config = new ProceduralAvionicsConfig();
+				config.Load(tNode);
+				proceduralAvionicsConfigList.Add(config);
+				Log("Loaded AvionicsConfg: " + config.name);
+			}
+
+			proceduralAvionicsConfigsSerialized = ObjectSerializer.Serialize(proceduralAvionicsConfigList);
+			Log("Serialized configs");
+
+		}
+
+		private void UpdateCurrentConfig()
+		{
+			if (avionicsConfigName == oldAvionicsConfigName)
+			{
+				return;
+			}
+			Log("Setting config to " + avionicsConfigName);
+			currentProceduralAvionicsConfig = proceduralAvionicsConfigs[avionicsConfigName];
+			oldAvionicsConfigName = avionicsConfigName;
+		}
+		#endregion
+
+		#region part attribute calculations
+		private float CalculateNewMass()
+		{
+			if (CurrentProceduralAvionicsConfig != null)
+			{
+				return proceduralMassLimit * CurrentProceduralAvionicsConfig.tonnageToMassRatio;
+			}
+			else
+			{
+				Log("Cannot compute mass yet");
+				return 0;
+			}
+		}
+
+		private float CalculateCost()
+		{
+			//TODO: define
+			return proceduralMassLimit * 100;
+		}
+		#endregion
+
+		#region private utiliy functions
+		private void UpdateMaxValues()
+		{
+			//update mass limit value slider
+
+			if (proceduralMassLimitEdit == null)
+			{
+				proceduralMassLimitEdit = (UI_FloatEdit)Fields["proceduralMassLimit"].uiControlEditor;
+			}
+
+			var maxAvionicsMass = GetCurrentVolume() * maxDensityOfAvionics;
+			proceduralMassLimitEdit.maxValue = maxAvionicsMass;
+		}
+
+		private void VerifyPart()
+		{
+			if (proceduralMassLimit == oldProceduralMassLimit)
+			{
+				return;
+			}
+			Log("verifying part");
+
+			var maxAvionicsMass = GetCurrentVolume() * maxDensityOfAvionics;
+			Log("new mass would be " + CalculateNewMass() + ", max avionics mass is " + maxAvionicsMass);
+			if (maxAvionicsMass < CalculateNewMass())
+			{
+				proceduralMassLimit = oldProceduralMassLimit;
+				Log("resetting part");
+			}
+			else
+			{
+				oldProceduralMassLimit = proceduralMassLimit;
+				Log("part verified");
+			}
+		}
+
+		private float GetCurrentVolume()
+		{
+			float currentShapeVolume = float.MaxValue;
+
+			foreach (var module in part.Modules)
+			{
+				var moduleType = module.GetType();
+				if (moduleType.FullName == "ProceduralParts.ProceduralPart")
+				{
+					//Log("Procedural Parts detected"); //This would spam the logs unless we do some old/current caching
+					var reflectedShape = moduleType.GetProperty("CurrentShape").GetValue(module, null);
+					currentShapeVolume = (float)reflectedShape.GetType().GetProperty("Volume").GetValue(reflectedShape, null);
+				}
+			}
+			return currentShapeVolume;
+		}
+
+		private void Log(string message)
+		{
+			UnityEngine.Debug.Log("[ProcAvi] - " + message);
+		}
+		#endregion
 	}
 }

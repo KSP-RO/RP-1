051ed374628704374fd9d22ad2ed72679347abc7
NathanKell <nathankell@gmail.com>
2017-06-17 19:06:57 -0700

    Start work on difficulty params thing

---
+++ Source/DifficultyPresets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+
+namespace RP0
+{
+    class ModuleShowInfo : PartModule
+    {
+        public override string GetInfo()
+        {
+            string tmp = "";
+            try
+            {
+                tmp += "Part name: " + part.name;
+                // throws - tmp += "\nTech Required: " + part.partInfo.TechRequired;
+                // throws - tmp += "\nEntry Cost: " + part.partInfo.entryCost;
+            }
+            catch (Exception e)
+            {
+                Debug.Log("**RP0 error getting info, exception " + e);
+            }
+            return tmp;
+        }
+    }
+}

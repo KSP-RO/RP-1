6f11fcaf02c90182e033900ef9bc6352ed42d431
NathanKell <nathankell@gmail.com>
2017-08-01 23:43:47 -0700

    Support decoupler tag.

---
+++ Source/ModuleTag/Decoupler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RP0.ModuleTags
+{
+    class ModuleTagDecoupler : ModuleTag
+    {
+        public override string GetInfo()
+        {
+            return "Contains a decoupler";
+        }
+    }
+}

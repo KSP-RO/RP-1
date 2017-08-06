170a28487287d62f6c2110df6e7b62c915634849
NathanKell <nathankell@gmail.com>
2017-08-05 23:27:21 -0700

    Add two more module tags. @pap1723 can you add them to the new tanks in the sheet? (Not to any old parts, for now)

---
+++ Source/ModuleTag/TankBalloon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RP0.ModuleTags
+{
+    class ModuleTagTankBalloon : ModuleTag
+    {
+        public override string GetInfo()
+        {
+            return "Contains a balloon tank";
+        }
+    }
+}

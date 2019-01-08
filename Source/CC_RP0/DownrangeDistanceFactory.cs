b85fa3d1863f46a6294f9efad00978856293a856
siimav <siim.aaver@gmail.com>
2019-01-08 06:57:55 +0200

    Added CC extension for downrange SR contract (#973)
    * Added CC extension for downrange SR contract
    
    * Added CC_RP0.dll

---
+++ Source/CC_RP0/DownrangeDistanceFactory.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using UnityEngine;
+
+namespace ContractConfigurator.RP0
+{
+    public class DownrangeDistanceFactory : ParameterFactory
+    {
+        protected double distance;
+
+        public override bool Load(ConfigNode configNode)
+        {
+            bool valid = base.Load(configNode);
+
+            valid &= ConfigNodeUtil.ParseValue<double>(configNode, "distance", x => distance = x, this, 1, x => Validation.GE(x, 1));
+
+            return valid;
+        }
+
+        public override ContractParameter Generate(Contract contract)
+        {
+            return new DownrangeDistance(title, distance);
+        }
+    }
+}

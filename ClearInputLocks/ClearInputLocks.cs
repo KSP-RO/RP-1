1f98745a4896ff4b05fdc67f9c6434b9521812be
Dan Paplaczyk <papwr17@gmail.com>
2019-01-30 17:30:49 -0600

    Added ClearInputLocks.dll
    This adds the ClearInputLocks to the F7 key so you no longer have to go into the ALT-F12 menu

---
+++ ClearInputLocks/ClearInputLocks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using KSP;
+
+namespace ClearInputLocks.RP0
+{
+    [KSPScenario(ScenarioCreationOptions.AddToAllGames, new GameScenes[] { GameScenes.EDITOR, GameScenes.FLIGHT, GameScenes.SPACECENTER, GameScenes.TRACKSTATION })]
+    public class ClearInputLocks : ScenarioModule
+    {
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F7))
+            {
+                InputLockManager.ClearControlLocks();
+                // Debug.Log("Input Locks Cleared");
+            }
+        }
+    }        
+}
---
+++ ClearInputLocks/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ClearInputLocks")]
+[assembly: AssemblyDescription("Simple DLL to Map ClearInputLocks from Cheat Menu to F7 key")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KSP-RO")]
+[assembly: AssemblyProduct("ClearInputLocks")]
+[assembly: AssemblyCopyright("Copyright ©  2019 CC-BY-NC-SA 4.0")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("25cfd266-ed2f-47cd-af33-4365c8d381e5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: KSPAssembly("RP-0", 1, 00)]

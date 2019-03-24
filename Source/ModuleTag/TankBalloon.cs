2df21e9e005ad9844ba26000624a204b1caa7879
gamer-rp1 <47701718+gamer-rp1@users.noreply.github.com>
2019-03-25 08:55:47 +1100

    Cost 2.0 -> 2.5 as in RP1_KCT_Presets.cfg

--- Source/ModuleTag/TankBalloon.cs
+++ Source/ModuleTag/TankBalloon.cs
@@ -10,7 +10,7 @@ namespace RP0.ModuleTags
         {
             string str = string.Empty;
             str = "Contains a Balloon Tank which are very difficult to handle properly and must be pressurized properly at all times increasing the overall rollout costs.\n\n" +
-                "<b><color=orange>Rollout Cost: Cost of This Part * 2.0</color></b>";
+                "<b><color=orange>Rollout Cost: Cost of This Part * 2.5</color></b>";
             return str;
         }
     }

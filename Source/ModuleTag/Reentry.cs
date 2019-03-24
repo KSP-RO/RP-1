f33f0acca7e938bdb71d4d160dd282884ab54b4a
gamer-rp1 <47701718+gamer-rp1@users.noreply.github.com>
2019-03-25 08:53:57 +1100

    Cost 2.0 -> 1.5 as in RP1_KCT_Presets.cfg

--- Source/ModuleTag/Reentry.cs
+++ Source/ModuleTag/Reentry.cs
@@ -10,7 +10,7 @@ namespace RP0.ModuleTags
         {
             string str = string.Empty;
             str = "Contains reentry equipment that needs proper integration and testing for safety. It will increase the overall rollout costs.\n\n" +
-                "<b><color=orange>Rollout Cost: Cost of This Part * 2.0</color></b>";
+                "<b><color=orange>Rollout Cost: Cost of This Part * 1.5</color></b>";
             return str;
         }
     }

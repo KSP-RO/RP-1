2d3e63ef09c0742f380a69e585c86c78b7b50ed0
siimav <siim.aaver@gmail.com>
2019-05-11 23:44:36 +0300

    Performance and GC optimizations for ToolingGUI

--- Source/Tooling/ToolingGUI.cs
+++ Source/Tooling/ToolingGUI.cs
@@ -12,26 +12,34 @@ namespace RP0
             public float untooledMultiplier;
             public float totalCost;
         };
-        private Vector2 toolingTypesScroll = new Vector2(), untooledTypesScroll = new Vector2();
-        private double deltaTime = 0d;
-        private const double UPDATEINTERVAL = 0.25d;
-        private List<untooledPart> untooledParts = new List<untooledPart>();
+
         public string currentToolingType;
+
+        private const float UPDATEINTERVAL = 0.5f;
+
+        private float nextUpdate = 0f;
+        private float allTooledCost;
+        private List<untooledPart> untooledParts = new List<untooledPart>();
+        private Vector2 toolingTypesScroll = new Vector2(), untooledTypesScroll = new Vector2();
 
         private void MaybeUpdate()
         {
             if (!HighLogic.LoadedSceneIsEditor)
                 return;
-            deltaTime += Time.deltaTime;
-            if (deltaTime > UPDATEINTERVAL) {
-                deltaTime = 0;
+
+            if (Time.time > nextUpdate)
+            {
+                nextUpdate = Time.time + UPDATEINTERVAL;
                 Update();
             }
         }
 
         private void Update()
         {
+            Debug.Log("[ModuleTooling] Running GUI data update");
             untooledParts.Clear();
+            float totalUntooledExtraCost = 0;
+
             if (EditorLogic.fetch != null && EditorLogic.fetch.ship != null && EditorLogic.fetch.ship.Parts.Count > 0) {
                 for (int i = EditorLogic.fetch.ship.Parts.Count; i-- > 0;) {
                     Part p = EditorLogic.fetch.ship.Parts[i];
@@ -41,18 +49,21 @@ namespace RP0
                         if (m is ModuleTooling && !((mT = (m as ModuleTooling)).IsUnlocked())) {
                             untooledPart uP;
                             if (m is ModuleToolingDiamLen) {
-                                uP.name = p.partInfo.title + " (" + mT.ToolingType + ") " + (m as ModuleToolingDiamLen).GetDimensions();
+                                uP.name = $"{p.partInfo.title} ({mT.ToolingType}) {(m as ModuleToolingDiamLen).GetDimensions()}";
                             } else {
-                                uP.name = p.partInfo.title + " (" + mT.ToolingType + ")";
+                                uP.name = $"{p.partInfo.title} ({mT.ToolingType})";
                             }
                             uP.toolingCost = mT.GetToolingCost();
                             uP.untooledMultiplier = mT.untooledMultiplier;
                             uP.totalCost = p.GetModuleCosts(p.partInfo.cost) + p.partInfo.cost;
+                            totalUntooledExtraCost += GetUntooledExtraCost(uP);
                             untooledParts.Add(uP);
                         }
                     }
                 }
             }
+
+            allTooledCost = EditorLogic.fetch.ship.GetShipCosts(out _, out _) - totalUntooledExtraCost;
         }
 
         public tabs toolingTab()
@@ -100,10 +111,10 @@ namespace RP0
                         try
                         {
                             GUILayout.Label(uP.name, boldLabel, GUILayout.Width(312));
-                            GUILayout.Label(uP.toolingCost.ToString("N0") + "f", rightLabel, GUILayout.Width(72));
+                            GUILayout.Label($"{uP.toolingCost:N0}f", rightLabel, GUILayout.Width(72));
                             var untooledExtraCost = GetUntooledExtraCost(uP);
-                            GUILayout.Label(uP.totalCost.ToString("N0") + "f", rightLabel, GUILayout.Width(72));
-                            GUILayout.Label((uP.totalCost - untooledExtraCost).ToString("N0") + "f", rightLabel, GUILayout.Width(72));
+                            GUILayout.Label($"{uP.totalCost:N0}f", rightLabel, GUILayout.Width(72));
+                            GUILayout.Label($"{(uP.totalCost - untooledExtraCost):N0}f", rightLabel, GUILayout.Width(72));
                         }
                         finally {
                             GUILayout.EndHorizontal();
@@ -114,8 +125,7 @@ namespace RP0
                 }
                 GUILayout.BeginHorizontal();
                 try {
-                    var allTooledCost = EditorLogic.fetch.ship.GetShipCosts(out _, out _) - untooledParts.Slinq().Select(p => GetUntooledExtraCost(p)).Sum();
-                    GUILayout.Label("Total vessel cost if all parts are tooled: " + allTooledCost.ToString("N0"));
+                    GUILayout.Label($"Total vessel cost if all parts are tooled: {allTooledCost:N0)}");
                 } finally {
                     GUILayout.EndHorizontal();
                 }
@@ -133,7 +143,7 @@ namespace RP0
                         new Vector2(0.5f, 0.5f),
                         new MultiOptionDialog(
                             "ConfirmAllToolingsPurchase",
-                            "Tooling for all untooled parts will cost " + totalToolingCost.ToString("N0") + " funds.",
+                            $"Tooling for all untooled parts will cost {totalToolingCost:N0)} funds.",
                             "Tooling Purchase",
                             HighLogic.UISkin,
                             new Rect(0.5f, 0.5f, 150f, 60f),
@@ -185,10 +195,11 @@ namespace RP0
         private void toolingTypeRow(float diameter, float length)
         {
             GUILayout.BeginHorizontal();
-            try {
-                GUILayout.Label(diameter.ToString("F3") + "m", HighLogic.Skin.label, GUILayout.Width(80));
+            try
+            {
+                GUILayout.Label($"{diameter:F3}m", HighLogic.Skin.label, GUILayout.Width(80));
                 GUILayout.Label("×", HighLogic.Skin.label);
-                GUILayout.Label(length.ToString("F3") + "m", rightLabel, GUILayout.Width(80));
+                GUILayout.Label($"{length:F3}m", rightLabel, GUILayout.Width(80));
             } finally {
                 GUILayout.EndHorizontal();
             }
@@ -199,7 +210,7 @@ namespace RP0
             GUILayout.BeginHorizontal();
             try {
                 GUILayout.FlexibleSpace();
-                GUILayout.Label("Toolings for type "+currentToolingType, HighLogic.Skin.label);
+                GUILayout.Label($"Toolings for type {currentToolingType}", HighLogic.Skin.label);
                 GUILayout.FlexibleSpace();
             } finally {
                 GUILayout.EndHorizontal();

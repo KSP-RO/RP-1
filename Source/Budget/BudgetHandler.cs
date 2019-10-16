b657aa90470e578a75fd664096dc0755694ed566
ryanc55 <54254625+ryanc55@users.noreply.github.com>
2019-10-16 16:35:43 -0700

    Difficulty option,KAC integration,Budget Reports
    Added KAC integration for Budget timer
    KAC integration controlled by difficulty option.
    Added Budget Report

--- Source/Budget/BudgetHandler.cs
+++ Source/Budget/BudgetHandler.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using UnityEngine;
+using KSP.UI.Screens;
+using System.Linq;
 
 namespace RP0
 {
@@ -21,6 +23,12 @@ namespace RP0
         [KSPField(isPersistant = true)]
         public double payout = 0;
 
+        [KSPField(isPersistant = true)]
+        public double  lastreputation= 0;
+
+        [KSPField(isPersistant = true)]
+        public string BudgetAlarmID = "";
+
         public const int BudgetPeriodMonths = 3;
         public const double BaseBudgetStart = 20000 * BudgetPeriodMonths / 12;
         public const double BaseBudgetEnd = 200000 * BudgetPeriodMonths / 12;
@@ -34,9 +42,10 @@ namespace RP0
 
         public void Start()
         {
-            if(reputation == -double.MaxValue)
+            if (reputation == -double.MaxValue)
             {
                 reputation = Reputation.CurrentRep;
+                lastreputation = reputation;
             }
         }
 
@@ -75,6 +84,10 @@ namespace RP0
             {
                 return;
             }
+            if (KACWrapper.APIReady && HighLogic.CurrentGame.Parameters.CustomParams<RP0Settings>().SetBudgetAlamrs && BudgetAlarmID == "" && nextUpdate != 0)
+            {
+                ScheduleNextUpdate();  // If SetBudgetAlamrs setting changes we need to schedule.
+            }
 
             if (nextUpdate > Planetarium.GetUniversalTime())
             {
@@ -82,6 +95,7 @@ namespace RP0
             }
             StopTimeWarp();
             PayBudget();
+            nextUpdate = Epoch.AddMonths(BudgetPeriodMonths * budgetCounter).Date.Subtract(Epoch).TotalSeconds;
             ScheduleNextUpdate();
         }
 
@@ -89,9 +103,23 @@ namespace RP0
 
         private void ScheduleNextUpdate()
         {
-            nextUpdate = Epoch.AddMonths(BudgetPeriodMonths * budgetCounter).Date.Subtract(Epoch).TotalSeconds;
+            if (KACWrapper.APIReady && HighLogic.CurrentGame.Parameters.CustomParams<RP0Settings>().SetBudgetAlamrs)
+            {
+                if (BudgetAlarmID != "")
+                {
+                    KACWrapper.KAC.DeleteAlarm(BudgetAlarmID);
+                }
+                BudgetAlarmID = KACWrapper.KAC.CreateAlarm(KACWrapper.KACAPI.AlarmTypeEnum.Raw, "Next Budget Payout", nextUpdate);
+                if (BudgetAlarmID != "")
+                {
+                    KACWrapper.KACAPI.KACAlarm a = KACWrapper.KAC.Alarms.First(z => z.ID == BudgetAlarmID);
+                    a.AlarmAction = KACWrapper.KACAPI.AlarmActionEnum.KillWarpOnly;
+                }
+            } else
+            {
+                BudgetAlarmID = ""; 
+            }
         }
-
         private void PayBudget()
         {
             payout = GetBudget();
@@ -104,14 +132,25 @@ namespace RP0
             var baseBudget = GetBaseBudget();
             var repBudget = GetRepBudget();
             var repTransfer = GetBufferTransfer();
-            buffer -= repTransfer;
-            reputation += repTransfer - repBudget / ReputationToFundsFactor;
             var budget = baseBudget + repBudget;
+            StockMessage("Budget Report",
+                String.Format("Budget Report for {0}\n", KSPUtil.PrintDate(BudgetHandler.Instance.nextUpdate, false)) +
+                              "---------------------------------------------------------------\n" +
+                String.Format("Last Period Reputation:\t\t{0:F0}\n", lastreputation) +
+                String.Format("Reputation earned this period:\t{0:F0}\n", reputation + buffer - lastreputation) +
+                String.Format("Reputation converted to funds:\t{0:F1}\n", repBudget/ReputationToFundsFactor) +
+                String.Format("Reputation balance:\t\t\t{0:F0}\n\n", reputation + buffer - repBudget / ReputationToFundsFactor) +
+                String.Format("Base budget payout: \t\t\t{0:F0}\n", baseBudget) +
+                String.Format("Reputation conversion payout:\t{0:F0}\n", repBudget) +
+                String.Format("Total payout:\t\t\t\t{0:F0}\n", budget));
+            buffer -= repTransfer;
+            lastreputation = reputation;
+            reputation += repTransfer - repBudget / ReputationToFundsFactor;
             Debug.Log($"[RP0] Budget payout: {budget} (Base: {baseBudget}, Rep: {repBudget})");
             return budget;
         }
 
-        public double GetBaseBudget() => BaseBudgetStart * Math.Pow(BaseBudgetEnd / BaseBudgetStart, (float) Math.Min(StartToEndPeriods, budgetCounter) / StartToEndPeriods);
+        public double GetBaseBudget() => BaseBudgetStart * Math.Pow(BaseBudgetEnd / BaseBudgetStart, (float)Math.Min(StartToEndPeriods, budgetCounter) / StartToEndPeriods);
 
         public double GetBufferTransfer()
         {
@@ -123,6 +162,12 @@ namespace RP0
             var reputationIncrease = GetBufferTransfer();
             var reputationToConvert = Math.Max((reputation + reputationIncrease) * ReputationDecayFactor, 0);
             return reputationToConvert * ReputationToFundsFactor;
+        }
+        private void StockMessage(String title, String text)
+        {
+
+            MessageSystem.Message m = new MessageSystem.Message(title, text, MessageSystemButton.MessageButtonColor.GREEN, MessageSystemButton.ButtonIcons.MESSAGE);
+            MessageSystem.Instance.AddMessage(m);
         }
     }
 }

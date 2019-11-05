03e581b0574c215a2309878374c28e1f5ce3fc45
siimav <siim.aaver@gmail.com>
2019-11-06 00:48:03 +0200

    Increment assembly versions and recompile DLLs

--- Source/CC_RP0/Properties/AssemblyInfo.cs
+++ Source/CC_RP0/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
 [assembly: KSPAssembly("RP-0", 1, 00)]

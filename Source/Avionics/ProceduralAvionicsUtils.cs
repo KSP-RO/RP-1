52a92b9ba86491a8b6abec1ae965446f2a294427
Pap <papwr17@gmail.com>
2019-12-14 13:10:17 -0600

    Proc Avionics Changes (#1169)
    * Fix Exceptions from UIBase()
    
    Unity is unhappy with some UI calls from a Monobehavior constructor.  Move UIHolder's TopWindow and CrewHandler's FSGUI instantiation to Start methods
    
    * Linting
    
    Remove unnecessary using statements
    Use interpolated strings
    
    * Modify for new ProcParts (no minVolume enforcement)
    
    Reduce configured controllable mass instead of forcing sufficient volume.  Enforce its limits in change handlers instead of Get()
    Update change handlers (no "oldVariableName" tests)
    Re-order some Start() calls & cached volume update.
    Inline some very simple and clear methods.
    Linting method ordering / grouping
    Linting KSPField declarations,  interpolated strings, newer language features
    Update to newer PAW refresh call
    
    * Define ModuleToolingProcAvionics later in PA module list
    
    ModuleProceduralAvionics needs to run in KSP's OnStart() not Unity's Start(), in order to initialize before the tooling modules do.  (Unity.Start() runs after KSP Part.ModulesOnStart() completes)
    Don't define the tooling module first, or it will go looking for fields that aren't initialized yet.
    Similarly, UpdateButton() -after- LoadPartModules()
    
    * Fix ControllableMass NaN Behavior
    
    GetControllableMass fixes a NaN result instead of propagating it.

--- Source/Avionics/ProceduralAvionicsUtils.cs
+++ Source/Avionics/ProceduralAvionicsUtils.cs
@@ -1,38 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace RP0.ProceduralAvionics
+﻿namespace RP0.ProceduralAvionics
 {
-	static class ProceduralAvionicsUtils
-	{
-		private static bool _enableLogging = false;
+    static class ProceduralAvionicsUtils
+    {
+        private static bool _enableLogging = true;
 
-		private const string logPreix = "[ProcAvi] - ";
-		public static void Log(params string[] message)
-		{
-			if (_enableLogging) {
-				var builder = StringBuilderCache.Acquire();
-				builder.Append(logPreix);
-				foreach (string part in message) {
-					builder.Append(part);
-				}
-				UnityEngine.Debug.Log(builder.ToStringAndRelease());
-			}
-		}
+        private const string logPreix = "[ProcAvi] - ";
+        public static void Log(params string[] message)
+        {
+            if (_enableLogging) {
+                var builder = StringBuilderCache.Acquire();
+                builder.Append(logPreix);
+                foreach (string part in message) {
+                    builder.Append(part);
+                }
+                UnityEngine.Debug.Log(builder.ToStringAndRelease());
+            }
+        }
 
-		public static void Log(params object[] parts)
-		{
-			if (_enableLogging) {
-				var builder = StringBuilderCache.Acquire();
-				builder.Append(logPreix);
-				foreach (object part in parts) {
-					builder.Append(part.ToString());
-				}
-				UnityEngine.Debug.Log(builder.ToStringAndRelease());
-			}
-		}
+        public static void Log(params object[] parts)
+        {
+            if (_enableLogging) {
+                var builder = StringBuilderCache.Acquire();
+                builder.Append(logPreix);
+                foreach (object part in parts) {
+                    builder.Append(part.ToString());
+                }
+                UnityEngine.Debug.Log(builder.ToStringAndRelease());
+            }
+        }
 
-	}
+    }
 }

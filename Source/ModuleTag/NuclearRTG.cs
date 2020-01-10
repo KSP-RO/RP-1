e20a636d7ac8b3639e2c8b305c64a51488d83f14
Pap <papwr17@gmail.com>
2020-01-10 07:54:23 -0600

    Added new NuclearRTG tag to fix too high of a Nuclear penalty

---
+++ Source/ModuleTag/NuclearRTG.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RP0.ModuleTags
+{
+    public class ModuleTagNuclearRTG : ModuleTag
+    {
+        public override string GetInfo()
+        {
+            string str = string.Empty;
+            str = "Contains radioactive elements that must be carefully handled and integrated. Additional permits need to be secure increasing rollout costs.\n\n" +
+                "<b><color=orange>Rollout Cost: Cost of This Part * 3.0</color></b>";
+            return str;
+        }
+    }
+}

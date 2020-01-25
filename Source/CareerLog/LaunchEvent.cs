a6fa5db388579ccd1b52cf71cb11b9fa9d427305
siimav <siim.aaver@gmail.com>
2020-01-25 02:55:15 +0200

    Implement career progress  tracking

---
+++ Source/CareerLog/LaunchEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RP0
+{
+    public class LaunchEvent : CareerEvent
+    {
+        [Persistent]
+        public string VesselName;
+
+        public LaunchEvent(double UT) : base(UT)
+        {
+        }
+
+        public LaunchEvent(ConfigNode n) : base(n)
+        {
+        }
+    }
+}

2f7dea0179ad7cabfc33cdfa29a357109a1a6b80
nepphhh <kikjezrous@gmail.com>
2020-02-22 14:06:06 -0500

    Added AvionicsCheck parameter

---
+++ Source/CC_RP0/AvionicsCheck.cs
@@ -0,0 +1,100 @@
+﻿using ContractConfigurator.Parameters;
+using Contracts;
+
+namespace ContractConfigurator.RP0
+{
+    public class AvionicsCheckFactory : ParameterFactory
+    {
+        protected bool continuousControlRequired;
+
+        public override bool Load(ConfigNode configNode)
+        {
+            bool valid = base.Load(configNode);
+
+            // Load parameter options
+            valid &= ConfigNodeUtil.ParseValue<bool>(configNode, "continuousControlRequired", x => continuousControlRequired = x, this, false);
+
+            return valid;
+        }
+
+        public override ContractParameter Generate(Contract contract)
+        {
+            return new AvionicsCheckParameter(title, continuousControlRequired);
+        }
+    }
+    public class AvionicsCheckParameter : VesselParameter
+    {
+        protected bool parameterIsSatisified = true;
+        protected bool controlHasLapsed = false;
+        protected bool continuousControlRequired = false;
+
+        public AvionicsCheckParameter() : base(null) { }
+
+        public AvionicsCheckParameter(string title, bool continuousControlRequired) : base(title)
+        {
+            this.continuousControlRequired = continuousControlRequired;
+            this.title = GetParameterTitle();
+        }
+
+        protected override void OnParameterSave(ConfigNode node)
+        {
+            base.OnParameterSave(node);
+
+            // Save parameter options on a per-vessel basis
+            node.AddValue("continuousControlRequired", continuousControlRequired);
+        }
+
+        protected override void OnParameterLoad(ConfigNode node)
+        {
+            base.OnParameterLoad(node);
+
+            // Save parameter options on a per-vessel basis
+            node.TryGetValue("continuousControlRequired", ref continuousControlRequired);
+        }
+        protected override void OnRegister()
+        {
+            base.OnRegister();
+
+            // We will only check the status of the parameter when input locks have changed
+            GameEvents.onInputLocksModified.Add(OnInputLocksModified);
+        }
+
+        protected override void OnUnregister()
+        {
+            base.OnUnregister();
+            GameEvents.onInputLocksModified.Remove(OnInputLocksModified);
+        }
+        protected override string GetParameterTitle()
+        {
+            // Title will change to reflect the state of the parameter, with special considerations if we
+            // need the user to not lose control at all during flight
+            return continuousControlRequired ?
+                controlHasLapsed ? $"Maintain sufficient avionics (failed, control was lost)"
+                    : $"Maintain sufficient avionics (do not lose control)"
+                : $"Have sufficient avionics for control";
+        }
+
+        private void OnInputLocksModified(GameEvents.FromToAction<ControlTypes, ControlTypes> data)
+        {
+            // Detect if a control lock is active
+            bool controlIsLocked = InputLockManager.GetControlLock("RP0ControlLocker") != 0;
+
+            // Keeps track if we've ever lost control
+            controlHasLapsed |= controlIsLocked;
+
+            // Differing logic depending on parameter properties
+            parameterIsSatisified = continuousControlRequired ? !controlHasLapsed && !controlIsLocked : !controlIsLocked;
+
+            // Refresh title in contracts screen in case we've had a lapse of control with continousControlRequired true
+            GetTitle();
+
+            // Have CC re-evaluate the parameter state (will call VesselMeetsCondition() internally)
+            CheckVessel(FlightGlobals.ActiveVessel);
+        }
+
+        protected override bool VesselMeetsCondition(Vessel vessel)
+        {
+            return parameterIsSatisified;
+        }
+    }
+}

dc0002eb390306610160e98b37c20db04e8336b9
Pap <papwr17@gmail.com>
2020-07-27 20:35:08 -0500

    Added a ModuleStationPart so that Station Habitats can be identified

---
+++ Source/ModuleStationPart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+namespace RP0
+{
+    public class ModuleStationPart : PartModule
+    {
+        public override string GetInfo()
+        {
+            return "Part is considered to be a habitat for a Space Station.";
+        }
+    }
+}

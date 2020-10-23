f03fe30ec716eba5980f4ea223f2d66fa5ddbe21
siimav <siim.aaver@gmail.com>
2020-10-24 02:22:58 +0300

    Change "Export to web" settings UI; add persistence

--- Source/CareerLog/CareerLogGUI.cs
+++ Source/CareerLog/CareerLogGUI.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using UnityEngine;
 
 namespace RP0
@@ -9,6 +10,20 @@ namespace RP0
         private string _exportStatus;
         private string _exportStatusWeb;
         private string _serverUrl;
+        private string _token;
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            var settings = HighLogic.CurrentGame.Parameters.CustomParams<RP0Settings>();
+            if (!string.IsNullOrWhiteSpace(settings?.CareerLog_URL))
+            {
+                byte[] bytes = Convert.FromBase64String(settings.CareerLog_URL);
+                _serverUrl = Encoding.UTF8.GetString(bytes);
+            }
+            _token = settings?.CareerLog_Token;
+        }
 
         public void RenderTab()
         {
@@ -29,13 +44,20 @@ namespace RP0
             GUILayout.Label(_exportStatus);
 
             GUILayout.Label("Server URL:");
-            _serverUrl = GUILayout.TextField(_serverUrl);
+            _serverUrl = GUILayout.TextField(_serverUrl, HighLogic.Skin.textField);
 
-            if (GUILayout.Button("Export to web", GUILayout.ExpandWidth(false), GUILayout.Height(30),
+            GUILayout.Label("Token:");
+            _token = GUILayout.TextField(_token, HighLogic.Skin.textField);
+
+            if (GUILayout.Button("Export to web", HighLogic.Skin.button, GUILayout.ExpandWidth(false), GUILayout.Height(30),
                 GUILayout.Width(125)))
             {
                 try
                 {
+                    var settings = HighLogic.CurrentGame.Parameters.CustomParams<RP0Settings>();
+                    settings.CareerLog_URL = Convert.ToBase64String(Encoding.UTF8.GetBytes(_serverUrl));    // KSP really doesn't like the symbols that a typical URL contains
+                    settings.CareerLog_Token = _token;
+
                     CareerLog.Instance.ExportToWeb(_serverUrl);
                     _exportStatusWeb = "Career progress exported to web.";
                 }
@@ -45,8 +67,6 @@ namespace RP0
                 }
             }
             GUILayout.Label(_exportStatusWeb);
-            GUILayout.Label("CareerLog id:");
-            GUILayout.TextField($"{SystemInfo.deviceUniqueIdentifier}");
 
             GUILayout.EndVertical();
         }

fcdbba0ce1763bf9ecc87bafbd22a40c0dfabb0c
DRVeyl <DRVeyl@gmail.com>
2021-03-16 20:53:10 -0400

    AirlaunchParams Validate consolidation

--- Source/KerbalConstructionTime/AirlaunchParams.cs
+++ Source/KerbalConstructionTime/AirlaunchParams.cs
@@ -24,37 +24,18 @@ namespace KerbalConstructionTime
             double minKscDist = 0;
 
             if (KscAzimuth >= 360 || KscAzimuth < 0)
-            {
                 errorMsg = "Invalid KSC azimuth";
-                return false;
-            }
-
-            if (LaunchAzimuth >= 360 || LaunchAzimuth < 0)
-            {
-                errorMsg = "Invalid KSC azimuth";
-                return false;
-            }
-
-            if (Altitude > lvl.MaxAltitude || Altitude < lvl.MinAltitude)
-            {
+            else if (LaunchAzimuth >= 360 || LaunchAzimuth < 0)
+                errorMsg = "Invalid Launch azimuth";
+            else if (Altitude > lvl.MaxAltitude || Altitude < lvl.MinAltitude)
                 errorMsg = $"Altitude needs to be between {lvl.MinAltitude} and {lvl.MaxAltitude} m";
-                return false;
-            }
-
-            if (Velocity > lvl.MaxVelocity || Velocity < lvl.MinVelocity)
-            {
+            else if (Velocity > lvl.MaxVelocity || Velocity < lvl.MinVelocity)
                 errorMsg = $"Velocity needs to be between {lvl.MinVelocity} and {lvl.MaxVelocity} m/s";
-                return false;
-            }
-
-            if (KscDistance > lvl.MaxKscDistance || KscDistance < minKscDist)
-            {
+            else if (KscDistance > lvl.MaxKscDistance || KscDistance < minKscDist)
                 errorMsg = $"Distance from Space Center needs to be between {minKscDist / 1000:0.#} and {lvl.MaxKscDistance / 1000:0.#} km";
-                return false;
-            }
-
-            errorMsg = null;
-            return true;
+            else
+                errorMsg = null;
+            return errorMsg == null;
         }
     }
 }

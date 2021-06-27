607bc1f5e3f599555488dd6e8a067c89a83366b5
siimav <siim.aaver@gmail.com>
2021-06-28 01:10:22 +0300

    Add Contracts tab that allows setting the desired payload amount for last-tier sat contracts (#1514)

---
+++ Source/UI/ContractGUI.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using UnityEngine;
+
+namespace RP0
+{
+    public class ContractGUI : UIBase
+    {
+        public const int MinPayload = 400;
+        public const int MaxPayload = 10000;
+
+        public static int CommsPayload = MinPayload;
+        public static int WeatherPayload = MinPayload;
+
+        public static Func<string, bool> WithdrawContractAction;
+
+        private static string[] _comSatContracts = new[] { "GEORepeatComSats", "TundraRepeatComSats", "MolniyaRepeatComSats" };
+        private static string[] _weatherSatContracts = new[] { "GEOWeather" };
+
+        private RP0Settings _settings;
+
+        protected override void OnStart()
+        {
+            _settings = HighLogic.CurrentGame.Parameters.CustomParams<RP0Settings>();
+            CommsPayload = _settings.CommsPayload;
+            WeatherPayload = _settings.WeatherPayload;
+        }
+
+        public void RenderContractsTab()
+        {
+            GUILayout.BeginVertical();
+            try
+            {
+                GUILayout.Label($"Use this tab to set the required payload amount for contracts.", BoldLabel);
+                GUILayout.Space(10f);
+
+                GUILayout.Label($"CommSat Payload range: {Math.Max(CommsPayload / 2, 300)} - {CommsPayload}", HighLogic.Skin.label, GUILayout.Width(250));
+                float commsAmnt = GUILayout.HorizontalSlider(CommsPayload, MinPayload, MaxPayload, HighLogic.Skin.horizontalSlider, HighLogic.Skin.horizontalSliderThumb);
+                CommsPayload = Mathf.RoundToInt(commsAmnt / 100) * 100;    // slider works in increments of 100
+                GUILayout.Space(5f);
+                GUILayout.Label($"WeatherSat Payload range: {Math.Max(WeatherPayload / 2, 300)} - {WeatherPayload}", HighLogic.Skin.label, GUILayout.Width(250));
+                float weatherAmnt = GUILayout.HorizontalSlider(WeatherPayload, MinPayload, MaxPayload, HighLogic.Skin.horizontalSlider, HighLogic.Skin.horizontalSliderThumb);
+                WeatherPayload = Mathf.RoundToInt(weatherAmnt / 100) * 100;
+
+                if (_settings.CommsPayload != CommsPayload)
+                {
+                    foreach (string contractName in _comSatContracts)
+                    {
+                        WithdrawContractAction?.Invoke(contractName);
+                    }
+                }
+
+                if (_settings.WeatherPayload != WeatherPayload)
+                {
+                    foreach (string contractName in _weatherSatContracts)
+                    {
+                        WithdrawContractAction?.Invoke(contractName);
+                    }
+                }
+
+                _settings.CommsPayload = CommsPayload;
+                _settings.WeatherPayload = WeatherPayload;
+            }
+            finally
+            {
+                GUILayout.EndVertical();
+            }
+        }
+    }
+}

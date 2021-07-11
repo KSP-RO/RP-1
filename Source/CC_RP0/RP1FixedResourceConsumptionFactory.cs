7d9d5f1bc518d6a8b1aa0880571d6886a4bfa19a
NathanKell <nathankell@gmail.com>
2021-07-11 13:49:44 -0700

    Implement a fixed ResourceConsumption parameter and switch to using that @siimav

---
+++ Source/CC_RP0/RP1FixedResourceConsumptionFactory.cs
@@ -0,0 +1,31 @@
+﻿using Contracts;
+
+namespace ContractConfigurator.RP0
+{
+    /// <summary>
+    /// ParameterFactory wrapper for HasResource ContractParameter.
+    /// </summary>
+    public class RP1FixedResourceConsumptionFactory : ParameterFactory
+    {
+        protected double minRate;
+        protected double maxRate;
+        protected PartResourceDefinition resource;
+
+        public override bool Load(ConfigNode configNode)
+        {
+            // Load base class
+            bool valid = base.Load(configNode);
+
+            valid &= ConfigNodeUtil.ParseValue<double>(configNode, "minRate", x => minRate = x, this, double.MinValue);
+            valid &= ConfigNodeUtil.ParseValue<double>(configNode, "maxRate", x => maxRate = x, this, double.MaxValue);
+            valid &= ConfigNodeUtil.ParseValue<PartResourceDefinition>(configNode, "resource", x => resource = x, this);
+
+            return valid;
+        }
+
+        public override ContractParameter Generate(Contract contract)
+        {
+            return new RP1FixedResourceConsumption(minRate, maxRate, resource, title);
+        }
+    }
+}

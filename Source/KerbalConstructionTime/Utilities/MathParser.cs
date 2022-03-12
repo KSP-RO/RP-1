f5dd0b06872d4446574eb804193f252d81798263
NathanKell <nathankell@gmail.com>
2022-03-12 00:40:52 -0800

    Rip out the common/separate build line selection, everything's unified now.

--- Source/KerbalConstructionTime/Utilities/MathParser.cs
+++ Source/KerbalConstructionTime/Utilities/MathParser.cs
@@ -46,29 +46,11 @@ namespace KerbalConstructionTime
             int level = 0, upgrades = 0;
             var variables = new Dictionary<string, string>();
 
-            if (PresetManager.Instance.ActivePreset.GeneralSettings.CommonBuildLine)
+            level = Utilities.GetBuildingUpgradeLevel(SpaceCenterFacility.VehicleAssemblyBuilding);
+            if (KSC.VABUpgrades.Count > index)
             {
-                level = Utilities.GetBuildingUpgradeLevel(SpaceCenterFacility.VehicleAssemblyBuilding);
-                if (KSC.VABUpgrades.Count > index)
-                {
-                    upgrades = KSC.VABUpgrades[index];
-                    upgrades += KSC.SPHUpgrades.Count > index ? KSC.SPHUpgrades[index] : 0;
-                }
-            }
-            else
-            {
-                if (type == BuildListVessel.ListType.VAB)
-                {
-                    level = Utilities.GetBuildingUpgradeLevel(SpaceCenterFacility.VehicleAssemblyBuilding);
-                    if (KSC.VABUpgrades.Count > index)
-                        upgrades = KSC.VABUpgrades[index];
-                }
-                else if (type == BuildListVessel.ListType.SPH)
-                {
-                    level = Utilities.GetBuildingUpgradeLevel(SpaceCenterFacility.SpaceplaneHangar);
-                    if (KSC.SPHUpgrades.Count > index)
-                        upgrades = KSC.SPHUpgrades[index];
-                }
+                upgrades = KSC.VABUpgrades[index];
+                upgrades += KSC.SPHUpgrades.Count > index ? KSC.SPHUpgrades[index] : 0;
             }
             upgrades += upgradeDelta;
             variables.Add("L", level.ToString());

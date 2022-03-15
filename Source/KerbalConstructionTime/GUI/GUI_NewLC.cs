2a66fce2f01d50fe9bb454c365f8de80e5334789
NathanKell <nathankell@gmail.com>
2022-03-15 00:20:18 -0700

    Support modifying LCs.

--- Source/KerbalConstructionTime/GUI/GUI_NewLC.cs
+++ Source/KerbalConstructionTime/GUI/GUI_NewLC.cs
@@ -15,65 +15,83 @@ namespace KerbalConstructionTime
 
         public static void GetPadStats(float tonnageLimit, Vector3 padSize, out float minTonnage, out double curPadCost, out double curVABCost, out float fractionalPadLvl)
         {
+            fractionalPadLvl = 0f;
             if (tonnageLimit > 0f)
             {
                 minTonnage = Mathf.Floor(tonnageLimit * 0.5f);
                 if (minTonnage < 10f)
                     minTonnage = 0f;
-            }
-            else
-            {
-                minTonnage = 0f;
-            }
 
-            double mass = tonnageLimit;
-            curPadCost = Math.Pow(mass, 0.5d) * 2500d + Math.Pow(mass, 1.5d) * 1.5d;
-            curVABCost = Math.Pow(padSize.magnitude, 2.5d) * 2d;
-            fractionalPadLvl = 0f;
+                double mass = tonnageLimit;
+                curPadCost = Math.Pow(mass, 0.5d) * 2500d + Math.Pow(mass, 1.5d) * 1.5d;
 
-            if (_padLvlOptions == null)
-            {
-                LoadPadData();
-            }
-
-            if (_padLvlOptions == null)
-            {
-                fractionalPadLvl = 0f;
-            }
-            else
-            {
-                float unlimitedTonnageThreshold = 3500f;
-
-                if (tonnageLimit >= unlimitedTonnageThreshold)
+                if (_padLvlOptions == null)
                 {
-                    int padLvl = _padLvlOptions.Length - 2;
-                    fractionalPadLvl = padLvl;
+                    LoadPadData();
+                }
+
+                if (_padLvlOptions == null)
+                {
+                    fractionalPadLvl = 0f;
                 }
                 else
                 {
-                    for (int i = 1; i < _padTons.Length; i++)
-                    {
-                        if (tonnageLimit < _padTons[i])
-                        {
-                            float lowerBound = _padTons[i - 1];
-                            float upperBound = Math.Min(_padTons[i], unlimitedTonnageThreshold);
-                            float fractionOverFullLvl = (tonnageLimit - lowerBound) / (upperBound - lowerBound);
-                            fractionalPadLvl = (i - 1) + fractionOverFullLvl;
+                    float unlimitedTonnageThreshold = 3500f;
 
-                            break;
+                    if (tonnageLimit >= unlimitedTonnageThreshold)
+                    {
+                        int padLvl = _padLvlOptions.Length - 2;
+                        fractionalPadLvl = padLvl;
+                    }
+                    else
+                    {
+                        for (int i = 1; i < _padTons.Length; i++)
+                        {
+                            if (tonnageLimit < _padTons[i])
+                            {
+                                float lowerBound = _padTons[i - 1];
+                                float upperBound = Math.Min(_padTons[i], unlimitedTonnageThreshold);
+                                float fractionOverFullLvl = (tonnageLimit - lowerBound) / (upperBound - lowerBound);
+                                fractionalPadLvl = (i - 1) + fractionOverFullLvl;
+
+                                break;
+                            }
                         }
                     }
                 }
             }
+            else
+            {
+                // SPH case
+                minTonnage = 0f;
+                curPadCost = 0f;
+                padSize.y *= 5f;
+            }
+            curVABCost = Math.Pow(padSize.magnitude, 2.5d) * 2d;
         }
 
         public static void DrawNewLCWindow(int windowID)
         {
-            GUILayout.BeginVertical();
-            GUILayout.Label("Name:");
-            _newName = GUILayout.TextField(_newName);
+            LCItem activeLC = KCTGameStates.ActiveKSC.ActiveLaunchComplexInstance;
+            double oldVABCost = 0, oldPadCost = 0, lpMult = 1;
 
-            GUILayout.Label("Launch Complex Info:");
+            GUILayout.BeginVertical();
+            if (GUIStates.ShowModifyLC)
+            {
+                GUILayout.Label(activeLC.Name);
+                GUILayout.Label($"Tonnage limits: {activeLC.SupportedMassAsPrettyText}");
+                GUILayout.Label($"Size limits: {activeLC.SupportedSizeAsPrettyText}");
+                                
+                GetPadStats(activeLC.massMax, activeLC.sizeMax, out _, out oldPadCost, out oldVABCost, out _);
+                lpMult = activeLC.LaunchPads.Count;
+            }
+            else
+            {
+                GUILayout.Label("Name:");
+                _newName = GUILayout.TextField(_newName);
+            }
+
+            GUILayout.Label(GUIStates.ShowModifyLC ? "New Limits" : "Launch Complex Limits:");
 
             double curPadCost = 0;
             double curVABCost = 0;
@@ -84,59 +102,86 @@ namespace KerbalConstructionTime
             float minTonnage = 0f;
             Vector3 curPadSize = Vector3.zero;
 
-            GUILayout.Label("Maximum tonnage:");
-            _tonnageLimit = GUILayout.TextField(_tonnageLimit);
-            if (float.TryParse(_tonnageLimit, out tonnageLimit) && float.TryParse(_heightLimit, out heightLimit) && float.TryParse(_widthLimit, out widthLimit))
+            bool hasTonnage = !GUIStates.ShowModifyLC || activeLC.isPad;
+            if (hasTonnage)
+            {
+                GUILayout.Label("Maximum tonnage:");
+                _tonnageLimit = GUILayout.TextField(_tonnageLimit);
+            }
+            if ((!hasTonnage || float.TryParse(_tonnageLimit, out tonnageLimit)) && float.TryParse(_heightLimit, out heightLimit) && float.TryParse(_widthLimit, out widthLimit))
             {
                 curPadSize.x = curPadSize.z = widthLimit;
                 curPadSize.y = heightLimit;
                 GetPadStats(tonnageLimit, new Vector3(widthLimit, heightLimit, widthLimit), out minTonnage, out curPadCost, out curVABCost, out fractionalPadLvl);
             }
-            GUILayout.Label($"Minimum tonnage: {minTonnage:N}");
+            if (hasTonnage)
+                GUILayout.Label($"Minimum tonnage: {minTonnage:N}");
+            else
+                tonnageLimit = -1f;
+
             GUILayout.Label("Size Limits:");
-            _heightLimit = GUILayout.TextField(_heightLimit);
             _widthLimit = GUILayout.TextField(_widthLimit);
+            _heightLimit = GUILayout.TextField(_heightLimit);
 
-            double curLCCost = curPadCost + curVABCost;
+            double totalCost = Math.Abs(curVABCost - oldVABCost) + Math.Abs(curPadCost - oldPadCost) * lpMult;
 
-            if (curLCCost > 0)
+            if (totalCost > 0)
             {
-                double curPadBuildTime = FacilityUpgrade.CalculateBuildTime(curLCCost, SpaceCenterFacility.LaunchPad);
+                double curPadBuildTime = FacilityUpgrade.CalculateBuildTime(totalCost, SpaceCenterFacility.LaunchPad);
                 string sBuildTime = KSPUtil.PrintDateDelta(curPadBuildTime, includeTime: false);
-                GUILayout.Label($"It will cost {Math.Round(curLCCost):N} funds to build the new launch complex, and additional pads there will cost {Math.Round(curPadCost):N}. " +
-                                $"Estimated construction time is {sBuildTime}.");
+                string costString;
+                if (GUIStates.ShowModifyLC)
+                {
+                    costString = $"It will cost {Math.Round(totalCost):N0} funds to renovate {activeLC.Name}.";
+                    if (activeLC.isPad)
+                        costString += $" Additional pads there will now cost {Math.Round(curPadCost):N}.";
+                }
+                else
+                {
+                    costString = $"It will cost {Math.Round(totalCost):N0} funds to build the new launch complex, and additional pads there will cost {Math.Round(curPadCost):N0}.";
+                }
+                GUILayout.Label(costString + $" Estimated construction time is {sBuildTime}.");
             }
 
-            GUILayout.Label("Would you like to build it?");
+            GUILayout.Label("Would you like to " + (GUIStates.ShowModifyLC ? "renovate it?" : "build it?"));
             GUILayout.BeginHorizontal();
-            if (GUILayout.Button("Yes") && ValidateLCCreationParameters(_newName, fractionalPadLvl, tonnageLimit, curPadSize))
+            if (GUILayout.Button("Yes") && ValidateLCCreationParameters(_newName, fractionalPadLvl, tonnageLimit, curPadSize, GUIStates.ShowModifyLC))
             {
 
-                GUIStates.ShowNewLC = false;
-                _centralWindowPosition.height = 1;
-                _centralWindowPosition.width = 150;
-                _centralWindowPosition.x = (Screen.width - 150) / 2;
-                GUIStates.ShowBuildList = true;
-
+                string lcName = GUIStates.ShowModifyLC ? activeLC.Name : _newName;
                 if (!Utilities.CurrentGameIsCareer())
                 {
-                    KCTDebug.Log("Building new launch complex!");
-                    KCTGameStates.ActiveKSC.LaunchComplexes.Add(new LCItem(_newName, tonnageLimit, curPadSize, true, KCTGameStates.ActiveKSC));
+                    KCTDebug.Log($"Building/Modifying launch complex {lcName}");
+                    if (GUIStates.ShowModifyLC)
+                        activeLC.Modify(tonnageLimit, curPadSize);
+                    else
+                        KCTGameStates.ActiveKSC.LaunchComplexes.Add(new LCItem(_newName, tonnageLimit, curPadSize, true, KCTGameStates.ActiveKSC));
                 }
-                else if (Funding.CanAfford((float)curLCCost))
+                else if (Funding.CanAfford((float)totalCost))
                 {
-                    KCTDebug.Log("Building new launch complex!");
-                    Utilities.SpendFunds(curLCCost, TransactionReasons.StructureConstruction);
-                    var lc = new LCItem(_newName, tonnageLimit, curPadSize, true, KCTGameStates.ActiveKSC);
-                    KCTGameStates.ActiveKSC.LaunchComplexes.Add(lc);
+                    KCTDebug.Log($"Building/Modifying launch complex {lcName}");
+                    Utilities.SpendFunds(totalCost, TransactionReasons.StructureConstruction);
+                    LCItem lc;
+                    if (GUIStates.ShowModifyLC)
+                    {
+                        lc = activeLC;
+                        activeLC.Modify(tonnageLimit, curPadSize);
+                        KCTGameStates.ActiveKSC.SwitchToPrevLaunchComplex();
+                    }
+                    else
+                    {
+                        lc = new LCItem(_newName, tonnageLimit, curPadSize, true, KCTGameStates.ActiveKSC);
+                        KCTGameStates.ActiveKSC.LaunchComplexes.Add(lc);
+                    }
+                    lc.isOperational = false;
 
                     var lcConstr = new LCConstruction
                     {
-                        LaunchComplexIndex = KCTGameStates.ActiveKSC.LaunchComplexes.Count - 1,
-                        Cost = curLCCost,
-                        Name = _newName
+                        LaunchComplexIndex = KCTGameStates.ActiveKSC.LaunchComplexes.IndexOf(lc),
+                        Cost = totalCost,
+                        Name = lcName
                     };
-                    lcConstr.SetBP(curLCCost);
+                    lcConstr.SetBP(totalCost);
                     KCTGameStates.ActiveKSC.LCConstructions.Add(lcConstr);
 
                     try
@@ -150,8 +195,14 @@ namespace KerbalConstructionTime
                 }
                 else
                 {
-                    ScreenMessages.PostScreenMessage("Not enough funds to build this launch complex.");
+                    ScreenMessages.PostScreenMessage("Not enough funds to " + (GUIStates.ShowModifyLC ? "renovate" : "build") + " this launch complex.");
                 }
+                GUIStates.ShowNewLC = false;
+                GUIStates.ShowModifyLC = false;
+                _centralWindowPosition.height = 1;
+                _centralWindowPosition.width = 150;
+                _centralWindowPosition.x = (Screen.width - 150) / 2;
+                GUIStates.ShowBuildList = true;
 
                 _padLvlOptions = null;
             }
@@ -163,6 +214,7 @@ namespace KerbalConstructionTime
                 _centralWindowPosition.x = (Screen.width - 150) / 2;
                 _padLvlOptions = null;
                 GUIStates.ShowNewLC = false;
+                GUIStates.ShowModifyLC = false;
                 GUIStates.ShowBuildList = true;
             }
 
@@ -197,17 +249,26 @@ namespace KerbalConstructionTime
             }
         }
 
-        private static bool ValidateLCCreationParameters(string newName, float fractionalPadLvl, float tonnageLimit, Vector3 curPadSize)
+        private static bool ValidateLCCreationParameters(string newName, float fractionalPadLvl, float tonnageLimit, Vector3 curPadSize, bool modify)
         {
-            if (fractionalPadLvl == -1 || tonnageLimit == 0 || curPadSize == Vector3.zero)
+            if (curPadSize == Vector3.zero)
             {
-                ScreenMessages.PostScreenMessage("Please enter a valid pad size");
+                ScreenMessages.PostScreenMessage("Please enter a valid size");
+                return false;
+            }
+
+            if (modify)
+                return true;
+
+            if(fractionalPadLvl == -1 || tonnageLimit == 0)
+            {
+                ScreenMessages.PostScreenMessage("Please enter a valid tonnage limit");
                 return false;
             }
 
             if (string.IsNullOrEmpty(_newName))
             {
-                ScreenMessages.PostScreenMessage("Enter a name for the new launchpad");
+                ScreenMessages.PostScreenMessage("Enter a name for the new launch complex");
                 return false;
             }
 

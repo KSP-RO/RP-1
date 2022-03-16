86b4ad958552549c3c2084fcd4b1ec3c66722fb3
NathanKell <nathankell@gmail.com>
2022-03-15 22:23:35 -0700

    Allow dismantling LCs. Fix KSC building upgrade popups. Rename KCT in strings to other things (Integration Info, Space Center Management, etc).

--- Source/KerbalConstructionTime/GUI/GUI_DismantlePad.cs
+++ Source/KerbalConstructionTime/GUI/GUI_DismantlePad.cs
@@ -6,21 +6,48 @@ namespace KerbalConstructionTime
     {
         public static void DrawDismantlePadWindow(int windowID)
         {
+            LCItem activeLC = KCTGameStates.ActiveKSC.ActiveLaunchComplexInstance;
+            bool isLC = GUIStates.ShowDismantleLC;
             GUILayout.BeginVertical();
-            GUILayout.Label("Are you sure you want to dismantle the currently selected launch pad? This cannot be undone!");
+            GUILayout.Label("Are you sure you want to dismantle the currently selected " + 
+                (isLC ? $"launch complex, {activeLC.Name}" : $"launch pad, {activeLC.ActiveLPInstance.name}") + "? This cannot be undone!");
             GUILayout.BeginHorizontal();
 
             if (GUILayout.Button("Yes"))
             {
-                if (KCTGameStates.ActiveKSC.ActiveLaunchComplexInstance.LaunchPadCount < 2) return;
-
-                KCT_LaunchPad lpToDel = KCTGameStates.ActiveKSC.ActiveLaunchComplexInstance.ActiveLPInstance;
-                if (!lpToDel.Delete(out string err))
+                if (isLC)
                 {
-                    ScreenMessages.PostScreenMessage("Dismantle failed: " + err, 5f, ScreenMessageStyle.UPPER_CENTER);
-                }
+                    if (!activeLC.isPad)
+                    {
+                        ScreenMessages.PostScreenMessage("Dismantle failed: can't dismantle the Hangar", 5f, ScreenMessageStyle.UPPER_CENTER);
+                        return;
+                    }
+                    if (!activeLC.CanModify)
+                    {
+                        ScreenMessages.PostScreenMessage("Dismantle failed: Launch Complex in use", 5f, ScreenMessageStyle.UPPER_CENTER);
+                        return;
+                    }
+                    KSCItem ksc = activeLC.KSC;
+                    ksc.SwitchToPrevLaunchComplex();
 
+                    for (int i = activeLC.Warehouse.Count; i-- > 0;)
+                    {
+                        Utilities.ScrapVessel(activeLC.Warehouse[i]);
+                    }
+                    ksc.LaunchComplexes.Remove(activeLC);
+                }
+                else
+                {
+                    if (activeLC.LaunchPadCount < 2) return;
+
+                    KCT_LaunchPad lpToDel = activeLC.ActiveLPInstance;
+                    if (!lpToDel.Delete(out string err))
+                    {
+                        ScreenMessages.PostScreenMessage("Dismantle failed: " + err, 5f, ScreenMessageStyle.UPPER_CENTER);
+                    }
+                }
                 GUIStates.ShowDismantlePad = false;
+                GUIStates.ShowDismantleLC = false;
                 GUIStates.ShowBuildList = true;
             }
 
@@ -29,6 +56,7 @@ namespace KerbalConstructionTime
                 _centralWindowPosition.width = 150;
                 _centralWindowPosition.x = (Screen.width - 150) / 2;
                 GUIStates.ShowDismantlePad = false;
+                GUIStates.ShowDismantleLC = false;
                 GUIStates.ShowBuildList = true;
             }
 

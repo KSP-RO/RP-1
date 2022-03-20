e0b357b46b24c54dc90f11eb0dd9a8fec478874a
NathanKell <nathankell@gmail.com>
2022-03-20 03:12:59 -0700

    Implement better numbers and better capping of build rates.

--- Source/KerbalConstructionTime/BuildItems/AirlaunchPrep.cs
+++ Source/KerbalConstructionTime/BuildItems/AirlaunchPrep.cs
@@ -6,7 +6,7 @@ namespace KerbalConstructionTime
     public class AirlaunchPrep : IKCTBuildItem
     {
         public string Name => Direction == PrepDirection.Mount ? Name_Mount : Name_Unmount;
-        public double BP = 0, Progress = 0, Cost = 0, Mass = 0;
+        public double BP = 0, Progress = 0, Cost = 0, Mass = 0, VesselBP;
         public string AssociatedID = string.Empty;
 
         public const string Name_Mount = "Mounting to carrier";
@@ -36,6 +36,7 @@ namespace KerbalConstructionTime
             BP = 0;
             Cost = 0;
             Mass = 0;
+            VesselBP = 0;
             Direction = PrepDirection.Mount;
             AssociatedID = string.Empty;
         }
@@ -48,12 +49,13 @@ namespace KerbalConstructionTime
 
             BP = MathParser.ParseAirlaunchTimeFormula(vessel);
             Cost = MathParser.ParseAirlaunchCostFormula(vessel);
-            Mass = Math.Max(0.001d, vessel.GetTotalMass());
+            Mass = vessel.GetTotalMass();
+            VesselBP = vessel.BuildPoints;
         }
 
         public double GetBuildRate()
         {
-            double buildRate = Math.Min(Utilities.GetBuildRate(0, LC), Mass * PresetManager.Instance.ActivePreset.GeneralSettings.MaxBuildRatePerTon);
+            double buildRate = Math.Min(Utilities.GetBuildRate(0, LC), Utilities.GetBuildRateCap(VesselBP, Mass, LC));
 
             if (Direction == PrepDirection.Unmount)
                 buildRate *= -1;

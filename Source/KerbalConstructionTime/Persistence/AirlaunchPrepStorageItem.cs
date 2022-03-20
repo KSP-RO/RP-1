dcc4438fa0f137ba51548854a31569d1d37104a8
NathanKell <nathankell@gmail.com>
2022-03-20 03:12:59 -0700

    Implement better numbers and better capping of build rates.

--- Source/KerbalConstructionTime/Persistence/AirlaunchPrepStorageItem.cs
+++ Source/KerbalConstructionTime/Persistence/AirlaunchPrepStorageItem.cs
@@ -3,7 +3,7 @@
     public class AirlaunchPrepStorageItem
     {
         [Persistent] private string name = "";
-        [Persistent] public double BP = 0, progress = 0, cost = 0, mass = 0;
+        [Persistent] public double BP = 0, progress = 0, cost = 0, mass = 0, vesselBP = 0;
         [Persistent] public string associatedID = "";
 
         public AirlaunchPrep ToAirlaunchPrep()
@@ -15,7 +15,8 @@
                 Cost = cost,
                 Direction = name == AirlaunchPrep.Name_Mount ? AirlaunchPrep.PrepDirection.Mount : AirlaunchPrep.PrepDirection.Unmount,
                 AssociatedID = associatedID,
-                Mass = mass
+                Mass = mass,
+                VesselBP = vesselBP
             };
             return ret;
         }
@@ -28,6 +29,7 @@
             cost = ap.Cost;
             associatedID = ap.AssociatedID;
             mass = ap.Mass;
+            vesselBP = ap.VesselBP;
             return this;
         }
     }

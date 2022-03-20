36e15de1af77b348d612d18b2f66cf2e453e1939
NathanKell <nathankell@gmail.com>
2022-03-19 19:27:46 -0700

    Add a separate ConstructionRate formula. Fix ambiguity re: buildTime vs buildPoints internally. Update FirstRun GUI.

--- Source/KerbalConstructionTime/Presets/PresetManager.cs
+++ Source/KerbalConstructionTime/Presets/PresetManager.cs
@@ -332,6 +332,7 @@ namespace KerbalConstructionTime
             KSCUpgradeFormula = "([C]^(1/2))*1000*[O]",
             ReconditioningFormula = "min([M]*[O]*[E], [X])*abs([RE]-[S])",
             BuildRateFormula = "(([I]+1)*0.05*[N] + max(0.1-[I], 0))*sign(2*[L]-[I]+1)",
+            ConstructionRateFormula = "([N]*0.005)^0.625",
             InventorySaleFormula = "([V]+[P] / 10000)^(0.5)",    //Gives the TOTAL amount of points, decimals are kept //[V] = inventory value in funds, [P] = Value of all previous sales combined
             IntegrationTimeFormula = "0",    //[M]=Vessel loaded mass, [m]=vessel empty mass, [C]=vessel loaded cost, [c]=vessel empty cost, [BP]=vessel BPs, [E]=editor level, [L]=launch site level (pad), [VAB]=1 if VAB craft, 0 if SPH
             RolloutCostFormula = "0",    //[M]=Vessel loaded mass, [m]=vessel empty mass, [C]=vessel loaded cost, [c]=vessel empty cost, [BP]=vessel BPs, [E]=editor level, [L]=launch site level (pad), [VAB]=1 if VAB craft, 0 if SPH

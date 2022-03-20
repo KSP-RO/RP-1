2369104120da03cab4deded15044e0eca87276c5
NathanKell <nathankell@gmail.com>
2022-03-20 15:45:18 -0700

    Actually recalculate build rate on complete

--- Source/KerbalConstructionTime/BuildItems/FacilityUpgrade.cs
+++ Source/KerbalConstructionTime/BuildItems/FacilityUpgrade.cs
@@ -160,7 +160,7 @@ namespace KerbalConstructionTime
         public void IncrementProgress(double UTDiff)
         {
             if (!IsComplete()) AddProgress(GetBuildRate() * UTDiff);
-            if (HighLogic.LoadedScene == GameScenes.SPACECENTER && (IsComplete() || !PresetManager.Instance.ActivePreset.GeneralSettings.KSCUpgradeTimes))
+            if (IsComplete() || !PresetManager.Instance.ActivePreset.GeneralSettings.KSCUpgradeTimes)
             {
                 if (ScenarioUpgradeableFacilities.Instance != null && !KCTGameStates.ErroredDuringOnLoad)
                 {

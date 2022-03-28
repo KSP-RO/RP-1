f760713a39d97e0d8f10cfa476a62938c0356be7
siimav <siim.aaver@gmail.com>
2022-03-29 01:38:44 +0300

    Implement support for Programs

--- Source/HarmonyPatcher.cs
+++ Source/HarmonyPatcher.cs
@@ -5,9 +5,9 @@ using KSP.UI.Screens.Editor;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
-using System.Reflection;
-using System.Reflection.Emit;
+using UnityEngine.UI;
 
 namespace RP0
 {
@@ -135,199 +135,49 @@ namespace RP0
                 }
             }
         }
-    
-        [HarmonyPatch(typeof(GameSettings))]
-        internal class PatchGameSettings
+
+        [HarmonyPatch(typeof(ReputationWidget))]
+        internal class PatchReputationWidget
         {
-            internal static bool DVSet = false;
-            internal static bool DELTAV_APP_ENABLED = true;
-            internal static bool DELTAV_CALCULATIONS_ENABLED = true;
-            public static void FixDV()
-            {
-                if (DVSet)
-                    return;
-
-                DVSet = true;
-
-                DELTAV_APP_ENABLED = GameSettings.DELTAV_APP_ENABLED;
-                DELTAV_CALCULATIONS_ENABLED = GameSettings.DELTAV_CALCULATIONS_ENABLED;
-
-                GameSettings.DELTAV_APP_ENABLED = false;
-                GameSettings.DELTAV_CALCULATIONS_ENABLED = false;
-            }
-
-            internal static System.Type EEXType = AccessTools.TypeByName("EditorExtensionsRedux.EditorExtensions");
-            internal static PropertyInfo EEXInstance = EEXType?.GetProperty("Instance", AccessTools.all);
-            internal static FieldInfo HotkeyEditor_toggleSymModePrimary = EEXType?.GetField("HotkeyEditor_toggleSymModePrimary", AccessTools.all);
-            internal static FieldInfo HotkeyEditor_toggleSymModeSecondary = EEXType?.GetField("HotkeyEditor_toggleSymModeSecondary", AccessTools.all);
-            internal static FieldInfo HotkeyEditor_toggleAngleSnapPrimary = EEXType?.GetField("HotkeyEditor_toggleAngleSnapPrimary", AccessTools.all);
-            internal static FieldInfo HotkeyEditor_toggleAngleSnapSecondary = EEXType?.GetField("HotkeyEditor_toggleAngleSnapSecondary", AccessTools.all);
-
+            public static TextMeshProUGUI RepLabel;
 
             [HarmonyPrefix]
-            [HarmonyPatch("WriteCfg")]
-            internal static void Prefix_WriteCfg()
+            [HarmonyPatch("onReputationChanged")]
+            internal static bool Prefix_onReputationChanged(float rep, TransactionReasons reason)
             {
-                if (EEXType != null)
+                if (RepLabel != null)
                 {
-                    Debug.Log("Temporarily changing GameSettings editor keybindings to work around an Editor Extensions Redux issue.");
-                    object eex = EEXInstance.GetValue(null);
-                    if (eex != null)
-                    {
-                        PatchEditorExtensionsRedux_Start.toggleSym.primary = HotkeyEditor_toggleSymModePrimary.GetValue(eex) as KeyCodeExtended;
-                        PatchEditorExtensionsRedux_Start.toggleSym.secondary = HotkeyEditor_toggleSymModeSecondary.GetValue(eex) as KeyCodeExtended;
-                        PatchEditorExtensionsRedux_Start.toggleSnap.primary = HotkeyEditor_toggleAngleSnapPrimary.GetValue(eex) as KeyCodeExtended;
-                        PatchEditorExtensionsRedux_Start.toggleSnap.secondary = HotkeyEditor_toggleAngleSnapSecondary.GetValue(eex) as KeyCodeExtended;
-                    }
-
-                    GameSettings.Editor_toggleSymMode.primary = PatchEditorExtensionsRedux_Start.toggleSym.primary;
-                    GameSettings.Editor_toggleSymMode.secondary = PatchEditorExtensionsRedux_Start.toggleSym.secondary;
-                    GameSettings.Editor_toggleAngleSnap.primary = PatchEditorExtensionsRedux_Start.toggleSnap.primary;
-                    GameSettings.Editor_toggleAngleSnap.secondary = PatchEditorExtensionsRedux_Start.toggleSnap.secondary;
-                }
-                
-                FixDV();
-                
-                GameSettings.DELTAV_APP_ENABLED = DELTAV_APP_ENABLED;
-                GameSettings.DELTAV_CALCULATIONS_ENABLED = DELTAV_CALCULATIONS_ENABLED;
-            }
-
-            [HarmonyPostfix]
-            [HarmonyPatch("WriteCfg")]
-            internal static void Postfix_WriteCfg()
-            {
-                if (EEXType != null)
-                {
-                    Debug.Log("Settings returned.");
-                    GameSettings.Editor_toggleSymMode.primary = new KeyCodeExtended(KeyCode.None);
-                    GameSettings.Editor_toggleSymMode.secondary = new KeyCodeExtended(KeyCode.None);
-                    GameSettings.Editor_toggleAngleSnap.primary = new KeyCodeExtended(KeyCode.None);
-                    GameSettings.Editor_toggleAngleSnap.secondary = new KeyCodeExtended(KeyCode.None);
+                    RepLabel.text = KSPUtil.LocalizeNumber(rep, "0.0");
                 }
 
-                GameSettings.DELTAV_APP_ENABLED = false;
-                GameSettings.DELTAV_CALCULATIONS_ENABLED = false;
+                return false;
             }
-
-            [HarmonyPostfix]
-            [HarmonyPatch("ParseCfg")]
-            internal static void Postfix_ParseCfg()
-            {
-                FixDV();
-            }
-
-            [HarmonyPostfix]
-            [HarmonyPatch("SetDefaultValues")]
-            internal static void Postfix_SetDefaultValues()
-            {
-                FixDV();
-
-                if (EEXType != null)
-                {
-                    PatchEditorExtensionsRedux_Start.toggleSym.primary = GameSettings.Editor_toggleSymMode.primary;
-                    PatchEditorExtensionsRedux_Start.toggleSym.secondary = GameSettings.Editor_toggleSymMode.secondary;
-
-                    PatchEditorExtensionsRedux_Start.toggleSnap.primary = GameSettings.Editor_toggleAngleSnap.primary;
-                    PatchEditorExtensionsRedux_Start.toggleSnap.secondary = GameSettings.Editor_toggleAngleSnap.secondary;
-
-                    GameSettings.Editor_toggleSymMode.primary = new KeyCodeExtended(KeyCode.None);
-                    GameSettings.Editor_toggleSymMode.secondary = new KeyCodeExtended(KeyCode.None);
-                    GameSettings.Editor_toggleAngleSnap.primary = new KeyCodeExtended(KeyCode.None);
-                    GameSettings.Editor_toggleAngleSnap.secondary = new KeyCodeExtended(KeyCode.None);
-                }
-            }
-        }
-        
-        [HarmonyPatch]
-        internal class PatchEditorExtensionsRedux_Start
-        {
-            static MethodBase TargetMethod() => AccessTools.TypeByName("EditorExtensionsRedux.EditorExtensions")?.GetMethod("Start", AccessTools.all);
-
-            internal static System.Type EEXType = AccessTools.TypeByName("EditorExtensionsRedux.EditorExtensions");
-
-            [HarmonyPrepare]
-            internal static bool Prepare()
-            {
-                return EEXType != null;
-            }
-
-            internal static KeyBinding toggleSym = new KeyBinding(KeyCode.None);
-            internal static KeyBinding toggleSnap = new KeyBinding(KeyCode.None);
-
 
             [HarmonyPrefix]
-            internal static void Prefix_Start()
+            [HarmonyPatch("DelayedStart")]
+            internal static bool Prefix_DelayedStart(ReputationWidget __instance)
             {
-                if (EEXType != null)
-                {
-                    toggleSym.primary = GameSettings.Editor_toggleSymMode.primary;
-                    toggleSym.secondary = GameSettings.Editor_toggleSymMode.secondary;
+                DestroyImmediate(__instance.gauge);
+                __instance.gauge = null;
+                DestroyImmediate(__instance.gameObject.transform.Find("circularGauge").gameObject);
 
-                    toggleSnap.primary = GameSettings.Editor_toggleAngleSnap.primary;
-                    toggleSnap.secondary = GameSettings.Editor_toggleAngleSnap.secondary;
-                }
-            }
-        }
+                var frameImage = (Image)__instance.gameObject.GetComponentInChildren(typeof(Image));
 
-        /// <summary>
-        /// Prevents EVAs being added to flight log when they happen at homeworld while situation < orbit.
-        /// </summary>
-        [HarmonyPatch(typeof(FlightEVA))]
-        internal class PatchFlightEVA
-        {
-            [HarmonyTranspiler]
-            [HarmonyPatch("onGoForEVA")]
-            internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
-            {
-                int startIndex = -1;
-                int endIndex = -1;
+                var img = Instantiate(new GameObject("repBackground"), __instance.transform, worldPositionStays: false).AddComponent<Image>();
+                img.color = new Color32(58, 58, 63, 255);
+                img.rectTransform.anchorMin = frameImage.rectTransform.anchorMin;
+                img.rectTransform.anchorMax = frameImage.rectTransform.anchorMax;
+                img.rectTransform.anchoredPosition = frameImage.rectTransform.anchoredPosition;
+                img.rectTransform.sizeDelta = frameImage.rectTransform.sizeDelta;
 
-                var codes = new List<CodeInstruction>(instructions);
+                RepLabel = Instantiate(new GameObject("repLabel"), __instance.transform, worldPositionStays: false).AddComponent<TextMeshProUGUI>();
+                RepLabel.alignment = TextAlignmentOptions.Right;
+                RepLabel.color = XKCDColors.Mustard;
+                RepLabel.fontSize = 22;
+                RepLabel.rectTransform.localPosition = new Vector3(-9, -1, 0);
+                RepLabel.fontStyle = FontStyles.Bold;
 
-                // Finds the start and end index of the following piece of code:
-                // this.pCrew.flightLog.AddEntryUnique(FlightLog.EntryType.ExitVessel, this.fromPart.vessel.orbit.referenceBody.name);
-                for (int i = 0; i < codes.Count; i++)
-                {
-                    if (codes[i].opcode == OpCodes.Callvirt &&
-                        codes[i].operand.ToString() == "Void AddEntryUnique(EntryType, System.String)")
-                    {
-                        endIndex = i;
-
-                        for (int j = i - 1; j >= 0; j--)
-                        {
-                            if (codes[j].opcode == OpCodes.Ldfld &&
-                                codes[j].operand.ToString() == "ProtoCrewMember pCrew")
-                            {
-                                startIndex = j - 1;    // include 1 more opcode (ldarg.0 / this) before the current one
-                                break;
-                            }
-                        }
-
-                        break;
-                    }
-                }
-
-                if (startIndex > -1 && endIndex > -1)
-                {
-                    // Remove everything after the ldarg.0 call since this has a tag
-                    codes.RemoveRange(startIndex + 1, endIndex - startIndex);
-                    CodeInstruction call = CodeInstruction.Call(typeof(PatchFlightEVA), "AddEntryUniquePatched");
-                    codes.Insert(startIndex + 1, call);
-                }
-
-                return codes.AsEnumerable();
-            }
-
-            public static void AddEntryUniquePatched(FlightEVA instance)
-            {
-                var fromPart = (Part)AccessTools.Field(typeof(FlightEVA), "fromPart").GetValue(instance);
-                Vessel v = fromPart.vessel;
-                if (!v.orbit.referenceBody.isHomeWorld ||
-                    (v.orbit.referenceBody.isHomeWorld && v.situation > Vessel.Situations.SUB_ORBITAL))
-                {
-                    var pCrew = (ProtoCrewMember)AccessTools.Field(typeof(FlightEVA), "pCrew").GetValue(instance);
-                    pCrew.flightLog.AddEntryUnique(FlightLog.EntryType.ExitVessel, v.orbit.referenceBody.name);
-                }
+                return true;
             }
         }
     }

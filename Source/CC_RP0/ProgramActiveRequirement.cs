77533ee8c9bd8baa83109b14ff9e1e88ad34cc27
siimav <siim.aaver@gmail.com>
2022-03-29 01:38:44 +0300

    Implement support for Programs

---
+++ Source/CC_RP0/ProgramActiveRequirement.cs
@@ -0,0 +1,54 @@
+﻿using RP0.Programs;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ContractConfigurator.RP0
+{
+    public class ProgramActiveRequirement : ContractRequirement
+    {
+        protected List<string> programs;
+
+        public override bool LoadFromConfig(ConfigNode configNode)
+        {
+            // Load base class
+            bool valid = base.LoadFromConfig(configNode);
+
+            // Check on active contracts too
+            checkOnActiveContract = configNode.HasValue("checkOnActiveContract") ? checkOnActiveContract : true;
+
+            valid &= ConfigNodeUtil.ParseValue<List<string>>(configNode, "program", x => programs = x, this, new List<string>());
+
+            return valid;
+        }
+
+        public override void OnLoad(ConfigNode configNode)
+        {
+            programs = ConfigNodeUtil.ParseValue<List<string>>(configNode, "program", new List<string>());
+        }
+
+        public override void OnSave(ConfigNode configNode)
+        {
+            foreach (string p in programs)
+            {
+                configNode.AddValue("program", p);
+            }
+        }
+
+        public override bool RequirementMet(ConfiguredContract contract)
+        {
+            return programs.All(p => ProgramHandler.Instance.ActivePrograms.Any(p2 => p2.name == p));
+        }
+
+        protected override string RequirementText()
+        {
+            if (invertRequirement)
+            {
+                return $"Must NOT have active program{(programs.Count > 1 ? "s" : "")}: {string.Join(", ", programs)}";
+            }
+            else
+            {
+                return $"Have active program{(programs.Count > 1 ? "s" : "")}: {string.Join(", ", programs)}";
+            }
+        }
+    }
+}
---
+++ Source/Programs/PredicateBuilder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace RP0.Programs
+{
+    public static class PredicateBuilder
+    {
+        public static Expression<Func<bool>> CombineExpressionsWithAnd(
+            this IEnumerable<Expression<Func<bool>>> expressions)
+        {
+            if (expressions == null || expressions.Count() == 0)
+            {
+                return () => true;
+            }
+            return expressions.Aggregate((a, b) => a.And(b));
+        }
+
+        public static Expression<Func<bool>> CombineExpressionsWithOr(
+            this IEnumerable<Expression<Func<bool>>> expressions)
+        {
+            if (expressions == null || expressions.Count() == 0)
+            {
+                return () => true;
+            }
+            return expressions.Aggregate((a, b) => a.Or(b));
+        }
+
+        public static Expression<Func<bool>> Or(this Expression<Func<bool>> expr1,
+                                                     Expression<Func<bool>> expr2)
+        {
+            return Expression.Lambda<Func<bool>>(Expression.OrElse(expr1.Body, expr2.Body));
+        }
+
+        public static Expression<Func<bool>> And(this Expression<Func<bool>> expr1,
+                                                      Expression<Func<bool>> expr2)
+        {
+            return Expression.Lambda<Func<bool>>(Expression.AndAlso(expr1.Body, expr2.Body));
+        }
+
+        public static Expression Replace(this Expression expression,
+            Expression searchEx, Expression replaceEx)
+        {
+            return new ReplaceVisitor(searchEx, replaceEx).Visit(expression);
+        }
+
+        private class ReplaceVisitor : ExpressionVisitor
+        {
+            private readonly Expression _from, _to;
+
+            public ReplaceVisitor(Expression from, Expression to)
+            {
+                _from = from;
+                _to = to;
+            }
+            public override Expression Visit(Expression node)
+            {
+                return node == _from ? _to : base.Visit(node);
+            }
+        }
+    }
+}

7e2793e3dd6e1deb7880b06a90ff8ca40b338d2c
NathanKell <nathankell@gmail.com>
2022-03-31 01:17:18 -0700

    Constructions cost funds over time. They debit every full 10% of construction. Refactor all constructions (and their storages) to derive from a shared abstract classes to reduce copypasta.

--- Source/KerbalConstructionTime/BuildItems/IKCTBuildItem.cs
+++ Source/KerbalConstructionTime/BuildItems/IKCTBuildItem.cs
@@ -11,24 +11,4 @@
         bool IsComplete();
         void IncrementProgress(double UTDiff);
     }
-
-    public interface IConstructionBuildItem : IKCTBuildItem
-    {
-        int BuildListIndex { get; set; }
-        double UpdateBuildRate(int index);
-        double EstimatedTimeLeft { get; }
-        void Cancel();
-        double BuildPoints();
-        double CurrentProgress();
-    }
 }
-
-/*
-    KerbalConstructionTime (c) by Michael Marvin, Zachary Eck
-
-    KerbalConstructionTime is licensed under a
-    Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
-
-    You should have received a copy of the license along with this
-    work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
-*/

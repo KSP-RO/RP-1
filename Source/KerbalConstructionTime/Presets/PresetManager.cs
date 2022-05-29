be11db94c090c2ae2525804ed4106237f013799a
NathanKell <nathankell@gmail.com>
2022-05-28 20:20:25 -0700

    Put rush building stuff in preset cfg

--- Source/KerbalConstructionTime/Presets/PresetManager.cs
+++ Source/KerbalConstructionTime/Presets/PresetManager.cs
@@ -374,7 +374,8 @@ namespace KerbalConstructionTime
         [Persistent]
         public double EngineerStartEfficiency = 0.5, GlobalEngineerStartEfficiency = 0.5, ResearcherStartEfficiency = 0.5, 
             EngineerMaxEfficiency = 1.0, ResearcherMaxEfficiency = 1.0, GlobalEngineerMaxEfficiency = 1.0,
-            EngineerDecayRate = 0.1, GlobalEngineerDecayRate = 0.1, ResearcherDecayRate = 0.1, AdditionalPadCostMult = 0.5d;
+            EngineerDecayRate = 0.1, GlobalEngineerDecayRate = 0.1, ResearcherDecayRate = 0.1, AdditionalPadCostMult = 0.5d,
+            RushRateMult = 1.5d, RushSalaryMult = 2d, RushEfficMult = 0.985d, RushEfficMin = 0.6d;
         [Persistent]
         public FloatCurve EngineerSkillupRate = new FloatCurve();
         [Persistent]

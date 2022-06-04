f67d363a54a488224b75b7607a3b9405e4ef4f2c
siimav <siim.aaver@gmail.com>
2022-06-04 15:56:53 +0300

    Add spying to the new LC stuff

--- Source/CareerLog/CareerEvent.cs
+++ Source/CareerLog/CareerEvent.cs
@@ -19,12 +19,12 @@ namespace RP0
             Load(n);
         }
 
-        public void Load(ConfigNode node)
+        public virtual void Load(ConfigNode node)
         {
             ConfigNode.LoadObjectFromConfig(this, node);
         }
 
-        public void Save(ConfigNode node)
+        public virtual void Save(ConfigNode node)
         {
             ConfigNode.CreateConfigFromObject(this, node);
         }

15cb30f17393b19937b3d8a8fb9714d7d770e9a7
NathanKell <nathankell@gmail.com>
2022-06-11 00:05:35 -0700

    Convert LCC and PC to using guids, add back-compat.
    Remove the stackable Avionics settings (since LC is breaking, take the opportunity to do it finally, it was added for backwards compatibility.)
    
    rename KCT upgrades file
    
    Bluesky techs past Satellite give eng/rnd efficiency upgrades. Remove old purchaseable partupgrades. Automatically set partupgrade descriptions for these upgrades based on the multiplier in the KCT preset.
    
    Tweak eng/rnd tech upgrades slightly
    
    Clarify tech upgrades
    
    Also clarify in UI

--- Source/KerbalConstructionTime/Persistence/LCConstructionStorageItem.cs
+++ Source/KerbalConstructionTime/Persistence/LCConstructionStorageItem.cs
@@ -5,33 +5,47 @@ namespace KerbalConstructionTime
     public class LCConstructionStorageItem : ConstructionStorage
     {
         [Persistent]
-        public int launchComplexID = 0;
-
-        [Persistent]
         public bool isModify = false;
 
         [Persistent]
         public Guid modId;
 
         [Persistent]
+        public Guid lcID;
+
+        [Persistent]
         public LCItem.LCData lcData = new LCItem.LCData();
 
-        public LCConstruction ToLCConstruction()
+        // Back-compat: pass in the parent KSC
+        public LCConstruction ToLCConstruction(KSCItem ksc)
         {
-            var lc = new LCConstruction();
-            LoadFields(lc);
-            lc.LaunchComplexIndex = launchComplexID;
-            lc.IsModify = isModify;
-            lc.ModID = modId;
-            lc.LCData = new LCItem.LCData(lcData);
+            var lcc = new LCConstruction();
+            LoadFields(lcc);
+            if (lcID == Guid.Empty)
+            {
+                LCItem lc = ksc.LaunchComplexes.Find(l => l.Name == lcData.Name && !l.IsOperational);
+                if (lc == null)
+                {
+                    lc = ksc.LaunchComplexes.Find(l => l.MassOrig == lcData.massOrig && !l.IsOperational);
+                }
+                if (lc == null)
+                {
+                    throw new Exception($"[KCT] Failed to find LC for LCConstructionStorageItem with name {lcData.Name}");
+                }
+                lcID = lc.ID;
+            }
+            lcc.LCID = lcID;
+            lcc.IsModify = isModify;
+            lcc.ModID = modId;
 
-            return lc;
+            lcc.LCData = new LCItem.LCData(lcData);
+
+            return lcc;
         }
 
         public LCConstructionStorageItem FromLCConstruction(LCConstruction lcc)
         {
             SaveFields(lcc);
-            launchComplexID = lcc.LaunchComplexIndex;
             isModify = lcc.IsModify;
             modId = lcc.ModID;
             lcData = new LCItem.LCData(lcc.LCData);
@@ -42,12 +56,14 @@ namespace KerbalConstructionTime
         {
             base.Load(node);
             node.TryGetValue(nameof(modId), ref modId);
+            node.TryGetValue(nameof(lcID), ref lcID);
         }
 
         public override void Save(ConfigNode node)
         {
             base.Save(node);
             node.AddValue(nameof(modId), modId);
+            node.AddValue(nameof(lcID), lcID);
         }
     }
 }

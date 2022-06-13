092a1d980fd5f0ab1dcbb9501eeb832447072e2e
NathanKell <nathankell@gmail.com>
2022-06-13 14:40:13 -0700

    Patch rep in loadgame dialog, reorganize harmony patches

---
+++ Source/Harmony/ContractPatcher.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace RP0
+{
+    public partial class HarmonyPatcher : MonoBehaviour
+    {
+        [HarmonyPatch(typeof(Contracts.ContractSystem))]
+        internal class PatchContractSystem
+        {
+            [HarmonyPatch("GetContractCounts")]
+            internal static bool Prefix_GetContractCounts(Contracts.ContractSystem __instance, ref float rep, ref int avgContracts, ref int tier1, ref int tier2, ref int tier3)
+            {
+                tier1 = tier2 = tier3 = int.MaxValue;
+                return false;
+            }
+        }
+    }
+}

7941c90d48b91ad33b7b4fa3b73e965de54bb6f5
siimav <siim.aaver@gmail.com>
2022-06-15 16:45:51 +0300

    Refactor LogicOp and add support for ATLEAST. Rename AND, OR operators to ALL, ANY. Do a tabification pass on Programs.cfg.

--- Source/Programs/PredicateBuilder.cs
+++ Source/Programs/PredicateBuilder.cs
@@ -1,32 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 
 namespace RP0.Programs
 {
     public static class PredicateBuilder
     {
-        public static Expression<Func<bool>> CombineExpressionsWithAnd(
-            this IEnumerable<Expression<Func<bool>>> expressions)
-        {
-            if (expressions == null || expressions.Count() == 0)
-            {
-                return () => true;
-            }
-            return expressions.Aggregate((a, b) => a.And(b));
-        }
-
-        public static Expression<Func<bool>> CombineExpressionsWithOr(
-            this IEnumerable<Expression<Func<bool>>> expressions)
-        {
-            if (expressions == null || expressions.Count() == 0)
-            {
-                return () => true;
-            }
-            return expressions.Aggregate((a, b) => a.Or(b));
-        }
-
         public static Expression<Func<bool>> Or(this Expression<Func<bool>> expr1,
                                                      Expression<Func<bool>> expr2)
         {

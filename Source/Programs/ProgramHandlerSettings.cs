1cb7f7fd5003ebc23ff555368fde4ffa8170feaf
NathanKell <nathankell@gmail.com>
2022-06-18 23:48:59 -0700

    Use DoubleCurves instead of FloatCurves for program funding, floats don't have the precision we need. Also support overriding funding curve per-program, and make debug logging a bit better for upkeep/programs.

--- Source/Programs/ProgramHandlerSettings.cs
+++ Source/Programs/ProgramHandlerSettings.cs
@@ -1,22 +1,8 @@
 ﻿namespace RP0.Programs
 {
-    public class ProgramHandlerSettings : IConfigNode
+    public class ProgramHandlerSettings
     {
         [Persistent]
-        public FloatCurve paymentCurve = new FloatCurve();
-
-        public void Load(ConfigNode node)
-        {
-            ConfigNode.LoadObjectFromConfig(this, node);
-
-            var fc = new FloatCurve();
-            fc.Load(node.GetNode("paymentCurve"));
-            paymentCurve = fc;
-        }
-
-        public void Save(ConfigNode node)
-        {
-            ConfigNode.CreateConfigFromObject(this, node);
-        }
+        public DoubleCurve paymentCurve = new DoubleCurve();
     }
 }

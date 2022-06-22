1dcddfa3bd18d323553e29c7f09cdc12a7c682d5
NathanKell <nathankell@gmail.com>
2022-06-21 22:12:04 -0700

    Patch starting rep difficulty slider to range 0-500 (was -1000->1000)

---
+++ Source/Harmony/DifficultyPatcher.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using KSP.UI.Screens;
+using System.Reflection;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace RP0
+{
+    public partial class HarmonyPatcher : MonoBehaviour
+    {
+        [HarmonyPatch(typeof(DifficultyOptionsMenu))]
+        internal class PatchDifficultyOptionsMenu
+        {
+            [HarmonyTranspiler]
+            [HarmonyPatch("CreateDifficultWindow")]
+            internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                bool skipNext = false;
+                foreach (var instruction in instructions)
+                {
+                    if (skipNext)
+                    {
+                        skipNext = false;
+                        continue;
+                    }
+
+                    if (instruction.LoadsConstant(-100f))
+                    {
+                        yield return new CodeInstruction(System.Reflection.Emit.OpCodes.Ldc_R4, 0f);
+                        yield return new CodeInstruction(System.Reflection.Emit.OpCodes.Ldc_R4, 50f);
+                        skipNext = true;
+                    }
+                    else
+                        yield return instruction;
+                }
+            }
+        }
+    }
+}

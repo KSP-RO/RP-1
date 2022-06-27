2082a2301c1d20e32b487361957d293b9079e7d5
NathanKell <nathankell@gmail.com>
2022-06-26 19:21:09 -0700

    Tooltips for sci/rep widgets (plus an attempt at the funds one, doesn't work)

--- Source/Utilities/TransformUtils.cs
+++ Source/Utilities/TransformUtils.cs
@@ -21,14 +21,14 @@ namespace RP0
 
         public static void Dump(this GameObject go, string indent = "")
         {
-            Debug.Log(indent + "Object: " + go.name);
+            Debug.Log($"{indent}+{go.name} {go.transform.position}/{go.transform.localPosition} {go.transform.localScale}");
             foreach (Component c in go.GetComponents<Component>())
             {
-                string logStr = indent + c;
+                string logStr = $"{indent}{c.ToString().Replace(go.name,"")}";
                 if (c is TextMeshProUGUI t)
                     logStr += ": text=" + t.text;
                 else if (c is RectTransform rt)
-                    logStr += $": rect: {rt.rect}";
+                    logStr += $": rectTrf anchor3d={rt.anchoredPosition3D}, anchorN/X={rt.anchorMin}/{rt.anchorMax} size={rt.sizeDelta} pivot={rt.pivot} offsetN/X={rt.offsetMin}/{rt.offsetMax}";
 
                 Debug.Log(logStr);
             }

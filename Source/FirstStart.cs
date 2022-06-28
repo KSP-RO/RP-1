5993769d5dba4ffe898a80ae458fa4c3e1dfd8c8
siimav <siim.aaver@gmail.com>
2022-06-29 01:41:25 +0300

    Add code that marks science experiments as fully completed on career start.
    This allows us to set landed and flying-low situation multipliers to a reasonable number.

--- Source/FirstStart.cs
+++ Source/FirstStart.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace RP0
@@ -25,6 +26,8 @@ namespace RP0
                     string msg = "'Bypass Entry Purchase' difficulty setting was automatically changed to false because RP-1 doesn't work correctly in this state.";
                     PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), "", "Difficulty settings changed", msg, "Understood", false, HighLogic.UISkin);
                 }
+
+                MarkExperimentsAsDone();
             }
         }
 
@@ -42,6 +45,62 @@ namespace RP0
                 };
                 ResearchAndDevelopment.Instance.SetTechState(StartTechID, ptn);
             }
+        }
+
+        private static void MarkExperimentsAsDone()
+        {
+            foreach (ConfigNode rootCN in GameDatabase.Instance.GetConfigNodes("IGNORED_EXPERIMENTS"))
+            {
+                foreach (ConfigNode bodyCN in rootCN.GetNodes("BODY"))
+                {
+                    string bodyName = bodyCN.GetValue("name");
+
+                    foreach (ConfigNode expCN in bodyCN.GetNodes("EXPERIMENT"))
+                    {
+                        string experimentName = expCN.GetValue("name");
+
+                        foreach (ConfigNode sitCN in expCN.GetNodes("SITUATIONS"))
+                        {
+                            foreach (string sitName in sitCN.GetValues("name"))
+                            {
+                                MarkExperimentAsDone(experimentName, sitName, bodyName);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void MarkExperimentAsDone(string experimentID, string situationName, string bodyName)
+        {
+            ScienceExperiment experiment = ResearchAndDevelopment.GetExperiment(experimentID);
+            CelestialBody body = FlightGlobals.GetBodyByName(bodyName);
+            if (!Enum.TryParse(situationName, out ExperimentSituations situation))
+            {
+                Debug.LogError($"[RP-0] MarkExperimentAsDone: Invalid situation {situationName}");
+                return;
+            }
+
+            if (experiment.BiomeIsRelevantWhile(situation))
+            {
+                List<string> allBiomes = ResearchAndDevelopment.GetBiomeTags(body, false);
+                foreach (string biomeName in allBiomes)
+                {
+                    string biomeTitle = ScienceUtil.GetBiomedisplayName(body, biomeName);
+                    MarkAsDone(experiment, situation, body, biomeName, biomeTitle);
+                }
+            }
+            else
+            {
+                MarkAsDone(experiment, situation, body, string.Empty, string.Empty);
+            }
+        }
+
+        private static void MarkAsDone(ScienceExperiment experiment, ExperimentSituations situation, CelestialBody body, string biomeName, string biomeTitle)
+        {
+            ScienceSubject subj = ResearchAndDevelopment.GetExperimentSubject(experiment, situation, body, biomeName, biomeTitle);
+            subj.scientificValue = 0;
+            subj.science = subj.scienceCap;
         }
     }
 }

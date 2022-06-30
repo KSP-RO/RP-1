9aa4cf34d35a039423edc50c6dfb15fb35b8876e
NathanKell <nathankell@gmail.com>
2022-06-30 01:31:38 -0700

    Fix facility costs loading before, not after, CBK.

---
+++ Source/Harmony/CBKPatcher.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using KSP.UI.Screens;
+using KSP.UI;
+using Strategies;
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Reflection;
+using RP0.Programs;
+using UniLinq;
+
+namespace RP0
+{
+    public partial class HarmonyPatcher : MonoBehaviour
+    {
+        [HarmonyPatch]
+        internal class PatchCBK
+        {
+            static MethodBase TargetMethod() => AccessTools.TypeByName("CustomBarnKit.CustomBarnKit").GetMethod("LoadUpgradesPrices", AccessTools.all);
+            
+            [HarmonyPrefix]
+            internal static void Prefix_LoadUpgradesPrices(ref bool ___varLoaded, out bool __state)
+            {
+                __state = ___varLoaded;
+            }
+
+            [HarmonyPostfix]
+            internal static void Postfix_LoadUpgradesPrices(ref bool ___varLoaded, bool __state)
+            {
+                if (___varLoaded && !__state)
+                {
+                    MaintenanceHandler.ClearFacilityCosts();
+                    if (MaintenanceHandler.Instance != null)
+                        MaintenanceHandler.Instance.UpdateUpkeep();
+                }
+            }
+        }
+    }
+}

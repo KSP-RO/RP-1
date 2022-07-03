80f7c2a1dc38bccbdb1c646b233fe2f6dffea7f0
NathanKell <nathankell@gmail.com>
2022-07-03 00:42:09 -0700

    Allow toggling of entire applauncher from SC subscenes. Make togglebutton toggle it rather than KCT. Close https://github.com/KSP-RO/ProgramsIssueTracker/issues/6

---
+++ Source/Harmony/AppLauncherPatcher.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using KSP.UI.Screens;
+using KSP.UI;
+using UnityEngine;
+using Contracts;
+using System.Reflection;
+using System.Collections.Generic;
+using System;
+
+namespace RP0
+{
+    public partial class HarmonyPatcher : MonoBehaviour
+    {
+        [HarmonyPatch(typeof(ApplicationLauncher))]
+        internal class PatchAppLauncher
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("Show")]
+            internal static bool Prefix_Show(ApplicationLauncher __instance)
+            {
+                //if (KerbalConstructionTime.KCT_GUI.InSCSubscene)
+                //{
+                //    Transform trf = __instance.transform;
+                //    while (trf.parent != null)
+                //        trf = trf.parent;
+                //    Debug.Log("$$$$$$$$$$$$");
+                //    trf.gameObject.Dump();
+                //}
+                if (ApplicationLauncher.Ready && __instance.launcherSpace.gameObject.activeSelf)
+                    return false;
+
+                return true;
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch("Hide")]
+            internal static bool Prefix_Hide(ApplicationLauncher __instance)
+            {
+                if (!ApplicationLauncher.Ready && !__instance.launcherSpace.gameObject.activeSelf)
+                    return false;
+
+                return true;
+            }
+        }
+    }
+}

a8e05952d0b5975a3a416a96cc0154e57ae0b0d0
NathanKell <nathankell@gmail.com>
2022-07-02 21:14:34 -0700

    Implement siimav's TooltipController_TextFunc since rebasing in that branch isn't working for me. Also change it to inherit from _Text, and support continuous update.

---
+++ Source/UI/TooltipController_TextFunc.cs
@@ -0,0 +1,30 @@
+﻿using KSP.UI;
+using KSP.UI.TooltipTypes;
+using System;
+using UnityEngine.EventSystems;
+
+namespace RP0.UI
+{
+    public class TooltipController_TextFunc : TooltipController_Text
+    {
+        public Func<string> getStringAction;
+
+        public override void OnTooltipSpawned(KSP.UI.Tooltip instance)
+        {
+            textString = getStringAction();
+            base.OnTooltipSpawned(instance);
+        }
+
+        public override bool OnTooltipUpdate(KSP.UI.Tooltip instance)
+        {
+            if(continuousUpdate && tooltipInstance != null)
+            {
+                textString = getStringAction();
+                if (tooltipInstance.label.text != textString)
+                    tooltipInstance.label.text = textString;
+            }
+
+            return OnTooltipAboutToSpawn();
+        }
+    }
+}

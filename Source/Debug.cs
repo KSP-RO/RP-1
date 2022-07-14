c5ee85ca63187413cd8491996762513d2849ec66
NathanKell <nathankell@gmail.com>
2022-07-14 00:28:54 -0700

    RP-0 drives KCT, run maintenance every 10s UT. This should perform fine and will avoid the spikes up and down from KCT running every 0.5s and RP-1 running maint/programs every 1hr to 1day.

---
+++ Source/Debug.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RP0
+{
+    public static class RP0Debug
+    {
+        public static void Log(string str)
+        {
+#if DEBUG
+            UnityEngine.Debug.Log(str);
+#endif
+        }
+
+        public static void LogWarning(string str)
+        {
+#if DEBUG
+            UnityEngine.Debug.LogWarning(str);
+#endif
+        }
+
+        public static void LogError(string str)
+        {
+#if DEBUG
+            UnityEngine.Debug.LogError(str);
+#endif
+        }
+    }
+}

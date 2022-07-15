9592eade12f731a5a32bd8267a4df3f890874610
NathanKell <nathankell@gmail.com>
2022-07-14 22:43:56 -0700

    Rework game start. Start with no LC, better First Run dialog.

--- Source/KerbalConstructionTime/GUI/GUI_FirstRun.cs
+++ Source/KerbalConstructionTime/GUI/GUI_FirstRun.cs
@@ -5,56 +5,79 @@ namespace KerbalConstructionTime
     public static partial class KCT_GUI
     {
         private static Rect _firstRunWindowPosition = new Rect((Screen.width - 150) / 2, Screen.height / 5, 300, 50);
+        private static bool _dontShowFirstRunAgain = false;
+        public static void ResetShowFirstRunAgain() { _dontShowFirstRunAgain = false; }
+
+        private static int _lastSteps1 = -1, _lastSteps2 = -1;
 
         public static void DrawFirstRun(int windowID)
         {
+            if (IsPrimarilyDisabled)
+            {
+                GUIStates.ShowFirstRun = false;
+                return;
+            }
+
+            if(_lastSteps1 != _lastSteps2)
+                _firstRunWindowPosition.height = 50;
+
             GUILayout.BeginVertical();
             GUILayout.Label("Follow the steps below to get set up.");
 
             int step = 1;
-            if (PresetManager.Instance.Presets.Count > 1 && GUILayout.Button($"{step++}) Choose a Preset", HighLogic.Skin.button))
+            //if (PresetManager.Instance.Presets.Count > 1 && GUILayout.Button($"{step++}) Choose a Preset", HighLogic.Skin.button))
+            //{
+            //    ShowSettings();
+            //}
+
+            GUILayout.Label($"{step++}) If you want to play from a different site than Cape Canaveral, switch to the Tracking Station and select a new site.");
+            GUILayout.Label("");
+
+            if (!KCTGameStates.StartedProgram)
             {
-                ShowSettings();
+                GUILayout.Label($"{step++}) Choose your starting Programs.");
+                GUILayout.Label("These provide you with funds over time, and allow you to select relevant contracts in Mission Control. Go to the Admin Building to select them.");
+                GUILayout.Label("");
             }
 
-            GUILayout.Label($"{step++}) If you want to play from a different site than Cape Canaveral, switch to the Tracking Station and select a new site. You will only get a free launch complex in one site.");
-
-            if (!KCTGameStates.StarterLCSelected)
+            if (!KCTGameStates.StarterLCBuilding)
             {
-                GUILayout.Label($"{step++}) Choose a starting Launch Complex:");
-
-                if (GUILayout.Button("1t Capacity (for small rockets)", HighLogic.Skin.button))
+                GUILayout.Label($"{step++}) Build a starting Launch Complex. You can build new LCs in Space Center Management in the Operations tab, by clicking New.");
+                GUILayout.Label("The New LC dialog defaults to a reasonable starting LC for small rockets, though a larger LC (15t, 22x3x3m) is needed later and experience players can start directly with that (or with something else entirely).");
+                if (GUILayout.Button($"Build a new LC", HighLogic.Skin.button))
                 {
-                    CreateStartingPad(LCItem.StartingLC1);
-                    Utilities.AddFunds(PresetManager.Instance.ActivePreset.GeneralSettings.SmallLCExtraFunds, TransactionReasons.None);
-                    KCTGameStates.StarterLCSelected = true;
+                    GUIStates.ShowNewLC = true;
                 }
-                GUILayout.Label($"Also gives √{PresetManager.Instance.ActivePreset.GeneralSettings.SmallLCExtraFunds:N0}");
-
-                GUILayout.Label("or", GetLabelCenterAlignStyle());
-
-                if (GUILayout.Button($"15t Capacity (min: {LCItem.CalcMassMin(15):N0}t)", HighLogic.Skin.button))
-                {
-                    CreateStartingPad(LCItem.StartingLC15);
-                    KCTGameStates.StarterLCSelected = true;
-                }
+                GUILayout.Label("");
             }
             
-            if (!IsPrimarilyDisabled && KCTGameStates.UnassignedPersonnel > 0 )
+            if (!KCTGameStates.HiredStarterApplicants)
             {
-                GUILayout.Label($"{step++}) Assign your {KCTGameStates.UnassignedPersonnel} Applicants");
+                GUILayout.Label($"{step++}) Assign your {KCTGameStates.UnassignedPersonnel} starting Applicants");
                 if (GUILayout.Button($"Go to Staffing", HighLogic.Skin.button))
                 {
                     GUIStates.ShowPersonnelWindow = true;
                 }
+                GUILayout.Label("");
             }
 
-            GUILayout.Label("");
-
+            _dontShowFirstRunAgain = GUILayout.Toggle(_dontShowFirstRunAgain, "Don't show again");
             if (GUILayout.Button("Understood", HighLogic.Skin.button))
             {
+                if (_dontShowFirstRunAgain)
+                {
+                    KCTGameStates.StarterLCBuilding = true;
+                    KCTGameStates.HiredStarterApplicants = true;
+                    KCTGameStates.StartedProgram = true;
+                }
                 GUIStates.ShowFirstRun = false;
             }
+
+            if (_lastSteps1 < 0)
+                _lastSteps1 = step;
+
+            _lastSteps2 = _lastSteps1;
+            _lastSteps1 = step;
 
             GUILayout.EndVertical();
             if (!Input.GetMouseButtonDown(1) && !Input.GetMouseButtonDown(2))

885bb2b1188cb33cde9cd2ff44304caf705fbd14
NathanKell <nathankell@gmail.com>
2022-07-14 23:12:51 -0700

    Rip out NoPlanes

--- Source/CC_RP0/CustomExpressionParserRegistrer.cs
+++ Source/CC_RP0/CustomExpressionParserRegistrer.cs
@@ -53,7 +53,6 @@ namespace ContractConfigurator.RP0
             RegisterGlobalFunction(new Function<float>("RP1DeadlineMult", GetDeadlineMult, false));
             RegisterGlobalFunction(new Function<int>("RP1CommsPayload", GetCommsPayload, false));
             RegisterGlobalFunction(new Function<int>("RP1WeatherPayload", GetWeatherPayload, false));
-            RegisterGlobalFunction(new Function<bool>("RP1YesPlanes", GetPlaneContractsEnabled, false));
         }
 
         private static void RegisterRP0Hooks()
@@ -74,11 +73,6 @@ namespace ContractConfigurator.RP0
         private static int GetWeatherPayload()
         {
             return ContractGUI.WeatherPayload;
-        }
-
-        private static bool GetPlaneContractsEnabled()
-        {
-            return HighLogic.CurrentGame?.Parameters.CustomParams<RP0Settings>()?.PlaneContractsEnabled ?? true;
         }
 
         private static bool WithdrawContract(string contractName)

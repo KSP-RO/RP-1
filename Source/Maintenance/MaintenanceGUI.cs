9067c3bba4cd8e79b49ce483bbe8aab224c39e5a
NathanKell <nathankell@gmail.com>
2022-07-18 01:06:46 -0700

    Start adding program speeds (store speed, change duration/funding/reploss

--- Source/Maintenance/MaintenanceGUI.cs
+++ Source/Maintenance/MaintenanceGUI.cs
@@ -399,7 +399,7 @@ namespace RP0
                 GUILayout.BeginHorizontal();
                 GUILayout.Label(" Nominal Deadline:", HighLogic.Skin.label, GUILayout.Width(160));
                 const double secsPerYear = 365.25d * 86400d;
-                GUILayout.Label(KSPUtil.PrintDate(p.acceptedUT + p.nominalDurationYears * secsPerYear, false), RightLabel, GUILayout.Width(160));
+                GUILayout.Label(KSPUtil.PrintDate(p.acceptedUT + p.DurationYears * secsPerYear, false), RightLabel, GUILayout.Width(160));
                 GUILayout.EndHorizontal();
 
                 GUILayout.BeginHorizontal();

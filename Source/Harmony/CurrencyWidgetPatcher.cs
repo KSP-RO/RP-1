570b59053f9253ad39e1f82505f8e8186f0f56a7
NathanKell <nathankell@gmail.com>
2022-07-21 01:54:01 -0700

    Implement trust, trust widget.

--- Source/Harmony/CurrencyWidgetPatcher.cs
+++ Source/Harmony/CurrencyWidgetPatcher.cs
@@ -40,6 +40,8 @@ namespace RP0
         internal class PatchReputationWidget
         {
             public static TextMeshProUGUI RepLabel;
+            internal static Sprite repSprite = null;
+            internal static Sprite trustSprite = null;
 
             [HarmonyPrefix]
             [HarmonyPatch("onReputationChanged")]
@@ -53,6 +55,34 @@ namespace RP0
                 return false;
             }
 
+            internal static void CreateTrustWidget(GameObject trustWidgetObj)
+            {
+                trustWidgetObj.name = "TrustWidget";
+
+                GameObject.Destroy(trustWidgetObj.GetComponent<ReputationWidget>());
+
+                var frameImage = (Image)trustWidgetObj.GetComponentInChildren(typeof(Image));
+                frameImage.sprite = Sprite.Create(GameDatabase.Instance.GetTexture("RP-0/Resources/trust_background", false), frameImage.sprite.rect, frameImage.sprite.pivot);
+
+                var img = GameObject.Instantiate(new GameObject("Background"), trustWidgetObj.transform, worldPositionStays: false).AddComponent<Image>();
+                img.color = new Color32(58, 58, 63, 255);
+                img.rectTransform.anchorMin = frameImage.rectTransform.anchorMin;
+                img.rectTransform.anchorMax = frameImage.rectTransform.anchorMax;
+                img.rectTransform.anchoredPosition = frameImage.rectTransform.anchoredPosition;
+                img.rectTransform.sizeDelta = ((RectTransform)trustWidgetObj.transform).sizeDelta;    // No idea why the frame image transform is larger than the component itself
+
+                var textComp = GameObject.Instantiate(new GameObject("Text"), trustWidgetObj.transform, worldPositionStays: false).AddComponent<TextMeshProUGUI>();
+                textComp.alignment = TextAlignmentOptions.Right;
+                textComp.color = XKCDColors.KSPBadassGreen;
+                textComp.fontSize = 22;
+                textComp.rectTransform.localPosition = new Vector3(-9, -1, 0);
+                textComp.fontStyle = FontStyles.Bold;
+
+                var trustWidget = trustWidgetObj.AddComponent<TrustWidget>();
+                trustWidget.text = textComp;
+                trustWidget.DelayedStart();
+            }
+
             [HarmonyPrefix]
             [HarmonyPatch("DelayedStart")]
             internal static bool Prefix_DelayedStart(ReputationWidget __instance)
@@ -60,6 +90,9 @@ namespace RP0
                 GameObject.DestroyImmediate(__instance.gauge);
                 __instance.gauge = null;
                 GameObject.DestroyImmediate(__instance.gameObject.transform.Find("circularGauge").gameObject);
+
+                // Create the Trust widget
+                CreateTrustWidget(GameObject.Instantiate(__instance.gameObject, __instance.transform.parent, worldPositionStays: false));
 
                 var frameImage = (Image)__instance.gameObject.GetComponentInChildren(typeof(Image));
                 frameImage.sprite = Sprite.Create(GameDatabase.Instance.GetTexture("RP-0/Resources/rep_background", false), frameImage.sprite.rect, frameImage.sprite.pivot);
@@ -78,6 +111,7 @@ namespace RP0
                 RepLabel.rectTransform.localPosition = new Vector3(-9, -1, 0);
                 RepLabel.fontStyle = FontStyles.Bold;
 
+                // Add rep tooltip
                 var tooltip = __instance.gameObject.AddComponent<TooltipController_TextFunc>();
                 var prefab = AssetBase.GetPrefab<Tooltip_Text>("Tooltip_Text");
                 tooltip.prefab = prefab;

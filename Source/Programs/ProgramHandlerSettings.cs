86236de8a1464a02e5ce702a4af0481029686fdd
NathanKell <nathankell@gmail.com>
2022-07-22 02:29:08 -0700

    Show confidence and applicant rewards for contracts in MC and in messages.

--- Source/Programs/ProgramHandlerSettings.cs
+++ Source/Programs/ProgramHandlerSettings.cs
@@ -3,6 +3,12 @@
     public class ProgramHandlerSettings
     {
         [Persistent]
+        public float repToConfidence = 5;
+
+        [Persistent]
+        public float sciToConfidence = 2;
+
+        [Persistent]
         public DoubleCurve paymentCurve = new DoubleCurve();
     }
 }

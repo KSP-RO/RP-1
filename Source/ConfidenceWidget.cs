1e9f473728b1aab81a90105c1ee91eaf97c08450
NathanKell <nathankell@gmail.com>
2022-07-21 23:40:33 -0700

    Rename trust->confidence

---
+++ Source/ConfidenceWidget.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace RP0
+{
+    public class ConfidenceWidget : CurrencyWidget
+    {
+        public TMPro.TextMeshProUGUI text;
+
+        private void Awake()
+        {
+            Confidence.OnConfidenceChanged.Add(onConfidenceChanged);
+        }
+
+        private void OnDestroy()
+        {
+            Confidence.OnConfidenceChanged.Remove(onConfidenceChanged);
+        }
+
+        private void onConfidenceChanged(float confidence, TransactionReasons reason)
+        {
+            text.text = confidence.ToString("N1");
+        }
+
+
+        public override void DelayedStart()
+        {
+            if (Confidence.Instance != null)
+            {
+                onConfidenceChanged(Confidence.CurrentConfidence, TransactionReasons.None);
+            }
+        }
+
+        public override bool OnAboutToStart()
+        {
+            return Confidence.Instance != null;
+        }
+    }
+}

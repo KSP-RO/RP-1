0b64eba1d102958eadfaff28ebb68843a50916c2
NathanKell <nathankell@gmail.com>
2022-07-21 23:40:33 -0700

    Rename trust->confidence

--- Source/DifficultyPresets.cs
+++ Source/DifficultyPresets.cs
@@ -72,8 +72,8 @@ namespace RP0
         [GameParameters.CustomFloatParameterUI("Maintenance cost multiplier", minValue = 0f, maxValue = 10f, stepCount = 101, displayFormat = "N1", gameMode = GameParameters.GameMode.CAREER)]
         public float MaintenanceCostMult = 1f;
 
-        [GameParameters.CustomFloatParameterUI("Starting Trust", minValue = 0f, maxValue = 1000f, stepCount = 21, displayFormat = "N0", gameMode = GameParameters.GameMode.CAREER)]
-        public float StartingTrust = 500f;
+        [GameParameters.CustomFloatParameterUI("Starting Confidence", minValue = 0f, maxValue = 1000f, stepCount = 21, displayFormat = "N0", gameMode = GameParameters.GameMode.CAREER)]
+        public float StartingConfidence = 500f;
 
         [GameParameters.CustomFloatParameterUI("Kerbal Death Fixed Rep Loss", minValue = 0f, maxValue = 200f, stepCount = 21, displayFormat = "N0", gameMode = GameParameters.GameMode.CAREER)]
         public float RepLossKerbalDeathFixed = 100f;
@@ -119,7 +119,7 @@ namespace RP0
                     ContractDeadlineMult = 1.7f;
                     RepLossKerbalDeathFixed = 50f;
                     RepLossKerbalDeathPercent = 0.05f;
-                    StartingTrust = 750f;
+                    StartingConfidence = 750f;
                     break;
                 case GameParameters.Preset.Normal:
                     IsTrainingEnabled = true;
@@ -128,7 +128,7 @@ namespace RP0
                     ContractDeadlineMult = 1.3f;
                     RepLossKerbalDeathFixed = 80f;
                     RepLossKerbalDeathPercent = 0.08f;
-                    StartingTrust = 500f;
+                    StartingConfidence = 500f;
                     break;
                 case GameParameters.Preset.Moderate:
                     IsTrainingEnabled = true;
@@ -137,7 +137,7 @@ namespace RP0
                     ContractDeadlineMult = 1f;
                     RepLossKerbalDeathFixed = 100f;
                     RepLossKerbalDeathPercent = 0.1f;
-                    StartingTrust = 500f;
+                    StartingConfidence = 500f;
                     break;
                 case GameParameters.Preset.Hard:
                     IsTrainingEnabled = true;
@@ -146,7 +146,7 @@ namespace RP0
                     ContractDeadlineMult = 0.8f;
                     RepLossKerbalDeathFixed = 200f;
                     RepLossKerbalDeathPercent = 0.2f;
-                    StartingTrust = 350f;
+                    StartingConfidence = 350f;
                     break;
             }
         }

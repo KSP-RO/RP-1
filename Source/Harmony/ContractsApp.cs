ce70043eb170d1802787ab4f85e6ade884c40c3a
NathanKell <nathankell@gmail.com>
2022-07-24 23:44:53 -0700

    Move all Harmony patches to their own namespace, remove the partial class stuff

--- Source/Harmony/ContractsApp.cs
+++ Source/Harmony/ContractsApp.cs
@@ -7,59 +7,56 @@ using System.Reflection;
 using System.Collections.Generic;
 using System;
 
-namespace RP0
+namespace RP0.Harmony
 {
-    public partial class HarmonyPatcher : MonoBehaviour
+    [HarmonyPatch(typeof(ContractsApp))]
+    internal class PatchContractsApp
     {
+        internal static MethodInfo CreateParameterListMethod = typeof(ContractsApp).GetMethod("CreateParameterList", AccessTools.all);
+        
         private static bool ContractIsRecord(ContractConfigurator.ConfiguredContract cc)
         {
             return cc.contractType.group.name == "Records" || cc.contractType.group.name == "HumanRecords";
         }
-
-        [HarmonyPatch(typeof(ContractsApp))]
-        internal class PatchContractsApp
+        
+        [HarmonyPrefix]
+        [HarmonyPatch("CreateItem")]
+        internal static bool Prefix_CreateItem(ContractsApp __instance, ref Contract contract, ref KSP.UI.UICascadingList.CascadingListItem __result, ref GenericCascadingList ___cascadingList, ref Dictionary<Guid, UICascadingList.CascadingListItem> ___contractList)
         {
-            internal static MethodInfo CreateParameterListMethod = typeof(ContractsApp).GetMethod("CreateParameterList", AccessTools.all);
-
-            [HarmonyPrefix]
-            [HarmonyPatch("CreateItem")]
-            internal static bool Prefix_CreateItem(ContractsApp __instance, ref Contract contract, ref KSP.UI.UICascadingList.CascadingListItem __result, ref GenericCascadingList ___cascadingList, ref Dictionary<Guid, UICascadingList.CascadingListItem> ___contractList)
+            if (contract is ContractConfigurator.ConfiguredContract configuredContract)
             {
-                if (contract is ContractConfigurator.ConfiguredContract configuredContract)
+                // Records go at the end
+                if (ContractIsRecord(configuredContract))
+                    return true;
+
+                UnityEngine.UI.Button button;
+                int index = -1;
+
+                // Find the index of the first record.
+                foreach (var kvp in ___contractList)
                 {
-                    // Records go at the end
-                    if (ContractIsRecord(configuredContract))
-                        return true;
-
-                    UnityEngine.UI.Button button;
-                    int index = -1;
-
-                    // Find the index of the first record.
-                    foreach (var kvp in ___contractList)
+                    if (ContractSystem.Instance.GetContractByGuid(kvp.Key) is ContractConfigurator.ConfiguredContract cc)
                     {
-                        if (ContractSystem.Instance.GetContractByGuid(kvp.Key) is ContractConfigurator.ConfiguredContract cc)
+                        if (ContractIsRecord(cc))
                         {
-                            if (ContractIsRecord(cc))
+                            int idx = ___cascadingList.ruiList.cascadingList.GetIndex(kvp.Value.header);
+                            if (idx < 0)
                             {
-                                int idx = ___cascadingList.ruiList.cascadingList.GetIndex(kvp.Value.header);
-                                if (idx < 0)
-                                {
-                                    Debug.LogError($"[RP-0] ContractsApp patcher: {cc.contractType.name} can't be found in UI list despite its guid existing in dictionary!");
-                                    continue;
-                                }
-                                if (index < 0 || idx < index)
-                                    index = idx;
+                                Debug.LogError($"[RP-0] ContractsApp patcher: {cc.contractType.name} can't be found in UI list despite its guid existing in dictionary!");
+                                continue;
                             }
+                            if (index < 0 || idx < index)
+                                index = idx;
                         }
                     }
-
-                    UIListItem header = ___cascadingList.CreateHeader("<color=#e6752a>" + contract.Title + "</color>", out button, scaleBg: true);
-                    __result = ___cascadingList.ruiList.AddCascadingItem(header, ___cascadingList.CreateFooter(), CreateParameterListMethod.Invoke(__instance, new object[] { contract }) as List<UIListItem>, button, index);
-                    return false;
                 }
 
-                return true;
+                UIListItem header = ___cascadingList.CreateHeader("<color=#e6752a>" + contract.Title + "</color>", out button, scaleBg: true);
+                __result = ___cascadingList.ruiList.AddCascadingItem(header, ___cascadingList.CreateFooter(), CreateParameterListMethod.Invoke(__instance, new object[] { contract }) as List<UIListItem>, button, index);
+                return false;
             }
+
+            return true;
         }
     }
 }

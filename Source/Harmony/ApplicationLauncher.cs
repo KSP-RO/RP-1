bc08e34ceb2bf0a0b1c5f8f8d2cffc2aac303685
NathanKell <nathankell@gmail.com>
2022-07-24 23:44:53 -0700

    Move all Harmony patches to their own namespace, remove the partial class stuff

--- Source/Harmony/ApplicationLauncher.cs
+++ Source/Harmony/ApplicationLauncher.cs
@@ -7,40 +7,37 @@ using System.Reflection;
 using System.Collections.Generic;
 using System;
 
-namespace RP0
+namespace RP0.Harmony
 {
-    public partial class HarmonyPatcher : MonoBehaviour
+    [HarmonyPatch(typeof(ApplicationLauncher))]
+    internal class PatchAppLauncher
     {
-        [HarmonyPatch(typeof(ApplicationLauncher))]
-        internal class PatchAppLauncher
+        [HarmonyPrefix]
+        [HarmonyPatch("Show")]
+        internal static bool Prefix_Show(ApplicationLauncher __instance)
         {
-            [HarmonyPrefix]
-            [HarmonyPatch("Show")]
-            internal static bool Prefix_Show(ApplicationLauncher __instance)
-            {
-                //if (KerbalConstructionTime.KCT_GUI.InSCSubscene)
-                //{
-                //    Transform trf = __instance.transform;
-                //    while (trf.parent != null)
-                //        trf = trf.parent;
-                //    Debug.Log("$$$$$$$$$$$$");
-                //    trf.gameObject.Dump();
-                //}
-                if (ApplicationLauncher.Ready && __instance.launcherSpace.gameObject.activeSelf)
-                    return false;
+            //if (KerbalConstructionTime.KCT_GUI.InSCSubscene)
+            //{
+            //    Transform trf = __instance.transform;
+            //    while (trf.parent != null)
+            //        trf = trf.parent;
+            //    Debug.Log("$$$$$$$$$$$$");
+            //    trf.gameObject.Dump();
+            //}
+            if (ApplicationLauncher.Ready && __instance.launcherSpace.gameObject.activeSelf)
+                return false;
 
-                return true;
-            }
+            return true;
+        }
 
-            [HarmonyPrefix]
-            [HarmonyPatch("Hide")]
-            internal static bool Prefix_Hide(ApplicationLauncher __instance)
-            {
-                if (!ApplicationLauncher.Ready && !__instance.launcherSpace.gameObject.activeSelf)
-                    return false;
+        [HarmonyPrefix]
+        [HarmonyPatch("Hide")]
+        internal static bool Prefix_Hide(ApplicationLauncher __instance)
+        {
+            if (!ApplicationLauncher.Ready && !__instance.launcherSpace.gameObject.activeSelf)
+                return false;
 
-                return true;
-            }
+            return true;
         }
     }
 }

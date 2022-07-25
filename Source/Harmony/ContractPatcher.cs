c94d819d7ada07c40c02cba77a1f90212409fb39
NathanKell <nathankell@gmail.com>
2022-07-24 23:44:53 -0700

    Move all Harmony patches to their own namespace, remove the partial class stuff

--- Source/Harmony/ContractPatcher.cs
+++ Source/Harmony/ContractPatcher.cs
@@ -3,122 +3,119 @@ using UnityEngine;
 using KSP.UI.Screens;
 using Contracts;
 
-namespace RP0
+namespace RP0.Harmony
 {
-    public partial class HarmonyPatcher : MonoBehaviour
+    [HarmonyPatch(typeof(ContractSystem))]
+    internal class PatchContractSystem
     {
-        [HarmonyPatch(typeof(ContractSystem))]
-        internal class PatchContractSystem
+        [HarmonyPrefix]
+        [HarmonyPatch("GetContractCounts")]
+        internal static bool Prefix_GetContractCounts(ContractSystem __instance, ref float rep, ref int avgContracts, ref int tier1, ref int tier2, ref int tier3)
         {
-            [HarmonyPrefix]
-            [HarmonyPatch("GetContractCounts")]
-            internal static bool Prefix_GetContractCounts(ContractSystem __instance, ref float rep, ref int avgContracts, ref int tier1, ref int tier2, ref int tier3)
+            tier1 = tier2 = tier3 = int.MaxValue;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(ContractParameter))]
+    internal class PatchContractParameter
+    {
+        internal static float _storedRep = 0f;
+
+        [HarmonyPrefix]
+        [HarmonyPatch("SendStateMessage")]
+        internal static void Prefix_SendStateMessage(ContractParameter __instance, ref string title, ref string message, ref MessageSystemButton.MessageButtonColor color, ref MessageSystemButton.ButtonIcons icon)
+        {
+            if (icon != MessageSystemButton.ButtonIcons.COMPLETE || !__instance.Optional)
+                return;
+
+            if (__instance.Root is ContractConfigurator.ConfiguredContract cc && Programs.ProgramHandler.Instance != null && Programs.ProgramHandler.Instance.IsContractOptional(cc))
             {
-                tier1 = tier2 = tier3 = int.MaxValue;
-                return false;
+                message += $"\n<color=#{RUIutils.ColorToHex(XKCDColors.KSPBadassGreen)}>{KSP.Localization.Localizer.Format("#rp0ConfidenceValue", (_storedRep * Programs.ProgramHandler.Settings.repToConfidence).ToString("N0"))}</color>";
             }
         }
 
-        [HarmonyPatch(typeof(ContractParameter))]
-        internal class PatchContractParameter
+        [HarmonyPrefix]
+        [HarmonyPatch("AwardCompletion")]
+        internal static void Prefix_AwardCompletion(ContractParameter __instance)
         {
-            internal static float _storedRep = 0f;
-
-            [HarmonyPrefix]
-            [HarmonyPatch("SendStateMessage")]
-            internal static void Prefix_SendStateMessage(ContractParameter __instance, ref string title, ref string message, ref MessageSystemButton.MessageButtonColor color, ref MessageSystemButton.ButtonIcons icon)
+            //KSP sets the rewards to 0 as part of this!
+            //So we have to intercept and store the value.
+            if (__instance.Root is ContractConfigurator.ConfiguredContract cc && Programs.ProgramHandler.Instance != null && Programs.ProgramHandler.Instance.IsContractOptional(cc))
             {
-                if (icon != MessageSystemButton.ButtonIcons.COMPLETE || !__instance.Optional)
-                    return;
-
-                if (__instance.Root is ContractConfigurator.ConfiguredContract cc && Programs.ProgramHandler.Instance != null && Programs.ProgramHandler.Instance.IsContractOptional(cc))
-                {
-                    message += $"\n<color=#{RUIutils.ColorToHex(XKCDColors.KSPBadassGreen)}>{KSP.Localization.Localizer.Format("#rp0ConfidenceValue", (_storedRep * Programs.ProgramHandler.Settings.repToConfidence).ToString("N0"))}</color>";
-                }
+                CurrencyModifierQuery cmq = CurrencyModifierQuery.RunQuery(TransactionReasons.ContractReward, 0, 0, __instance.ReputationCompletion);
+                _storedRep = cmq.GetTotal(Currency.Reputation);
             }
+        }
+    }
 
-            [HarmonyPrefix]
-            [HarmonyPatch("AwardCompletion")]
-            internal static void Prefix_AwardCompletion(ContractParameter __instance)
+    [HarmonyPatch]
+    internal class PatchContractRewards
+    {
+        internal static ContractConfigurator.ConfiguredContract _contract;
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Contract))]
+        [HarmonyPatch("MessageRewards")]
+        internal static void Prefix_MessageRewards(Contract __instance)
+        {
+            if (__instance is ContractConfigurator.ConfiguredContract cc)
             {
-                //KSP sets the rewards to 0 as part of this!
-                //So we have to intercept and store the value.
-                if (__instance.Root is ContractConfigurator.ConfiguredContract cc && Programs.ProgramHandler.Instance != null && Programs.ProgramHandler.Instance.IsContractOptional(cc))
-                {
-                    CurrencyModifierQuery cmq = CurrencyModifierQuery.RunQuery(TransactionReasons.ContractReward, 0, 0, __instance.ReputationCompletion);
-                    _storedRep = cmq.GetTotal(Currency.Reputation);
-                }
+                _contract = cc;
             }
         }
 
-        [HarmonyPatch]
-        internal class PatchContractRewards
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Contract))]
+        [HarmonyPatch("MessageRewards")]
+        internal static void Postfix_MessageRewards(Contract __instance)
         {
-            internal static ContractConfigurator.ConfiguredContract _contract;
+            _contract = null;
+        }
 
-            [HarmonyPrefix]
-            [HarmonyPatch(typeof(Contract))]
-            [HarmonyPatch("MessageRewards")]
-            internal static void Prefix_MessageRewards(Contract __instance)
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Contract))]
+        [HarmonyPatch("MissionControlTextRich")]
+        internal static void Prefix_MissionControlTextRich(Contract __instance)
+        {
+            if (__instance is ContractConfigurator.ConfiguredContract cc)
             {
-                if (__instance is ContractConfigurator.ConfiguredContract cc)
-                {
-                    _contract = cc;
-                }
+                _contract = cc;
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Contract))]
+        [HarmonyPatch("MissionControlTextRich")]
+        internal static void Postfix_MissionControlTextRich(Contract __instance)
+        {
+            _contract = null;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(RichTextUtil))]
+        [HarmonyPatch("TextAward")]
+        internal static bool Postfix_TextAward(ref string __result, ref string title, ref string value, ref int lines)
+        {
+            if (_contract == null)
+                return true;
+
+            if (Programs.ProgramHandler.Instance != null && Programs.ProgramHandler.Instance.IsContractOptional(_contract))
+            {
+                CurrencyModifierQuery cmq = CurrencyModifierQuery.RunQuery(TransactionReasons.ContractReward, 0, 0, _contract.ReputationCompletion);
+                value += $"\n<color=#{RUIutils.ColorToHex(XKCDColors.KSPBadassGreen)}>{KSP.Localization.Localizer.Format("#rp0ConfidenceValue", (cmq.GetTotal(Currency.Reputation) * Programs.ProgramHandler.Settings.repToConfidence).ToString("N0"))}</color>";
+            }
+            if (KerbalConstructionTime.PresetManager.Instance != null)
+            {
+                int applicants = KerbalConstructionTime.PresetManager.Instance.ActivePreset.GeneralSettings.ContractApplicants.GetApplicantsFromContract(_contract.contractType.name);
+                if (applicants > 0)
+                    value += $"\n{KSP.Localization.Localizer.Format("#rp0GainApplicants", applicants)}";
             }
 
-            [HarmonyPostfix]
-            [HarmonyPatch(typeof(Contract))]
-            [HarmonyPatch("MessageRewards")]
-            internal static void Postfix_MessageRewards(Contract __instance)
-            {
-                _contract = null;
-            }
-
-            [HarmonyPrefix]
-            [HarmonyPatch(typeof(Contract))]
-            [HarmonyPatch("MissionControlTextRich")]
-            internal static void Prefix_MissionControlTextRich(Contract __instance)
-            {
-                if (__instance is ContractConfigurator.ConfiguredContract cc)
-                {
-                    _contract = cc;
-                }
-            }
-
-            [HarmonyPostfix]
-            [HarmonyPatch(typeof(Contract))]
-            [HarmonyPatch("MissionControlTextRich")]
-            internal static void Postfix_MissionControlTextRich(Contract __instance)
-            {
-                _contract = null;
-            }
-
-            [HarmonyPrefix]
-            [HarmonyPatch(typeof(RichTextUtil))]
-            [HarmonyPatch("TextAward")]
-            internal static bool Postfix_TextAward(ref string __result, ref string title, ref string value, ref int lines)
-            {
-                if (_contract == null)
-                    return true;
-
-                if (Programs.ProgramHandler.Instance != null && Programs.ProgramHandler.Instance.IsContractOptional(_contract))
-                {
-                    CurrencyModifierQuery cmq = CurrencyModifierQuery.RunQuery(TransactionReasons.ContractReward, 0, 0, _contract.ReputationCompletion);
-                    value += $"\n<color=#{RUIutils.ColorToHex(XKCDColors.KSPBadassGreen)}>{KSP.Localization.Localizer.Format("#rp0ConfidenceValue", (cmq.GetTotal(Currency.Reputation) * Programs.ProgramHandler.Settings.repToConfidence).ToString("N0"))}</color>";
-                }
-                if (KerbalConstructionTime.PresetManager.Instance != null)
-                {
-                    int applicants = KerbalConstructionTime.PresetManager.Instance.ActivePreset.GeneralSettings.ContractApplicants.GetApplicantsFromContract(_contract.contractType.name);
-                    if (applicants > 0)
-                        value += $"\n{KSP.Localization.Localizer.Format("#rp0GainApplicants", applicants)}";
-                }
-
-                __result = $"<b><color=#{RUIutils.ColorToHex(RichTextUtil.colorAwards)}>{title}:</color></b> {value}";
-                if(lines > 0)
-                    __result += new string('\n', lines);
-                return false;
-            }
+            __result = $"<b><color=#{RUIutils.ColorToHex(RichTextUtil.colorAwards)}>{title}:</color></b> {value}";
+            if (lines > 0)
+                __result += new string('\n', lines);
+            return false;
         }
     }
 }

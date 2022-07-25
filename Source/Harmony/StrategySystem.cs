ea67f78ed4f96f42c05dc39583c6edb5efbdd77e
NathanKell <nathankell@gmail.com>
2022-07-24 23:44:53 -0700

    Move all Harmony patches to their own namespace, remove the partial class stuff

--- Source/Harmony/StrategySystem.cs
+++ Source/Harmony/StrategySystem.cs
@@ -11,63 +11,60 @@ using System.Reflection;
 using RP0.Programs;
 using UniLinq;
 
-namespace RP0
+namespace RP0.Harmony
 {
-    public partial class HarmonyPatcher : MonoBehaviour
+    [HarmonyPatch(typeof(StrategySystem))]
+    internal class PatchStrategySystem
     {
-        [HarmonyPatch(typeof(StrategySystem))]
-        internal class PatchStrategySystem
+        internal static HashSet<string> acceptablePrograms = new HashSet<string>();
+        internal static HashSet<string> completedPrograms = new HashSet<string>();
+
+        [HarmonyPrefix]
+        [HarmonyPatch("GetStrategies")]
+        internal static bool Prefix_GetStrategies(StrategySystem __instance, ref string department, ref List<Strategy> __result, ref List<Strategy> ___strategies)
         {
-            internal static HashSet<string> acceptablePrograms = new HashSet<string>();
-            internal static HashSet<string> completedPrograms = new HashSet<string>();
+            if (department != "Programs")
+                return true;
 
-            [HarmonyPrefix]
-            [HarmonyPatch("GetStrategies")]
-            internal static bool Prefix_GetStrategies(StrategySystem __instance, ref string department, ref List<Strategy> __result, ref List<Strategy> ___strategies)
+            List<Strategy> list = new List<Strategy>();
+
+            // Cache what programs can be accepted (and which have been completed)
+            // We don't care about confidence thresholds because you can always accept at Slow speed
+            foreach (Program p in ProgramHandler.Programs)
+                if (p.CanAccept && !ProgramHandler.Instance.DisabledPrograms.Contains(p.name))
+                    acceptablePrograms.Add(p.name);
+
+            foreach (Program p in ProgramHandler.Instance.CompletedPrograms)
+                completedPrograms.Add(p.name);
+
+            // Insert acceptable programs first
+            for (int i = 0; i < ___strategies.Count; ++i)
             {
-                if (department != "Programs")
-                    return true;
+                Strategy strat = ___strategies[i];
+                if (strat.DepartmentName != department)
+                    continue;
 
-                List<Strategy> list = new List<Strategy>();
-
-                // Cache what programs can be accepted (and which have been completed)
-                // We don't care about confidence thresholds because you can always accept at Slow speed
-                foreach (Program p in ProgramHandler.Programs)
-                    if (p.CanAccept && !ProgramHandler.Instance.DisabledPrograms.Contains(p.name))
-                        acceptablePrograms.Add(p.name);
-
-                foreach (Program p in ProgramHandler.Instance.CompletedPrograms)
-                    completedPrograms.Add(p.name);
-
-                // Insert acceptable programs first
-                for (int i = 0; i < ___strategies.Count; ++i)
-                {
-                    Strategy strat = ___strategies[i];
-                    if (strat.DepartmentName != department)
-                        continue;
-
-                    string name = strat.Config.Name;
-                    if (acceptablePrograms.Contains(name) && !completedPrograms.Contains(name))
-                        list.Add(strat);
-                }
-
-                // then insert other programs
-                for (int i = 0; i < ___strategies.Count; ++i)
-                {
-                    Strategy strat = ___strategies[i];
-                    if (strat.DepartmentName != department)
-                        continue;
-
-                    string name = strat.Config.Name;
-                    if (!acceptablePrograms.Contains(name) && !completedPrograms.Contains(name) && !ProgramHandler.Instance.DisabledPrograms.Contains(name))
-                        list.Add(strat);
-                }
-
-                __result = list;
-                acceptablePrograms.Clear();
-                completedPrograms.Clear();
-                return false;
+                string name = strat.Config.Name;
+                if (acceptablePrograms.Contains(name) && !completedPrograms.Contains(name))
+                    list.Add(strat);
             }
+
+            // then insert other programs
+            for (int i = 0; i < ___strategies.Count; ++i)
+            {
+                Strategy strat = ___strategies[i];
+                if (strat.DepartmentName != department)
+                    continue;
+
+                string name = strat.Config.Name;
+                if (!acceptablePrograms.Contains(name) && !completedPrograms.Contains(name) && !ProgramHandler.Instance.DisabledPrograms.Contains(name))
+                    list.Add(strat);
+            }
+
+            __result = list;
+            acceptablePrograms.Clear();
+            completedPrograms.Clear();
+            return false;
         }
     }
 }

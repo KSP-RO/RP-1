2cce4b3326ea0a262e6ae88dcc72995cc260060b
NathanKell <nathankell@gmail.com>
2022-07-24 23:44:53 -0700

    Move all Harmony patches to their own namespace, remove the partial class stuff

--- Source/Harmony/FundsWidget.cs
+++ Source/Harmony/FundsWidget.cs
@@ -7,41 +7,38 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace RP0
+namespace RP0.Harmony
 {
-    public partial class HarmonyPatcher : MonoBehaviour
+    [HarmonyPatch(typeof(FundsWidget))]
+    internal class PatchFundsWidget
     {
-        [HarmonyPatch(typeof(FundsWidget))]
-        internal class PatchFundsWidget
+        [HarmonyPostfix]
+        [HarmonyPatch("DelayedStart")]
+        internal static void Postfix_DelayedStart(FundsWidget __instance)
         {
-            [HarmonyPostfix]
-            [HarmonyPatch("DelayedStart")]
-            internal static void Postfix_DelayedStart(FundsWidget __instance)
-            {
-                // Get foreground element
-                var foreground = __instance.transform.Find("Foreground");
+            // Get foreground element
+            var foreground = __instance.transform.Find("Foreground");
 
-                // The top level object for the widget has a Canvas component but no
-                // GraphicRaycaster, we need one so OnMouseEnter/Exit events handled
-                // by the tooltip are triggered.
-                foreground.parent.gameObject.AddComponent<GraphicRaycaster>();
+            // The top level object for the widget has a Canvas component but no
+            // GraphicRaycaster, we need one so OnMouseEnter/Exit events handled
+            // by the tooltip are triggered.
+            foreground.parent.gameObject.AddComponent<GraphicRaycaster>();
 
-                // Add tooltip
-                var tooltip = foreground.gameObject.AddComponent<TooltipController_TextFunc>();
-                var prefab = AssetBase.GetPrefab<Tooltip_Text>("Tooltip_Text");
-                tooltip.RequireInteractable = false;
-                tooltip.prefab = prefab;
-                tooltip.getStringAction = GetTooltipText;
-                tooltip.continuousUpdate = false;
-            }
+            // Add tooltip
+            var tooltip = foreground.gameObject.AddComponent<TooltipController_TextFunc>();
+            var prefab = AssetBase.GetPrefab<Tooltip_Text>("Tooltip_Text");
+            tooltip.RequireInteractable = false;
+            tooltip.prefab = prefab;
+            tooltip.getStringAction = GetTooltipText;
+            tooltip.continuousUpdate = false;
+        }
 
-            private static string GetTooltipText()
-            {
-                return Localizer.Format("#rp0FundsWidgetTooltip",
-                                        LocalizationHandler.FormatValuePositiveNegative(KCTGameStates.GetBudgetDelta(86400d), "N1"),
-                                        LocalizationHandler.FormatValuePositiveNegative(KCTGameStates.GetBudgetDelta(86400d * 30d), "N0"),
-                                        LocalizationHandler.FormatValuePositiveNegative(KCTGameStates.GetBudgetDelta(86400d * 365.25d), "N0"));
-            }
+        private static string GetTooltipText()
+        {
+            return Localizer.Format("#rp0FundsWidgetTooltip",
+                                    LocalizationHandler.FormatValuePositiveNegative(KCTGameStates.GetBudgetDelta(86400d), "N1"),
+                                    LocalizationHandler.FormatValuePositiveNegative(KCTGameStates.GetBudgetDelta(86400d * 30d), "N0"),
+                                    LocalizationHandler.FormatValuePositiveNegative(KCTGameStates.GetBudgetDelta(86400d * 365.25d), "N0"));
         }
     }
 }

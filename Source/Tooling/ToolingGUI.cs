6baae19fc60d9adc33bd4a04c4fcbda3804cee93
NathanKell <nathankell@gmail.com>
2022-07-27 00:00:59 -0700

    Fix uses of Funding that weren't through CMQ.

--- Source/Tooling/ToolingGUI.cs
+++ Source/Tooling/ToolingGUI.cs
@@ -134,7 +134,7 @@ namespace RP0
             if (GUILayout.Button("Tool All", HighLogic.Skin.button))
             {
                 GetUntooledPartsAndCost(out List<ModuleTooling> untooledParts, out float toolingCost);
-                bool canAfford = Funding.Instance.Funds >= toolingCost;
+                bool canAfford = CurrencyModifierQuery.RunQuery(TransactionReasons.RnDPartPurchase, -toolingCost, 0f, 0f).CanAfford();
                 string buttonText = canAfford ? "Purchase All Toolings" : "Can't Afford";
 
                 var dialog = new MultiOptionDialog(
@@ -161,7 +161,7 @@ namespace RP0
         {
             GetUntooledPartsAndCost(out List<ModuleTooling> untooledParts, out float toolingCost);
 
-            bool canAfford = Funding.Instance.Funds >= toolingCost;
+            bool canAfford = CurrencyModifierQuery.RunQuery(TransactionReasons.RnDPartPurchase, -toolingCost, 0f, 0f).CanAfford();
             if (canAfford)
             {
                 ModuleTooling.PurchaseToolingBatch(untooledParts);

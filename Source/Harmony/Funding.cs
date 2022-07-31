7d61b93e886254d715c63de8bf4e3ea872521c35
NathanKell <nathankell@gmail.com>
2022-07-30 22:45:21 -0700

    Replace CurrencyModifierQuery with our own. Caveat: Anyone who asks for `new CurrencyModifierQuery` will still get the old one, but I patched the stock callsites to use RunQuery (which almost eveyrthing does), and mods should hopefully do that as well. (Patched CC just now.)

---
+++ Source/Harmony/Funding.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+
+namespace RP0.Harmony
+{
+    [HarmonyPatch(typeof(Funding))]
+    internal class PatchFunding
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("AddFunds")]
+        internal static bool Prefix_AddFunds(Funding __instance, double value, TransactionReasons reason, ref double ___funds)
+        {
+            ___funds += value;
+            CurrencyModifierQueryRP0 data = new CurrencyModifierQueryRP0(reason, value, 0f, 0f);
+            GameEvents.Modifiers.OnCurrencyModifierQuery.Fire(data);
+            GameEvents.Modifiers.OnCurrencyModified.Fire(data);
+            if (!HighLogic.CurrentGame.Parameters.CustomParams<GameParameters.AdvancedParams>().AllowNegativeCurrency && ___funds < 0d)
+                ___funds = 0d;
+
+            GameEvents.OnFundsChanged.Fire(___funds, reason);
+
+            return false;
+        }
+    }
+}

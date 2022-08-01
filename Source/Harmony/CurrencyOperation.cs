690c9ee3930a97d70567e6ca726a32e4c1990f28
NathanKell <nathankell@gmail.com>
2022-07-31 19:45:20 -0700

    Fix issue with CurrencyConverter/operation nodes acting destructively

--- Source/Harmony/CurrencyOperation.cs
+++ Source/Harmony/CurrencyOperation.cs
@@ -8,8 +8,11 @@ namespace RP0.Harmony
     {
         [HarmonyPrefix]
         [HarmonyPatch("OnLoadFromConfig")]
-        internal static void Prefix_OnLoadFromConfig(CurrencyOperation __instance, ConfigNode node, ref Currency ___currency, ref TransactionReasons ___AffectReasons)
+        internal static void Prefix_OnLoadFromConfig(CurrencyOperation __instance, ref ConfigNode node, ref Currency ___currency, ref TransactionReasons ___AffectReasons)
         {
+            // We need to copy the node so we can remove values from it
+            node = node.CreateCopy();
+
             CurrencyRP0 cur = CurrencyRP0.Funds;
             node.TryGetEnum<CurrencyRP0>("currency", ref cur, CurrencyRP0.Funds);
             ___currency = (Currency)cur;
@@ -24,7 +27,15 @@ namespace RP0.Harmony
                 int num = array.Length;
                 for (int i = 0; i < num; i++)
                 {
-                    ___AffectReasons |= (TransactionReasons)System.Enum.Parse(typeof(TransactionReasonsRP0), array[i].Trim());
+                    if (!System.Enum.TryParse(array[i].Trim(), out TransactionReasonsRP0 reason))
+                    {
+                        UnityEngine.Debug.LogError($"[RP-0] Error parsing TransactionReasonsRP0 enum value {array[i].Trim()}");
+                    }
+                    else
+                    {
+                        UnityEngine.Debug.Log($"$$$$ Parsed {array[i].Trim()} as {reason.ToString()} with stock version {reason.Stock().ToString()}");
+                        ___AffectReasons |= reason.Stock();
+                    }
                 }
                 // No throwing!
                 node.RemoveValue("AffectReasons");

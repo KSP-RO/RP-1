ccc04a704d1169077791234d298858a65fc6a4aa
NathanKell <nathankell@gmail.com>
2022-08-01 02:05:34 -0700

    Add description support to our currencies

---
+++ Source/Harmony/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+
+namespace RP0.Harmony
+{
+    [HarmonyPatch(typeof(EnumExtensions))]
+    internal class PatchEnumExtensions
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("Description")]
+        internal static void Prefix_Description(ref Enum e)
+        {
+            if (e is Currency)
+            {
+                CurrencyRP0 newValue = (CurrencyRP0)e;
+                e = newValue;
+            }
+        }
+    }
+}

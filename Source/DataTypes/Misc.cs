169deb221a185184715659f39ebc3ac5a14f1087
NathanKell <nathankell@gmail.com>
2022-08-04 00:21:19 -0700

    * Refactor Currencies to distinguish between rate and time * Add two new leader effects, one to modify research rate for particular nodes, one to modify effective costs of parts (or whole vessel) * Fix using confidence instead of time in a CMQ output * Move Leader-related files to Leaders

---
+++ Source/DataTypes/Misc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RP0.DataTypes
+{
+    public class Boxed<T> where T : struct
+    {
+        public T value;
+        public Boxed(T val) { value = val; }
+        public Boxed() { }
+    }
+}

ce2b335214821ee6e0fe211e76f8464be1dcc189
NathanKell <nathankell@gmail.com>
2022-08-04 00:21:19 -0700

    * Refactor Currencies to distinguish between rate and time * Add two new leader effects, one to modify research rate for particular nodes, one to modify effective costs of parts (or whole vessel) * Fix using confidence instead of time in a CMQ output * Move Leader-related files to Leaders

---
+++ Source/Leaders/Effects/ResearchRateModifier.cs
@@ -0,0 +1,56 @@
+﻿using Strategies;
+using UnityEngine;
+using System.Collections.Generic;
+using RP0.DataTypes;
+using KerbalConstructionTime;
+
+namespace RP0.Leaders
+{
+    public class ResearchRateModifier : StrategyEffect
+    {
+        [Persistent]
+        private string effectDescription = string.Empty;
+
+        [Persistent]
+        private double value = 0d;
+
+        [Persistent]
+        private PersistentListValueType<NodeType> nodeTypes = new PersistentListValueType<NodeType>();
+
+        private NodeType nodeType;
+
+        public ResearchRateModifier(Strategy parent)
+            : base(parent)
+        {
+        }
+
+        protected override string GetDescription()
+        {
+            return KSP.Localization.Localizer.Format("#rp0LeaderEffectResearchRateModifier", LocalizationHandler.FormatRatioAsPercent(value), effectDescription);
+        }
+
+        protected override void OnLoadFromConfig(ConfigNode node)
+        {
+            ConfigNode.LoadObjectFromConfig(this, node);
+            nodeType = NodeType.None;
+            foreach (var n in nodeTypes)
+                nodeType |= n;
+        }
+
+        protected override void OnRegister()
+        {
+            KCTEvents.ApplyResearchRateMultiplier.Add(OnEffectQuery);
+        }
+
+        protected override void OnUnregister()
+        {
+            KCTEvents.ApplyResearchRateMultiplier.Remove(OnEffectQuery);
+        }
+
+        protected void OnEffectQuery(Boxed<double> rate, NodeType type, string nodeID)
+        {
+            if ((nodeType & type) != 0)
+                rate.value *= value;
+        }
+    }
+}

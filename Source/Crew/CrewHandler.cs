3180db1b195a2347732ba7b529d0ddb6748e394b
NathanKell <nathankell@gmail.com>
2022-08-05 01:14:12 -0700

    Change static initialization order of CrewHandler so they're done in Awake and there before first OnLoad

--- Source/Crew/CrewHandler.cs
+++ Source/Crew/CrewHandler.cs
@@ -159,6 +159,15 @@ namespace RP0.Crew
             KCT_GUI.AvailabilityChecker = CheckCrewForPart;
 
             _cliTooltip = typeof(CrewListItem).GetField("tooltipController", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (Settings == null)
+            {
+                Settings = new CrewHandlerSettings();
+                foreach (ConfigNode stg in GameDatabase.Instance.GetConfigNodes("CREWHANDLERSETTINGS"))
+                    Settings.Load(stg);
+            }
+
+            TrainingDatabase.EnsureInitialized();
         }
 
         public void Start()
@@ -176,13 +185,6 @@ namespace RP0.Crew
         public override void OnLoad(ConfigNode node)
         {
             base.OnLoad(node);
-
-            if (Settings == null)
-            {
-                Settings = new CrewHandlerSettings();
-                foreach (ConfigNode stg in GameDatabase.Instance.GetConfigNodes("CREWHANDLERSETTINGS"))
-                    Settings.Load(stg);
-            }
 
             if (saveVersion < 1)
             {
@@ -242,7 +244,6 @@ namespace RP0.Crew
 
             saveVersion = VERSION;
 
-            TrainingDatabase.EnsureInitialized();
             KACWrapper.InitKACWrapper();
         }
 

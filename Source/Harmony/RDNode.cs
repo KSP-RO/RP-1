dd0b976c34359dcb9ecb4d85870360b1aa1c45d7
NathanKell <nathankell@gmail.com>
2022-08-04 23:31:42 -0700

    Show cost of researched nodes, too

---
+++ Source/Harmony/RDNode.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using KSP.UI.Screens;
+using KSP.Localization;
+
+namespace RP0.Harmony
+{
+    [HarmonyPatch(typeof(RDNode))]
+    internal class PatchRDNode
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("GetTooltipCaption")]
+        internal static void Prefix_GetTooltipCaption(RDNode __instance, ref string __result)
+        {
+            if (__instance.state == RDNode.State.RESEARCHED)
+            {
+                __result = $"{CurrencyModifierQueryRP0.RunQuery(TransactionReasons.RnDTechResearch, 0f, -__instance.tech.scienceCost, 0f).GetCostLine(true, true)}\n{__result}";
+            }
+        }
+    }
+}

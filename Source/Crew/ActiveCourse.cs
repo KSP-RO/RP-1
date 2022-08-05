c237d9df7415e5f2b4d18b27639e1148793d1900
NathanKell <nathankell@gmail.com>
2022-08-05 01:12:07 -0700

    Change CrewHandler data to be autosave/load, redo ActiveCourse to have a template, not inherit from it

--- Source/Crew/ActiveCourse.cs
+++ Source/Crew/ActiveCourse.cs
@@ -3,89 +3,74 @@ using System.Collections.Generic;
 using UniLinq;
 using System.Text;
 using KerbalConstructionTime;
+using RP0.DataTypes;
 
 namespace RP0.Crew
 {
-    public class ActiveCourse : CourseTemplate, KerbalConstructionTime.IKCTBuildItem
+    public class ActiveCourse : IKCTBuildItem, IConfigNode
     {
-        public List<ProtoCrewMember> Students = new List<ProtoCrewMember>();
+        [Persistent]
+        public string id;
 
+        [Persistent(name = "STUDENTS")]
+        public PersistentParsableList<ProtoCrewMember> Students = new PersistentParsableList<ProtoCrewMember>();
+
+        [Persistent]
         public double elapsedTime = 0;
+
+        [Persistent]
         public double startTime = 0d;
+
+        [Persistent]
+        public bool Started = false;
+
+        [Persistent]
+        public bool Completed = false;
+
+        private CourseTemplate template;
+        public int seatMin => template.seatMin;
+        public int seatMax => template.seatMax;
+        public string description => template.description;
+        public bool isTemporary => template.isTemporary;
+        public ConfigNode RewardLog => template.RewardLog;
+
         public double baseCourseTime = 0d;
-        public bool Started = false, Completed = false;
 
         public ActiveCourse(CourseTemplate template)
         {
-            sourceNode = template.sourceNode;
-            PopulateFromSourceNode(template.sourceNode);
+            id = template.id;
         }
 
         public ActiveCourse(ConfigNode node)
         {
-            FromConfigNode(node);
+            Load(node);
         }
 
-        public ConfigNode AsConfigNode()
+        public void Load(ConfigNode node)
         {
-            //save the source node, the variables, the teacher name, the student names, Started/Completed and the elapsed time
-            ConfigNode node = new ConfigNode("ACTIVE_COURSE");
-            node.AddValue("id", id);
-            node.AddValue("name", name);
-            node.AddValue("startTime", startTime);
-            node.AddValue("elapsedTime", elapsedTime);
-            node.AddValue("Started", Started);
-            node.AddValue("Completed", Completed);
-            ConfigNode studentNode = new ConfigNode("STUDENTS");
-            foreach (ProtoCrewMember student in Students)
-                studentNode.AddValue("student", student.name);
-            node.AddNode("STUDENTS", studentNode);
-
-            node.AddNode("SOURCE_NODE", sourceNode);
-
-            return node;
+            ConfigNode.LoadObjectFromConfig(this, node);
+            template = CrewHandler.Instance.CourseTemplates.Find(c => c.id == id);
         }
 
-        public void FromConfigNode(ConfigNode node)
+        public void Save(ConfigNode node)
         {
-            node.TryGetValue("startTime", ref startTime);
-            node.TryGetValue("elapsedTime", ref elapsedTime);
-            node.TryGetValue("Started", ref Started);
-            node.TryGetValue("Completed", ref Completed);
-
-            //load students
-            ConfigNode studentNode = node.GetNode("STUDENTS");
-            if (studentNode != null)
-            {
-                Students.Clear();
-                foreach (ConfigNode.Value val in studentNode.values)
-                {
-                    if (HighLogic.CurrentGame.CrewRoster.Exists(val.value))
-                    {
-                        Students.Add(HighLogic.CurrentGame.CrewRoster[val.value]);
-                    }
-                }
-            }
-
-            sourceNode = node.GetNode("SOURCE_NODE");
-
-            PopulateFromSourceNode(sourceNode);
+            ConfigNode.CreateConfigFromObject(this, node);
         }
 
         public bool MeetsStudentReqs(ProtoCrewMember student)
         {
-            if (!(student.type == ProtoCrewMember.KerbalType.Crew && (seatMax <= 0 || Students.Count < seatMax) && !student.inactive 
-                && student.rosterStatus == ProtoCrewMember.RosterStatus.Available && student.experienceLevel >= minLevel && student.experienceLevel <= maxLevel 
-                && (classes.Length == 0 || classes.Contains(student.trait)) && !Students.Contains(student)))
+            if (!(student.type == ProtoCrewMember.KerbalType.Crew && (template.seatMax <= 0 || Students.Count < template.seatMax) && !student.inactive 
+                && student.rosterStatus == ProtoCrewMember.RosterStatus.Available && student.experienceLevel >= template.minLevel && student.experienceLevel <= template.maxLevel 
+                && (template.classes.Length == 0 || template.classes.Contains(student.trait)) && !Students.Contains(student)))
                 return false;
 
-            int pCount = preReqs.GetLength(0);
-            int pACount = preReqsAny.GetLength(0);
-            int cCount = conflicts.GetLength(0);
+            int pCount = template.preReqs.GetLength(0);
+            int pACount = template.preReqsAny.GetLength(0);
+            int cCount = template.conflicts.GetLength(0);
             if (pCount > 0 || cCount > 0 || pACount > 0)
             {
                 for (int i = pCount; i-- > 0;)
-                    pChecker[i] = true;
+                    template.pChecker[i] = true;
 
                 int needCount = pCount;
                 bool needAnyStill = pACount > 0;
@@ -98,22 +83,22 @@ namespace RP0.Crew
 
                     for (int preIdx = pCount; preIdx-- > 0 && needCount > 0;)
                     {
-                        if (pChecker[preIdx] && (e.type == preReqs[preIdx, 0] && tgt == preReqs[preIdx, 1]))
+                        if (template.pChecker[preIdx] && (e.type == template.preReqs[preIdx, 0] && tgt == template.preReqs[preIdx, 1]))
                         {
-                            pChecker[preIdx] = false;
+                            template.pChecker[preIdx] = false;
                             --needCount;
                         }
                     }
 
                     for (int anyIdx = pACount; anyIdx-- > 0 && needAnyStill;)
                     {
-                        if (e.type == preReqsAny[anyIdx, 0] && tgt == preReqsAny[anyIdx, 1])
+                        if (e.type == template.preReqsAny[anyIdx, 0] && tgt == template.preReqsAny[anyIdx, 1])
                             needAnyStill = false;
                     }
 
                     for (int conIdx = cCount; conIdx-- > 0;)
                     {
-                        if (e.type == conflicts[conIdx, 0] && tgt == conflicts[conIdx, 1])
+                        if (e.type == template.conflicts[conIdx, 0] && tgt == template.conflicts[conIdx, 1])
                             return false;
                     }
                 }
@@ -125,7 +110,7 @@ namespace RP0.Crew
         }
         public void AddStudent(ProtoCrewMember student)
         {
-            if (seatMax <= 0 || Students.Count < seatMax)
+            if (template.seatMax <= 0 || Students.Count < template.seatMax)
             {
                 if (!Students.Contains(student))
                     Students.Add(student);
@@ -158,17 +143,17 @@ namespace RP0.Crew
             RemoveStudent(HighLogic.CurrentGame.CrewRoster[student]);
         }
 
-        public override double GetTime(List<ProtoCrewMember> students)
+        public double GetTime(List<ProtoCrewMember> students)
         {
             if (Started)
             {
                 if (baseCourseTime == 0d)
-                    baseCourseTime = GetBaseTime(students);
+                    baseCourseTime = template.GetBaseTime(students);
 
-                return baseCourseTime * GetTimeMultiplierFacility() / CurrencyUtils.Rate(TransactionReasonsRP0.RateTraining);
+                return baseCourseTime * template.GetTimeMultiplierFacility() / CurrencyUtils.Rate(TransactionReasonsRP0.RateTraining);
             }
 
-            return base.GetTime(students);
+            return template.GetTime(students);
         }
 
         public double GetTime()
@@ -218,9 +203,9 @@ namespace RP0.Crew
 
                     double retireTimeOffset = 0d;
                     
-                    if (ExpireLog != null)
+                    if (template.ExpireLog != null)
                     {
-                        foreach (ConfigNode.Value v in ExpireLog.values)
+                        foreach (ConfigNode.Value v in template.ExpireLog.values)
                         {
                             for (int i = student.careerLog.Count; i-- > 0;)
                             {
@@ -235,18 +220,18 @@ namespace RP0.Crew
                         }
                     }
 
-                    if (RewardLog != null)
+                    if (template.RewardLog != null)
                     {
                         if (student.flightLog.Count > 0)
                             student.ArchiveFlightLog();
 
                         TrainingExpiration exp = null;
-                        if (expiration > 0d)
+                        if (template.expiration > 0d)
                         {
                             exp = new TrainingExpiration();
                             exp.PcmName = student.name;
-                            exp.Expiration = expiration;
-                            if (expirationUseStupid)
+                            exp.Expiration = template.expiration;
+                            if (template.expirationUseStupid)
                                 exp.Expiration *= UtilMath.Lerp(CrewHandler.Settings.trainingProficiencyStupidMin,
                                     CrewHandler.Settings.trainingProficiencyStupidMax,
                                     student.stupidity);
@@ -255,7 +240,7 @@ namespace RP0.Crew
 
                         bool prevMissionsAlreadyExpired = false;
                         int lastEntryIndex = student.careerLog.Entries.Count - 1;
-                        foreach (ConfigNode.Value v in RewardLog.values)
+                        foreach (ConfigNode.Value v in template.RewardLog.values)
                         {
                             string[] s = v.value.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
                             string trainingType = s[0];
@@ -281,16 +266,16 @@ namespace RP0.Crew
 
                             student.flightLog.AddEntry(trainingType, trainingTarget);
                             student.ArchiveFlightLog();
-                            if (expiration > 0d)
+                            if (template.expiration > 0d)
                                 exp.Entries.Add(v.value);
                         }
 
-                        if (expiration > 0d)
+                        if (template.expiration > 0d)
                             CrewHandler.Instance.AddExpiration(exp);
                     }
 
-                    if (rewardXP != 0)
-                        student.ExtraExperience += rewardXP;
+                    if (template.rewardXP != 0)
+                        student.ExtraExperience += template.rewardXP;
 
                     double retireOffset = CrewHandler.Instance.IncreaseRetireTime(student.name, retireTimeOffset);
                     if(retireOffset > 0d)
@@ -300,7 +285,7 @@ namespace RP0.Crew
                 if (CrewHandler.Instance.RetirementEnabled && retirementChanges.Count > 0)
                 {
                     StringBuilder sb = new StringBuilder();
-                    sb.Append($"{name} training completed! The following retirement changes have occurred:");
+                    sb.Append($"{template.name} training completed! The following retirement changes have occurred:");
                     foreach (string s in retirementChanges)
                         sb.Append(s);
 
@@ -329,9 +314,9 @@ namespace RP0.Crew
 
             //ensure we have more than the minimum number of students and not more than the maximum number
             int studentCount = Students.Count;
-            if (seatMax > 0 && studentCount > seatMax)
+            if (template.seatMax > 0 && studentCount > template.seatMax)
                 return false;
-            if (seatMin > 0 && studentCount < seatMin)
+            if (template.seatMin > 0 && studentCount < template.seatMin)
                 return false;
 
             Started = true;
@@ -348,7 +333,7 @@ namespace RP0.Crew
 
         public string GetItemName()
         {
-            return name;
+            return template.name;
         }
 
         public double GetBuildRate()

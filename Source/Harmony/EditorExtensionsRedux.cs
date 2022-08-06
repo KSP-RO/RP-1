0535da3bf1a1c853d541fad6515c1ae55e82e150
NathanKell <nathankell@gmail.com>
2022-08-06 01:41:57 -0700

    Fix EER and DV patches

---
+++ Source/Harmony/EditorExtensionsRedux.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace RP0.Harmony
+{
+    [HarmonyPatch]
+    internal class PatchEditorExtensionsRedux_Start
+    {
+        static MethodBase TargetMethod() => AccessTools.TypeByName("EditorExtensionsRedux.EditorExtensions")?.GetMethod("Start", AccessTools.all);
+
+        internal static System.Type EEXType = AccessTools.TypeByName("EditorExtensionsRedux.EditorExtensions");
+
+        [HarmonyPrepare]
+        internal static bool Prepare()
+        {
+            return EEXType != null;
+        }
+
+        internal static KeyBinding toggleSym = new KeyBinding(KeyCode.None);
+        internal static KeyBinding toggleSnap = new KeyBinding(KeyCode.None);
+
+
+        [HarmonyPrefix]
+        internal static void Prefix_Start()
+        {
+            if (EEXType != null)
+            {
+                toggleSym.primary = GameSettings.Editor_toggleSymMode.primary;
+                toggleSym.secondary = GameSettings.Editor_toggleSymMode.secondary;
+
+                toggleSnap.primary = GameSettings.Editor_toggleAngleSnap.primary;
+                toggleSnap.secondary = GameSettings.Editor_toggleAngleSnap.secondary;
+            }
+        }
+    }
+}

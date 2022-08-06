42791d7a4da47065cfb81b85cb1a90c2a88466cd
siimav <siim.aaver@gmail.com>
2022-08-06 14:52:55 +0300

    Remove a bunch of (hopefully?!) unnecessary autosaves

---
+++ Source/Harmony/AdministrationSceneSpawner.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using KSP.UI;
+using KSP.UI.Screens;
+
+namespace RP0.Harmony
+{
+    [HarmonyPatch(typeof(AdministrationSceneSpawner))]
+    internal class PatchAdministrationSceneSpawner
+    {
+        /// <summary>
+        /// Removes autosaving when closing the Admin building overlay.
+        /// </summary>
+        /// <param name="__instance"></param>
+        /// <returns></returns>
+        [HarmonyPrefix]
+        [HarmonyPatch("onAdminDespawn")]
+        internal static bool Prefix_onAdminDespawn(AdministrationSceneSpawner __instance)
+        {
+            UIMasterController.Instance.RemoveCanvas(__instance.AdministrationScreenPrefab);
+            MusicLogic.fetch.UnpauseWithCrossfade();
+            return false;
+        }
+    }
+}

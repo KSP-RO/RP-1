fabb3b5bba320500de029b6f3e35b45a8d203c53
siimav <siim.aaver@gmail.com>
2022-08-06 14:52:55 +0300

    Remove a bunch of (hopefully?!) unnecessary autosaves

---
+++ Source/Harmony/SpaceTracking.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using KSP.UI.Screens;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace RP0.Harmony
+{
+    [HarmonyPatch(typeof(SpaceTracking))]
+    internal class PatchSpaceTracking
+    {
+        [HarmonyTranspiler]
+        [HarmonyPatch("OnVesselDeleteConfirm")]
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            int startIndex = -1;
+            int endIndex = -1;
+
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldstr &&
+                    codes[i].operand as string == "persistent")
+                {
+                    startIndex = i;
+
+                    for (int j = startIndex; j < codes.Count; j++)
+                    {
+                        if (codes[j].opcode == OpCodes.Ldarg_0)
+                        {
+                            endIndex = j;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+
+            if (startIndex > -1 && endIndex > -1)
+            {
+                // Cuts out the section about GamePersistence.SaveGame()
+                codes.RemoveRange(startIndex, endIndex - startIndex);
+            }
+
+            return codes.AsEnumerable();
+        }
+    }
+}

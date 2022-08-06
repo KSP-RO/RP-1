ef53c60fcc9e5caa6c735e3e147bd3339b0d79f6
siimav <siim.aaver@gmail.com>
2022-08-06 14:52:55 +0300

    Remove a bunch of (hopefully?!) unnecessary autosaves

---
+++ Source/Harmony/MCSceneSpawner.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using KSP.UI;
+
+namespace RP0.Harmony
+{
+    [HarmonyPatch(typeof(MCSceneSpawner))]
+    internal class PatchMCSceneSpawner
+    {
+        /// <summary>
+        /// Removes autosaving when closing the Mission Control overlay.
+        /// </summary>
+        /// <param name="__instance"></param>
+        /// <returns></returns>
+        [HarmonyPrefix]
+        [HarmonyPatch("OnMCDespawn")]
+        internal static bool Prefix_OnMCDespawn(MCSceneSpawner __instance)
+        {
+            MusicLogic.fetch.UnpauseWithCrossfade();
+            UIMasterController.Instance.RemoveCanvas(__instance.missionControlPrefab);
+            return false;
+        }
+    }
+}

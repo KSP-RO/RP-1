e0701c9d549e2f472e5f4886dacc3f44f1a19619
NathanKell <nathankell@gmail.com>
2022-08-10 00:21:25 -0700

    Set LCID for simmed vessels

--- Source/KerbalConstructionTime/GUI/GUI_Simulation.cs
+++ Source/KerbalConstructionTime/GUI/GUI_Simulation.cs
@@ -229,7 +229,9 @@ namespace KerbalConstructionTime
             bool humanRated;
             double effCost = Utilities.GetEffectiveCost(EditorLogic.fetch.ship.Parts, out humanRated);
             double bp = Utilities.GetVesselBuildPoints(effCost);
+
             KCTGameStates.LaunchedVessel = new BuildListVessel(EditorLogic.fetch.ship, EditorLogic.fetch.launchSiteName, effCost, bp, EditorLogic.FlagURL, humanRated);
+            KCTGameStates.LaunchedVessel.LCID = KCTGameStates.EditorShipEditingMode ? KCTGameStates.EditedVessel.LCID : KCTGameStates.ActiveKSC.ActiveLaunchComplexInstance.ID;
 
             VesselCrewManifest manifest = KSP.UI.CrewAssignmentDialog.Instance.GetManifest();
             if (manifest == null)

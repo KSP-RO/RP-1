f73d7841c7a6cde928a0bb112f03a776c68a0546
NathanKell <nathankell@gmail.com>
2022-08-12 02:28:47 -0700

    Mass convert System.Linq to Unilinq

--- Source/KerbalConstructionTime/KCTDataLoader.cs
+++ Source/KerbalConstructionTime/KCTDataLoader.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
+using UniLinq;
 using UnityEngine;
 
 namespace KerbalConstructionTime
--- Source/Utilities/ContractUtils.cs
+++ Source/Utilities/ContractUtils.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
+using UniLinq;
 using System.Reflection;
 
 namespace RP0

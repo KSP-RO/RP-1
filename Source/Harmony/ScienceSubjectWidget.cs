3927849f07f25374db8b0e7fca6dfabb33ec4ca1
NathanKell <nathankell@gmail.com>
2022-08-15 21:59:03 -0700

    Replace flask with atom in Mission Recovery dialog for science

---
+++ Source/Harmony/ScienceSubjectWidget.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using KSP.UI.Screens.SpaceCenter.MissionSummaryDialog;
+using UnityEngine.UI;
+using KSP.UI.Util;
+using System.Reflection;
+
+namespace RP0.Harmony
+{
+    [HarmonyPatch(typeof(ScienceSubjectWidget))]
+    internal class PatchScienceSubjectWidget
+    {
+        internal static FieldInfo image = typeof(ImgText).GetField("imgComponent", AccessTools.all);
+
+        [HarmonyPostfix]
+        [HarmonyPatch("UpdateFields")]
+        internal static void Postfix_UpdateFields(ScienceSubjectWidget __instance)
+        {
+            (image.GetValue(__instance.scienceWidgetScienceContent) as Image).gameObject.SetActive(false);
+            if (!__instance.scienceWidgetScienceContent.text.StartsWith("<sprite"))
+                __instance.scienceWidgetScienceContent.text = "<sprite=\"CurrencySpriteAsset\" name=\"Science\" tint=1> " + __instance.scienceWidgetScienceContent.text;
+        }
+    }
+}

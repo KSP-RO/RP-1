5216117b93746d64d0d1ef5f20dc7c5509b6af11
NathanKell <nathankell@gmail.com>
2022-08-16 00:12:22 -0700

    Start adding our own design concerns, speculative fix to existing design concern (unfed consumer)

---
+++ Source/DesignConcerns/UntooledParts.cs
@@ -0,0 +1,50 @@
+﻿using PreFlightTests;
+using System;
+using KSP.Localization;
+
+namespace RP0.DesignConcerns
+{
+    public class UntooledParts : DesignConcernBase
+    {
+        private int failureCount = 0;
+        public override string GetConcernDescription()
+        {
+            if (failureCount < 2)
+                return Localizer.GetStringByTag("#pr0ER_Concern_UntooledParts_DescriptionSingle");
+
+            return Localizer.Format("#pr0ER_Concern_UntooledParts_DescriptionMany", failureCount.ToString());
+        }
+
+        public override string GetConcernTitle()
+        {
+            return Localizer.GetStringByTag("#pr0ER_Concern_UntooledParts_Title");
+        }
+
+        public override DesignConcernSeverity GetSeverity()
+        {
+            return DesignConcernSeverity.WARNING;
+        }
+
+        public override bool TestCondition()
+        {
+            failureCount = 0;
+
+            if (EditorLogic.fetch.ship == null)
+                return true;
+
+            foreach (Part p in EditorLogic.fetch.ship.parts)
+            {
+                foreach (PartModule pm in p.Modules)
+                {
+                    if (pm is ModuleTooling tooling)
+                    {
+                        if (!tooling.IsUnlocked())
+                            ++failureCount;
+                    }
+                }
+            }
+
+            return failureCount == 0;
+        }
+    }
+}

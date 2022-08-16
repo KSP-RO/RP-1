89275fe6ccd4a35f205508210a309fe0e383baa1
NathanKell <nathankell@gmail.com>
2022-08-16 00:11:42 -0700

    Create static events immediately

--- Source/Maintenance/MaintenanceHandler.cs
+++ Source/Maintenance/MaintenanceHandler.cs
@@ -30,7 +30,7 @@ namespace RP0
         private static readonly Dictionary<SpaceCenterFacility, float[]> _facilityLevelCosts = new Dictionary<SpaceCenterFacility, float[]>();
         public static void ClearFacilityCosts() { _facilityLevelCosts.Clear(); }
 
-        public static EventVoid OnRP0MaintenanceChanged;
+        public static EventVoid OnRP0MaintenanceChanged = new EventVoid("OnRP0MaintenanceChanged");
 
         [KSPField(isPersistant = true)]
         public double nextUpdate = -1d;

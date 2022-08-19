32336f8d06a7a20bcf2d41a9cb6b5099f52ade79
NathanKell <nathankell@gmail.com>
2022-08-18 23:38:22 -0700

    Localize all our "OK" buttons

--- Source/GameplayTips.cs
+++ Source/GameplayTips.cs
@@ -94,7 +94,7 @@ namespace RP0
                                          "ShowInterplanetaryAvionicsReminder",
                                          "Deep Space Avionics",
                                          msg,
-                                         "OK",
+                                         KSP.Localization.Localizer.GetStringByTag("#autoLOC_190905"),
                                          false,
                                          HighLogic.UISkin);
         }
@@ -111,7 +111,7 @@ namespace RP0
                                          "ShowAirlaunchTip",
                                          "Simulate airlaunch",
                                          msg,
-                                         "OK",
+                                         KSP.Localization.Localizer.GetStringByTag("#autoLOC_190905"),
                                          false,
                                          HighLogic.UISkin);
         }
@@ -131,7 +131,7 @@ namespace RP0
         {
             string msg = $"Tool them in the RP-1 menu to reduce vessel cost and build time.";
             DialogGUIBase[] options = new DialogGUIBase[2];
-            options[0] = new DialogGUIButton("OK", () => { });
+            options[0] = new DialogGUIButton(KSP.Localization.Localizer.GetStringByTag("#autoLOC_190905"), () => { });
             options[1] = new DialogGUIButton("Never remind me again", () => { HighLogic.CurrentGame.Parameters.CustomParams<RP0Settings>().NeverShowToolingReminders = true; });
             MultiOptionDialog diag = new MultiOptionDialog("ShowUntooledPartsReminder", msg, "Untooled parts", null, 300, options);
             PopupDialog.SpawnPopupDialog(diag, false, HighLogic.UISkin);
@@ -151,7 +151,7 @@ namespace RP0
                                          "ShowRealChuteTip",
                                          "Configuring parachutes",
                                          msg,
-                                         "OK",
+                                         KSP.Localization.Localizer.GetStringByTag("#autoLOC_190905"),
                                          false,
                                          HighLogic.UISkin);
         }

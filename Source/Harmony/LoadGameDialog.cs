0c54eebd3bb27c3980e65d38c26581691acf9573
NathanKell <nathankell@gmail.com>
2022-08-19 21:38:35 -0700

    Remove no-longer-needed reflection on methods

---
+++ Source/Harmony/LoadGameDialog.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using KSP.UI.Screens;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace RP0.Harmony
+{
+    // Patching this with a transpiler to show confidence is gonna be HARD.
+    // Punting for now.
+
+    //[HarmonyPatch(typeof(LoadGameDialog))]
+    //internal class PatchLoadGameDialog
+    //{
+    //    [HarmonyTranspiler]
+    //    [HarmonyPatch("CreateLoadList")]
+    //    internal static IEnumerable<CodeInstruction> Transpiler_LoadDetailsFromGame(IEnumerable<CodeInstruction> instructions)
+    //    {
+    //        foreach (var instruction in instructions)
+    //        {
+    //            if (instruction.LoadsConstant(10f))
+    //                yield return new CodeInstruction(System.Reflection.Emit.OpCodes.Ldc_R4, 1f);
+    //            else
+    //                yield return instruction;
+    //        }
+    //    }
+    //}
+}

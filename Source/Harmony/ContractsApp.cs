17879a218cf670879c3c16daaaa261f319e90cf1
NathanKell <nathankell@gmail.com>
2022-08-19 22:24:20 -0700

    Remove a bunch of reflection and Harmony private-field-passing in favor of just accessing the private fields.

--- Source/Harmony/ContractsApp.cs
+++ Source/Harmony/ContractsApp.cs
@@ -14,7 +14,7 @@ namespace RP0.Harmony
     {
         [HarmonyPrefix]
         [HarmonyPatch("CreateItem")]
-        internal static bool Prefix_CreateItem(ContractsApp __instance, ref Contract contract, ref KSP.UI.UICascadingList.CascadingListItem __result, ref GenericCascadingList ___cascadingList, ref Dictionary<Guid, UICascadingList.CascadingListItem> ___contractList)
+        internal static bool Prefix_CreateItem(ContractsApp __instance, ref Contract contract, ref UICascadingList.CascadingListItem __result)
         {
             if (contract is ContractConfigurator.ConfiguredContract configuredContract)
             {
@@ -26,13 +26,13 @@ namespace RP0.Harmony
                 int index = -1;
 
                 // Find the index of the first record.
-                foreach (var kvp in ___contractList)
+                foreach (var kvp in __instance.contractList)
                 {
                     if (ContractSystem.Instance.GetContractByGuid(kvp.Key) is ContractConfigurator.ConfiguredContract cc)
                     {
                         if (ContractUtils.ContractIsRecord(cc))
                         {
-                            int idx = ___cascadingList.ruiList.cascadingList.GetIndex(kvp.Value.header);
+                            int idx = __instance.cascadingList.ruiList.cascadingList.GetIndex(kvp.Value.header);
                             if (idx < 0)
                             {
                                 Debug.LogError($"[RP-0] ContractsApp patcher: {cc.contractType.name} can't be found in UI list despite its guid existing in dictionary!");
@@ -44,8 +44,8 @@ namespace RP0.Harmony
                     }
                 }
 
-                UIListItem header = ___cascadingList.CreateHeader("<color=#e6752a>" + contract.Title + "</color>", out button, scaleBg: true);
-                __result = ___cascadingList.ruiList.AddCascadingItem(header, ___cascadingList.CreateFooter(), __instance.CreateParameterList(contract), button, index);
+                UIListItem header = __instance.cascadingList.CreateHeader("<color=#e6752a>" + contract.Title + "</color>", out button, scaleBg: true);
+                __result = __instance.cascadingList.ruiList.AddCascadingItem(header, __instance.cascadingList.CreateFooter(), __instance.CreateParameterList(contract), button, index);
                 return false;
             }
 

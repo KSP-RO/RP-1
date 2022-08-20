bd369149ada249f30038d77cc06f41dc262422a0
NathanKell <nathankell@gmail.com>
2022-08-20 13:54:16 -0700

    Refactor some cost handling, change VesselRollout to VesselPurchase

--- Source/Harmony/Funding.cs
+++ Source/Harmony/Funding.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using KSP.UI.Screens.SpaceCenter.MissionSummaryDialog;
+using System.Collections.Generic;
 
 namespace RP0.Harmony
 {
@@ -21,6 +23,74 @@ namespace RP0.Harmony
 
             GameEvents.OnFundsChanged.Fire(__instance.funds, reason);
 
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch("onVesselRecoveryProcessing")]
+        internal static bool Prefix_onVesselRecoveryProcessing(Funding __instance, ProtoVessel pv, KSP.UI.Screens.MissionRecoveryDialog mrDialog, ref float recoveryScore)
+        {
+            if (pv == null)
+                return false;
+
+            if (!KerbalConstructionTime.Utilities.IsVesselKCTRecovering(pv))
+                return true;
+
+            if (mrDialog == null)
+                return false;
+
+            List<ProtoPartSnapshot> allProtoPartsIncludingCargo = pv.GetAllProtoPartsIncludingCargo();
+            for (int i = 0; i < allProtoPartsIncludingCargo.Count; i++)
+            {
+                ProtoPartSnapshot protoPartSnapshot = allProtoPartsIncludingCargo[i];
+                AvailablePart availablePart = null;
+                if (protoPartSnapshot.partInfo == null)
+                {
+                    if (!string.IsNullOrEmpty(protoPartSnapshot.partName))
+                    {
+                        availablePart = PartLoader.getPartInfoByName(protoPartSnapshot.partName);
+                    }
+                }
+                else
+                {
+                    availablePart = protoPartSnapshot.partInfo;
+                }
+                if (availablePart != null)
+                {
+                    if (!string.Equals(availablePart.name, "kerbalEVA"))
+                    {
+                        mrDialog.AddPartWidget(PartWidget.Create(availablePart, 0, 0, mrDialog));
+                    }
+                    for (int j = 0; j < protoPartSnapshot.resources.Count; j++)
+                    {
+                        ProtoPartResourceSnapshot protoPartResourceSnapshot = protoPartSnapshot.resources[j];
+                        PartResourceDefinition definition = PartResourceLibrary.Instance.GetDefinition(protoPartResourceSnapshot.resourceName);
+                        if (definition != null)
+                        {
+                            mrDialog.AddResourceWidget(ResourceWidget.Create(definition, (float)protoPartResourceSnapshot.amount, 0, mrDialog));
+                        }
+                    }
+                }
+            }
+            mrDialog.fundsEarned = 0f;
+
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch("onVesselRollout")]
+        internal static bool Prefix_onVesselRollout(Funding __instance, ShipConstruct ship)
+        {
+            __instance.AddFunds(-KerbalConstructionTime.Utilities.GetTotalVesselCost(ship.Parts), TransactionReasonsRP0.VesselPurchase.Stock());
+            return false;
+        }
+
+        // For future use
+        [HarmonyPrefix]
+        [HarmonyPatch("onCrewHired")]
+        internal static bool Prefix_onCrewHired(Funding __instance, ProtoCrewMember crew, int crewCount)
+        {
+            __instance.AddFunds(-GameVariables.Instance.GetRecruitHireCost(crewCount), TransactionReasons.CrewRecruited);
             return false;
         }
     }

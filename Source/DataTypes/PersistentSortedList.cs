2c4b117d2cdf823c491f1dd61f6b71b2c9022cd5
NathanKell <nathankell@gmail.com>
2022-08-21 22:13:48 -0700

    Fix Dictionary/SortedList persistence because node names can't have spaces.

--- Source/DataTypes/PersistentSortedList.cs
+++ Source/DataTypes/PersistentSortedList.cs
@@ -12,22 +12,41 @@ namespace RP0.DataTypes
         public void Load(ConfigNode node)
         {
             Clear();
-            foreach (ConfigNode n in node.nodes)
+            if (node.values.Count == 0 || node.nodes.Count == 0 || node.values[0].name != "version")
             {
-                TKey key = ParseKey(n.name);
+                foreach (ConfigNode n in node.nodes)
+                {
+                    TKey key = ParseKey(n.name);
+                    TValue value = System.Activator.CreateInstance<TValue>();
+                    value.Load(n);
+                    Add(key, value);
+                }
+                return;
+            }
+
+            ConfigNode keyNode = node.nodes[0];
+            ConfigNode valueNode = node.nodes[1];
+            for (int i = 0; i < keyNode.values.Count; ++i)
+            {
+                TKey key = ParseKey(keyNode.values[i].value);
                 TValue value = System.Activator.CreateInstance<TValue>();
-                value.Load(n);
+                value.Load(valueNode.nodes[i]);
                 Add(key, value);
             }
         }
 
         public void Save(ConfigNode node)
         {
+            node.AddValue("version", 1);
+            ConfigNode keyNode = node.AddNode("Keys");
+            ConfigNode valueNode = node.AddNode("Values");
+
             foreach (var kvp in this)
             {
-                ConfigNode n = new ConfigNode(kvp.Key.ToString());
+                keyNode.AddValue("key", kvp.Key.ToString());
+                ConfigNode n = new ConfigNode("VALUE");
                 kvp.Value.Save(n);
-                node.AddNode(n);
+                valueNode.AddNode(n);
             }
         }
     }
@@ -71,38 +90,76 @@ namespace RP0.DataTypes
         public void Load(ConfigNode node)
         {
             Clear();
-            foreach (ConfigNode.Value v in node.values)
+            if (node.values.Count == 0 || node.nodes.Count == 0 || node.values[0].name != "version")
+            {
+                foreach (ConfigNode.Value v in node.values)
+                {
+                    TKey key;
+                    if (KeyType == typeof(Guid))
+                    {
+                        object o = new Guid(v.value);
+                        key = (TKey)o;
+                    }
+                    else
+                    {
+                        key = (TKey)ConfigNode.ReadValue(KeyType, v.name);
+                    }
+
+                    TValue value;
+                    if (ValueType == typeof(Guid))
+                    {
+                        object o = new Guid(v.value);
+                        value = (TValue)o;
+                    }
+                    else
+                    {
+                        value = (TValue)ConfigNode.ReadValue(ValueType, v.value);
+                    }
+                    Add(key, value);
+                }
+                return;
+            }
+
+            ConfigNode keyNode = node.nodes[0];
+            ConfigNode valueNode = node.nodes[1];
+            for (int i = 0; i < keyNode.values.Count; ++i)
             {
                 TKey key;
                 if (KeyType == typeof(Guid))
                 {
-                    object o = new Guid(v.value);
+                    object o = new Guid(keyNode.values[i].value);
                     key = (TKey)o;
                 }
                 else
                 {
-                    key = (TKey)ConfigNode.ReadValue(KeyType, v.name);
+                    key = (TKey)ConfigNode.ReadValue(KeyType, keyNode.values[i].value);
                 }
 
                 TValue value;
                 if (ValueType == typeof(Guid))
                 {
-                    object o = new Guid(v.value);
+                    object o = new Guid(valueNode.values[i].value);
                     value = (TValue)o;
                 }
                 else
                 {
-                    value = (TValue)ConfigNode.ReadValue(ValueType, v.value);
+                    value = (TValue)ConfigNode.ReadValue(ValueType, valueNode.values[i].value);
                 }
+
                 Add(key, value);
             }
         }
 
         public void Save(ConfigNode node)
         {
+            node.AddValue("version", 1);
+            ConfigNode keyNode = node.AddNode("Keys");
+            ConfigNode valueNode = node.AddNode("Values");
+
             foreach (var kvp in this)
             {
-                node.AddValue(kvp.Key.ToString(), kvp.Value.ToString());
+                keyNode.AddValue("key", kvp.Key.ToString());
+                valueNode.AddValue("value", kvp.Value.ToString());
             }
         }
 

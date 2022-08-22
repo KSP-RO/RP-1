a73d65747a0c5e401787cd9e04d3ef748e44e327
NathanKell <nathankell@gmail.com>
2022-08-21 18:31:20 -0700

    LCItem is now IConfigNode

--- Source/KerbalConstructionTime/LCItem.cs
+++ Source/KerbalConstructionTime/LCItem.cs
@@ -8,29 +8,49 @@ namespace KerbalConstructionTime
 {
     public class LCItem : IConfigNode
     {
-        public string Name;
-        protected Guid _id;
-        public Guid ID => _id;
-        protected Guid _modID;
-        public Guid ModID => _modID;
-        public KCTObservableList<BuildListVessel> BuildList = new KCTObservableList<BuildListVessel>();
-        public KCTObservableList<BuildListVessel> Warehouse = new KCTObservableList<BuildListVessel>();
-        public KCTObservableList<PadConstruction> PadConstructions = new KCTObservableList<PadConstruction>();
-        public PersistentList<ReconRollout> Recon_Rollout = new PersistentList<ReconRollout>();
-        public PersistentList<AirlaunchPrep> Airlaunch_Prep = new PersistentList<AirlaunchPrep>();
+        public const int MinEngineersConst = 1;
+        public const int EngineersPerPacket = 10;
 
+        [Persistent(name = "LCName")]
+        public string Name;
+        [Persistent(name = "id")]
+        private Guid _id;
+        public Guid ID => _id;
+        [Persistent(name = "modID")]
+        private Guid _modID;
+        public Guid ModID => _modID;
+        [Persistent]
+        public int Engineers = 0;
+        [Persistent]
+        public bool IsRushing;
+        [Persistent(name = "operational")]
+        public bool IsOperational = false;
+        [Persistent]
+        public int ActiveLaunchPadIndex = 0;
+
+        [Persistent(name = "Stats")]
         private LCData _lcData = new LCData();
         public LCData Stats => _lcData;
+
+
+        [Persistent]
+        public PersistentList<KCT_LaunchPad> LaunchPads = new PersistentList<KCT_LaunchPad>();
+        [Persistent]
+        public KCTObservableList<BuildListVessel> BuildList = new KCTObservableList<BuildListVessel>();
+        [Persistent]
+        public KCTObservableList<BuildListVessel> Warehouse = new KCTObservableList<BuildListVessel>();
+        [Persistent]
+        public KCTObservableList<PadConstruction> PadConstructions = new KCTObservableList<PadConstruction>();
+        [Persistent]
+        public PersistentList<ReconRollout> Recon_Rollout = new PersistentList<ReconRollout>();
+        [Persistent]
+        public PersistentList<AirlaunchPrep> Airlaunch_Prep = new PersistentList<AirlaunchPrep>();
 
         private double _rate;
         private double _rateHRCapped;
         public double Rate => _rate;
         public double RateHRCapped => _rateHRCapped;
-
-        public const int MinEngineersConst = 1;
-        public const int EngineersPerPacket = 10;
         
-        public int Engineers = 0;
         private static double RawMaxEngineers(float massMax, Vector3 sizeMax) =>
             massMax != float.MaxValue ? Math.Pow(massMax, 0.75d) : sizeMax.sqrMagnitude * 0.01d;
         public static int MaxEngineersCalc(float massMax, Vector3 sizeMax, bool isHuman) => 
@@ -52,7 +72,7 @@ namespace KerbalConstructionTime
         }
         public int MaxEngineersFor(BuildListVessel blv) => blv == null ? MaxEngineers : MaxEngineersFor(blv.GetTotalMass(), blv.buildPoints + blv.integrationPoints, blv.humanRated);
 
-        protected double _strategyRateMultiplier = 1d;
+        private double _strategyRateMultiplier = 1d;
         public double StrategyRateMultiplier => _strategyRateMultiplier;
 
         private LCEfficiency _efficiencySource = null;
@@ -80,11 +100,8 @@ namespace KerbalConstructionTime
             }
         }
 
-        public bool IsRushing;
         public double RushRate => IsRushing ? PresetManager.Instance.ActivePreset.GeneralSettings.RushRateMult : 1d;
         public double RushSalary => IsRushing ? PresetManager.Instance.ActivePreset.GeneralSettings.RushSalaryMult : 1d;
-
-        public bool IsOperational = false;
 
         public LaunchComplexType LCType => _lcData.lcType;
         public bool IsHumanRated => _lcData.isHumanRated;
@@ -93,9 +110,7 @@ namespace KerbalConstructionTime
         public float MassMin => _lcData.MassMin;
         public Vector3 SizeMax => _lcData.sizeMax;
         public PersistentDictionaryValueTypes<string, double> ResourcesHandled => _lcData.resourcesHandled;
-
-        public List<KCT_LaunchPad> LaunchPads = new List<KCT_LaunchPad>();
-        public int ActiveLaunchPadIndex = 0;
+                
 
         public static string SupportedMassAsPrettyTextCalc(float mass) => mass == float.MaxValue ? "unlimited" : $"{LCData.CalcMassMin(mass):N0}-{mass:N0}t";
         public string SupportedMassAsPrettyText => SupportedMassAsPrettyTextCalc(MassMax);
@@ -123,11 +138,7 @@ namespace KerbalConstructionTime
         }
         #endregion
 
-        public LCItem(KSCItem ksc)
-        {
-            _ksc = ksc;
-            AddListeners();
-        }
+        public LCItem() { } // does not add listeners, instead adds them in Load.
 
         public LCItem(LCData lcData, KSCItem ksc)
         {
@@ -324,190 +335,30 @@ namespace KerbalConstructionTime
                 LCEfficiency.ClearEmpty();
         }
 
-        public ConfigNode AsConfigNode()
+        public void Save(ConfigNode node)
         {
-            KCTDebug.Log("Saving LC " + Name);
-            var node = new ConfigNode("LaunchComplex");
-            node.AddValue("LCName", Name);
-            node.AddValue("ActiveLPID", ActiveLaunchPadIndex);
-            node.AddValue("operational", IsOperational);
-            node.AddValue("id", _id);
-            node.AddValue("modID", _modID);
-            node.AddValue("Engineers", Engineers);
-            node.AddValue("IsRushing", IsRushing);
-
-            var statsNode = new ConfigNode("Stats");
-            ConfigNode.CreateConfigFromObject(_lcData, statsNode);
-            node.AddNode(statsNode);
-
-            var cnBuildl = new ConfigNode("BuildList");
-            BuildList.Save(cnBuildl);
-            node.AddNode(cnBuildl);
-
-            var cnWh = new ConfigNode("Warehouse");
-            Warehouse.Save(cnWh);
-            node.AddNode(cnWh);
-
-            var cnPadConstructions = new ConfigNode("PadConstructions");
-            PadConstructions.Save(cnPadConstructions);
-            node.AddNode(cnPadConstructions);
-
-            var cnRR = new ConfigNode("Recon_Rollout");
-            Recon_Rollout.Save(cnRR);
-            node.AddNode(cnRR);
-
-            var cnAP = new ConfigNode("Airlaunch_Prep");
-            Airlaunch_Prep.Save(cnAP);
-            node.AddNode(cnAP);
-
-            var cnLPs = new ConfigNode("LaunchPads");
-            foreach (KCT_LaunchPad lp in LaunchPads)
-            {
-                ConfigNode lpCN = new ConfigNode("KCT_LauncHPad");
-                lp.Save(lpCN);
-                cnLPs.AddNode(lpCN);
-            }
-            node.AddNode(cnLPs);
-
-            return node;
+            ConfigNode.CreateConfigFromObject(this, node);
         }
 
-        public LCItem FromConfigNode(ConfigNode node)
+        public void Load(ConfigNode node)
         {
-            BuildList.Clear();
-            Warehouse.Clear();
-            PadConstructions.Clear();
-            Recon_Rollout.Clear();
-            Airlaunch_Prep.Clear();
-            LaunchPads.Clear();
-            _rate = 0;
-            _rateHRCapped = 0;
-            Engineers = 0;
-            IsRushing = false;
+            ConfigNode.LoadObjectFromConfig(this, node);
 
-            Name = node.GetValue("LCName");
-            ActiveLaunchPadIndex = 0;
-            node.TryGetValue("ActiveLPID", ref ActiveLaunchPadIndex);
-            if (ActiveLaunchPadIndex < 0)
-                ActiveLaunchPadIndex = 0;
-            node.TryGetValue("operational", ref IsOperational);
-            node.TryGetValue("id", ref _id);
-            if (!node.TryGetValue("modID", ref _modID) || _modID == (new Guid()) )
-                _modID = Guid.NewGuid();
-            node.TryGetValue("Engineers", ref Engineers);
-            node.TryGetValue("IsRushing", ref IsRushing);
-
-            ConfigNode tmp = node.GetNode("Stats");
-            if (tmp != null)
-                ConfigNode.LoadObjectFromConfig(_lcData, tmp);
-
-            BuildList.Load(node.GetNode("BuildList"));
-            foreach (var blv in BuildList)
-                blv.LinkToLC(this);
-
-            Warehouse.Load(node.GetNode("Warehouse"));
-            foreach (var blv in Warehouse)
-                blv.LinkToLC(this);
-
-            tmp = node.GetNode("Recon_Rollout");
-            Recon_Rollout.Load(tmp);
             foreach (var rr in Recon_Rollout)
                 rr.LC = this;
 
-            if (node.TryGetNode("Airlaunch_Prep", ref tmp))
-            {
-                Airlaunch_Prep.Load(tmp);
-                foreach (var al in Airlaunch_Prep)
-                    al.LC = this;
-            }
-
-            tmp = node.GetNode("LaunchPads");
-            if (tmp != null)
-            {
-                foreach (ConfigNode cn in tmp.GetNodes("KCT_LaunchPad"))
-                {
-                    var tempLP = new KCT_LaunchPad("LP0");
-                    tempLP.Load(cn);
-                    LaunchPads.Add(tempLP);
-                }
-            }
-
-            tmp = node.GetNode("PadConstructions");
-            if (tmp != null)
-            {
-                PadConstructions.Load(tmp);
-            }
+            foreach (var al in Airlaunch_Prep)
+                al.LC = this;
 
             if (KerbalConstructionTimeData.Instance.LoadedSaveVersion < KCTGameStates.VERSION)
             {
-                if (KerbalConstructionTimeData.Instance.LoadedSaveVersion < 1)
-                {
-                    Engineers *= 2;
-                }
-
-                if (KerbalConstructionTimeData.Instance.LoadedSaveVersion < 6 && LCType != LaunchComplexType.Hangar)
-                {
-                    double oldEffic = 0.5d;
-                    node.TryGetValue("EfficiencyEngineers", ref oldEffic);
-
-                    // we can't use the dict yet
-                    bool createEffic = true;
-                    foreach (var e in KerbalConstructionTimeData.Instance.LCEfficiencies)
-                    {
-                        if (e.Contains(_id) && e.Efficiency < oldEffic)
-                        {
-                            e.IncreaseEfficiency(oldEffic - e.Efficiency, false);
-                            createEffic = false;
-                            break;
-                        }
-                    }
-                    if (createEffic)
-                    {
-                        LCEfficiency closest = LCEfficiency.FindClosest(this, out double closeness);
-                        if (closeness == 1d)
-                        {
-                            createEffic = false;
-
-                            if (closest.Efficiency < oldEffic)
-                                closest.IncreaseEfficiency(oldEffic - closest.Efficiency, false);
-                        }
-                    }
-                    if (createEffic)
-                    {
-                        var e = LCEfficiency.GetOrCreateEfficiencyForLC(this, true);
-                        if (e.Efficiency < oldEffic)
-                            e.IncreaseEfficiency(oldEffic - e.Efficiency, false);
-                    }
-                }
-                if (KerbalConstructionTimeData.Instance.LoadedSaveVersion < 7)
-                {
-                    node.TryGetEnum<LaunchComplexType>("lcType", ref _lcData.lcType, LaunchComplexType.Pad);
-                    node.TryGetValue("massMax", ref _lcData.massMax);
-                    node.TryGetValue("massOrig", ref _lcData.massOrig);
-                    node.TryGetValue("sizeMax", ref _lcData.sizeMax);
-                    node.TryGetValue("IsHumanRated", ref _lcData.isHumanRated);
-                }
-                if (KerbalConstructionTimeData.Instance.LoadedSaveVersion < 8)
-                {
-                    if (_id == Guid.Empty)
-                        _id = Guid.NewGuid();
-                    if (_modID == Guid.Empty)
-                        _modID = Guid.NewGuid();
-
-                    // check if we're the hangar
-                    if (_ksc.LaunchComplexes.Count == 0) // KSC loader hasn't added us yet
-                    {
-                        if (_lcData.lcType != LaunchComplexType.Hangar || _lcData.massMax != float.MaxValue)
-                            _lcData.SetFrom(LCData.StartingHangar);
-                    }
-                }
                 if (KerbalConstructionTimeData.Instance.LoadedSaveVersion < 12)
                 {
                     _lcData.Name = Name;
                 }
                 if (KerbalConstructionTimeData.Instance.LoadedSaveVersion < 13)
                 {
-                    tmp = node.GetNode("Plans");
+                    ConfigNode tmp = node.GetNode("Plans");
 
                     if (tmp != null)
                     {
@@ -520,20 +371,24 @@ namespace KerbalConstructionTime
                             KCTGameStates.Plans.Add(blv.shipName, blv);
                         }
                     }
+                    if (KerbalConstructionTimeData.Instance.LoadedSaveVersion < 18)
+                    {
+                        node.TryGetValue("ActiveLPID", ref ActiveLaunchPadIndex);
+                        if (ActiveLaunchPadIndex < 0)
+                            ActiveLaunchPadIndex = 0;
+                    }
                 }
             }
 
-            return this;
+            AddListeners();
         }
 
-        public void Load(ConfigNode node)
+        public void PostLoad(KSCItem ksc)
         {
-            throw new NotImplementedException();
-        }
-
-        public void Save(ConfigNode node)
-        {
-            throw new NotImplementedException();
+            _ksc = ksc;
+            int i = 0;
+            foreach (var pc in PadConstructions)
+                added(i++, pc);
         }
     }
 }

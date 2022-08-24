0d5d202d1dee4cfb840bb4f7cf9df55ffb645cb1
Standecco <dlombardo32@gmail.com>
2022-08-24 18:13:31 +0200

    Add UI scaling

--- Source/KerbalConstructionTime/GUI/GUI_Airlaunch.cs
+++ Source/KerbalConstructionTime/GUI/GUI_Airlaunch.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using UnityEngine;
+using RP0;
 
 namespace KerbalConstructionTime
 {
     public static partial class KCT_GUI
     {
-        private static Rect _airlaunchWindowPosition = new Rect(Screen.width - 300, 40, 300, 1);
+        private const int _airLaunchWindowWidth = 300;
+        
+        private static Rect _airlaunchWindowPosition = new Rect(Screen.width - 300, 40, _airLaunchWindowWidth*UIHolder.UIScale, 1);
         private static string _sKscDistance = "500";
         private static string _sKscAzimuth = "90";
         private static string _sAltitude = "10000";
@@ -34,32 +37,32 @@ namespace KerbalConstructionTime
 
             GUILayout.BeginHorizontal();
             GUILayout.Label("Distance from Space Center: ", GUILayout.ExpandWidth(true));
-            _sKscDistance = GUILayout.TextField(_sKscDistance, GUILayout.MaxWidth(70f));
-            GUILayout.Label("km", GUILayout.Width(25f));
+            _sKscDistance = GUILayout.TextField(_sKscDistance, UIHolder.MaxWidth(70f));
+            GUILayout.Label("km", UIHolder.Width(25f));
             GUILayout.EndHorizontal();
 
             GUILayout.BeginHorizontal();
             GUILayout.Label("Azimuth from Space Center: ", GUILayout.ExpandWidth(true));
-            _sKscAzimuth = GUILayout.TextField(_sKscAzimuth, GUILayout.MaxWidth(70f));
-            GUILayout.Label("°", GUILayout.Width(25f));
+            _sKscAzimuth = GUILayout.TextField(_sKscAzimuth, UIHolder.MaxWidth(70f));
+            GUILayout.Label("°", UIHolder.Width(25f));
             GUILayout.EndHorizontal();
 
             GUILayout.BeginHorizontal();
             GUILayout.Label("Launch azimuth: ", GUILayout.ExpandWidth(true));
-            _sAzimuth = GUILayout.TextField(_sAzimuth, GUILayout.MaxWidth(70f));
-            GUILayout.Label("°", GUILayout.Width(25f));
+            _sAzimuth = GUILayout.TextField(_sAzimuth, UIHolder.MaxWidth(70f));
+            GUILayout.Label("°", UIHolder.Width(25f));
             GUILayout.EndHorizontal();
 
             GUILayout.BeginHorizontal();
             GUILayout.Label("Altitude from SL: ", GUILayout.ExpandWidth(true));
-            _sAltitude = GUILayout.TextField(_sAltitude, GUILayout.MaxWidth(70f));
-            GUILayout.Label("m", GUILayout.Width(25f));
+            _sAltitude = GUILayout.TextField(_sAltitude, UIHolder.MaxWidth(70f));
+            GUILayout.Label("m", UIHolder.Width(25f));
             GUILayout.EndHorizontal();
 
             GUILayout.BeginHorizontal();
             GUILayout.Label("Velocity: ", GUILayout.ExpandWidth(true));
-            _sVelocity = GUILayout.TextField(_sVelocity, GUILayout.MaxWidth(70f));
-            GUILayout.Label("m/s", GUILayout.Width(25f));
+            _sVelocity = GUILayout.TextField(_sVelocity, UIHolder.MaxWidth(70f));
+            GUILayout.Label("m/s", UIHolder.Width(25f));
             GUILayout.EndHorizontal();
 
             if (_errorMsg != null)
@@ -80,8 +83,9 @@ namespace KerbalConstructionTime
             }
             if (GUILayout.Button("Cancel"))
             {
-                _centralWindowPosition.width = 150;
-                _centralWindowPosition.x = (Screen.width - 150) / 2;
+                var w = 150 * UIHolder.UIScale;
+                _centralWindowPosition.width = w;
+                _centralWindowPosition.x = (Screen.width - w) / 2;
                 GUIStates.ShowAirlaunch = false;
                 GUIStates.ShowBuildList = true;
                 _errorMsg = null;

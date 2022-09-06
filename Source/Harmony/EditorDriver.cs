78ff304777201df60f2a8a55b5a408c4b6d91e8d
NathanKell <nathankell@gmail.com>
2022-09-05 23:59:43 -0700

    Change LC to correct type when entering editor

---
+++ Source/Harmony/EditorDriver.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using KerbalConstructionTime;
+
+namespace RP0.Harmony
+{
+    [HarmonyPatch(typeof(EditorDriver))]
+    internal class PatchEditorDriver
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("StartEditor")]
+        internal static void Prefix_StartEditor(EditorFacility facility)
+        {
+            FixActiveLC(facility);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch("StartAndLoadVessel")]
+        internal static void Prefix_StartAndLoadVessel(EditorFacility facility)
+        {
+            FixActiveLC(facility);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch("SwitchEditor")]
+        internal static void Prefix_SwitchEditor(EditorFacility facility)
+        {
+            FixActiveLC(facility);
+        }
+
+        private static void FixActiveLC(EditorFacility facility)
+        {
+            var type = facility == EditorFacility.SPH ? LaunchComplexType.Hangar : LaunchComplexType.Pad;
+            if (KCTGameStates.ActiveKSC?.ActiveLaunchComplexInstance.LCType != type)
+            {
+                for (int i = 0; i < KCTGameStates.ActiveKSC.LaunchComplexes.Count; ++i)
+                {
+                    var lc = KCTGameStates.ActiveKSC.LaunchComplexes[i];
+                    if (lc.IsOperational && lc.LCType == type)
+                    {
+                        KCTGameStates.ActiveKSC.SwitchLaunchComplex(i);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}

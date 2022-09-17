9271aae3c0fb3602220f1bc4777992217208f661
NathanKell <nathankell@gmail.com>
2022-09-17 01:16:41 -0700

    Fix sims not starting in orbit. Close https://github.com/KSP-RO/ProgramsIssueTracker/issues/88

--- Source/KerbalConstructionTime/SimulationParams.cs
+++ Source/KerbalConstructionTime/SimulationParams.cs
@@ -1,8 +1,32 @@
-﻿namespace KerbalConstructionTime
+﻿using UniLinq;
+
+namespace KerbalConstructionTime
 {
     public class SimulationParams : IConfigNode
     {
-        public CelestialBody SimulationBody;
+        public CelestialBody SimulationBody
+        {
+            get
+            {
+                if (_simulationBody == null)
+                {
+                    if (simulationBodyName != null && simulationBodyName != string.Empty)
+                        _simulationBody = FlightGlobals.Bodies.FirstOrDefault(b => b.bodyName == simulationBodyName);
+                }
+                return _simulationBody;
+            }
+            set
+            {
+                _simulationBody = value;
+                simulationBodyName = value.bodyName;
+            }
+        }
+
+        private CelestialBody _simulationBody = null;
+
+        [Persistent]
+        private string simulationBodyName = string.Empty;
+
         [Persistent]
         public bool SimulateInOrbit, DisableFailures;
         public bool IsVesselMoved;

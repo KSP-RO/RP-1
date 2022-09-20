2b27a91ab227305da914e1949c7eebe1636603b6
NathanKell <nathankell@gmail.com>
2022-09-19 23:04:25 -0700

    Kill the last of the statics: PartCrewAssignments now live on the scenariomodule

--- Source/KerbalConstructionTime/PartCrewAssignment.cs
+++ Source/KerbalConstructionTime/PartCrewAssignment.cs
@@ -2,10 +2,16 @@
 
 namespace KerbalConstructionTime
 {
-    public class PartCrewAssignment
+    public class PartCrewAssignment : IConfigNode
     {
-        public List<CrewMemberAssignment> CrewList { get; set; }
-        public uint PartID { get; set; }
+        [Persistent]
+        public List<CrewMemberAssignment> CrewList = new List<CrewMemberAssignment>();
+        [Persistent]
+        public uint PartID;
+
+        public PartCrewAssignment()
+        {
+        }
 
         public PartCrewAssignment(uint ID, List<CrewMemberAssignment> crew)
         {
@@ -18,13 +24,44 @@ namespace KerbalConstructionTime
             PartID = part.flightID;
             CrewList = crew;
         }
+
+        public void Load(ConfigNode node)
+        {
+            ConfigNode.LoadObjectFromConfig(this, node);
+        }
+
+        public void Save(ConfigNode node)
+        {
+            ConfigNode.CreateConfigFromObject(this, node);
+        }
     }
 
     public class CrewMemberAssignment
     {
-        public ProtoCrewMember PCM { get; set; }
-        public bool HasChute { get; set; }
-        public bool HasJetpack { get; set; }
+        public ProtoCrewMember PCM 
+        {
+            get
+            {
+                if (_pcm == null && _pcmName != string.Empty)
+                    _pcm = HighLogic.CurrentGame.CrewRoster[_pcmName];
+                return _pcm;
+            }
+            set
+            {
+                _pcm = value;
+                if (value == null)
+                    _pcmName = string.Empty;
+                else
+                    _pcmName = value.name;
+            }
+        }
+        private ProtoCrewMember _pcm;
+        [Persistent]
+        private string _pcmName = string.Empty;
+        [Persistent]
+        public bool HasChute;
+        [Persistent]
+        public bool HasJetpack;
 
         public CrewMemberAssignment()
         {

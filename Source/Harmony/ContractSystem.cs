f94b09b09411c80642bc111ae2f2189399d00f62
siimav <siim.aaver@gmail.com>
2023-04-04 01:37:12 +0300

    Use the new CC CompletedContractsByName dictionary

--- Source/Harmony/ContractSystem.cs
+++ Source/Harmony/ContractSystem.cs
@@ -1,7 +1,5 @@
-﻿using HarmonyLib;
-using UnityEngine;
-using KSP.UI.Screens;
-using Contracts;
+﻿using Contracts;
+using HarmonyLib;
 
 namespace RP0.Harmony
 {
@@ -14,32 +12,6 @@ namespace RP0.Harmony
         {
             tier1 = tier2 = tier3 = int.MaxValue;
             return false;
-        }
-
-        internal static bool _nextResetIsAfterLoad = false;
-        [HarmonyPrefix]
-        [HarmonyPatch("OnLoadRoutine")]
-        internal static void Prefix_OnLoadRoutine()
-        {
-            _nextResetIsAfterLoad = true;
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch("ResetContracts")]
-        internal static void Postfix_ResetContracts(ContractSystem __instance)
-        {
-            if (!_nextResetIsAfterLoad)
-                return;
-
-            _nextResetIsAfterLoad = false;
-
-            foreach (var c in __instance.ContractsFinished)
-            {
-                if (c is ContractConfigurator.ConfiguredContract cc && c.ContractState == Contract.State.Completed && cc.contractType != null && !string.IsNullOrEmpty(cc.contractType.name))
-                {
-                    Programs.ProgramHandler.Instance.CompletedCCContracts.Add(cc.contractType.name);
-                }
-            }
         }
     }
 }

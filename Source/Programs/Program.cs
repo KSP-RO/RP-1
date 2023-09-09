a557d71d0dc6d98ff328a3146b27edc14a4b4174
NathanKell <nathankell@gmail.com>
2023-09-08 23:11:58 -0700

    Year X -> Nominal Year X in program funding summary

--- Source/Programs/Program.cs
+++ Source/Programs/Program.cs
@@ -547,7 +547,7 @@ namespace RP0.Programs
                         double fundAtYear = GetFundsAtTime(Math.Min(i, duration) * secPerYear);
                         double paidThisYear = fundAtYear - totalPaid;
                         totalPaid = fundAtYear;
-                        text += $"\nYear {(lastYear > 10 ? " " : string.Empty)}{i}:  {CurrencyModifierQueryRP0.RunQuery(TransactionReasonsRP0.ProgramFunding, paidThisYear, 0d, 0d).GetCostLineOverride(false, false, false, true)}";
+                        text += $"\nNominal Year {(lastYear > 10 ? " " : string.Empty)}{i}:  {CurrencyModifierQueryRP0.RunQuery(TransactionReasonsRP0.ProgramFunding, paidThisYear, 0d, 0d).GetCostLineOverride(false, false, false, true)}";
                     }
                 }
             }

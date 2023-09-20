c1c3fd0ecba2e8323c8d070168f74f1004af6d27
NathanKell <nathankell@gmail.com>
2023-09-20 16:57:20 -0700

    Hard dep Kerbalism, fix experiment patcher

--- Source/RP0/HarmonyPatcher.cs
+++ Source/RP0/HarmonyPatcher.cs
@@ -5,12 +5,14 @@ namespace RP0
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     public class HarmonyPatcher : MonoBehaviour
     {
-        internal void Start()
+        // We have to run in Update rather than start, so that Kerbalism is loaded
+        internal void Update()
         {
             var harmony = new HarmonyLib.Harmony("RP0.HarmonyPatcher");
             harmony.PatchAll();
 
             Harmony.PatchGameSettings.FixDV();
+            Destroy(this);
         }
     }
 }
---
+++ Source/RP0/Utilities/KerbalismUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RP0
+{
+    public static class KerbalismUtils
+    {
+        public static ExperimentSituations ToExperimentSituations(this KERBALISM.ScienceSituation sit)
+        {
+            int sitInt = 1 << (int)KERBALISM.ScienceSituationUtils.ToValidStockSituation(sit);
+            return (ExperimentSituations)sitInt;
+        }
+    }
+}

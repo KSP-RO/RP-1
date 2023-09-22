2afd03577a1c899c3f2bcaa922585a85eab567b9
NathanKell <nathankell@gmail.com>
2023-09-22 00:52:37 -0700

    Modification of Kerbalism time-in-shadow algorithm * Calculate beta angle * For low-ecc orbits, use formula from https://commons.erau.edu/cgi/viewcontent.cgi?article=1412&context=ijaaa * Calculate beta* and early-out if beta angle > this (per paper) -- will be wrong in higher-ecc orbits because LAN/ArgPe is ignored, but close enough * Report 0 time in darkness on e>=1 orbits. This is wrong, but you spend very little time in darkness on those as a fraction of period... * Consider tidal-lockedness if landed on a tidally-locked body orbiting the Sun

--- Source/RP0/Harmony/KerbalismPatcher.cs
+++ Source/RP0/Harmony/KerbalismPatcher.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Reflection;
 using KERBALISM;
+using UnityEngine;
 
 namespace RP0.Harmony
 {
@@ -77,6 +78,144 @@ namespace RP0.Harmony
 
             if (sits.Contains(vs.ScienceSituation.ToExperimentSituations()))
                 mainIssue = Local.Module_Experiment_issue1;
+        }
+    }
+
+    [HarmonyPatch(typeof(Sim))]
+    internal class PatchKerbalism_Sim
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("ShadowPeriod", new Type[] { typeof(Vessel) })]
+        internal static bool Prefix_ShadowPeriod(Vessel v, ref double __result)
+        {
+            if (Lib.Landed(v) || v.orbit is not Orbit obt || double.IsNaN(obt.inclination))
+            {
+                var sun = Planetarium.fetch.Sun;
+                var mb = v.mainBody;
+                if (mb.referenceBody == sun && mb.tidallyLocked)
+                {
+                    Vector3 sunV = sun.transform.position - v.transform.position;
+                    Vector3 bodyV = mb.transform.position - v.transform.position;
+                    __result = Vector3.Dot(sunV, bodyV) > 0f ? mb.rotationPeriod : 0d;
+                }
+                else
+                {
+                    __result = mb.rotationPeriod * 0.5d;
+                }
+                return false;
+            }
+
+            double e = obt.eccentricity;
+            if (e >= 1d)
+            {
+                // This is wrong, of course, but given the speed of an escape trajectory
+                // you'll be in shadow for a very miniscule fraction of the period.
+                __result = 0d;
+                return false;
+            }
+
+            Vector3d planeNormal = Vector3d.Cross(v.velocityD, v.orbit.referenceBody.position - v.transform.position);
+            double sunDot = Math.Abs(Vector3d.Dot(Planetarium.fetch.Sun.transform.position - v.transform.position, planeNormal));
+            double betaAngle = Math.PI * 0.5d - Math.Acos(sunDot);
+
+            double a = obt.semiMajorAxis;
+            double R = obt.referenceBody.Radius;
+
+            // Now, branch depending on if we're in a low-ecc orbit
+            // We check locally for betaStar because we might bail early in the Kerbalism case
+            if (e < 0.1d)
+                __result = FracEclipseCirc(betaAngle, a, R);
+            else
+                __result = FracEclipseKerbalism(v, betaAngle, a, R, e);
+
+            __result *= obt.period;
+
+            return false;
+        }
+
+        internal static double FracEclipseCirc(double betaAngle, double sma, double R)
+        {
+            // from https://commons.erau.edu/cgi/viewcontent.cgi?article=1412&context=ijaaa
+            double betaStar = Math.Asin(R / sma);
+            if (Math.Abs(betaAngle) >= betaStar)
+                return 0d;
+
+            double avgHeight = sma - R;
+            return (1d / Math.PI) * Math.Acos(Math.Sqrt(avgHeight * avgHeight + 2 * R * avgHeight) / (sma * Math.Cos(betaAngle)));
+        }
+
+        internal static double FracEclipseKerbalism(Vessel v, double betaAngle, double a, double R, double e)
+        {
+            // This is the Kerbalism code, complete with comments. However
+            // we check for ecc >=1 and just bail in that case, instead of the NaN check
+            // and we check for beta angle vs beta*
+
+            // Calculation for elliptical orbits
+
+            // see https://wiki.kerbalspaceprogram.com/wiki/Orbit_darkness_time
+
+            // Limitations:
+            //
+            // - This method assumes the orbit is an ellipse / circle which is not
+            // changing or being altered by other bodies.
+            //
+            // - It also assumes the sun's rays are parallel across the orbiting
+            // planet, although all bodies are small enough and far enough from the
+            // sun for this to be nearly true.
+            //
+            // - The method does not take into account darkness caused by eclipses
+            // of a different body than the orbited body, for example, orbiting
+            // Laythe but Jool blocks the sun.
+            //
+            // - The method gives the longest amount of time spent in darkness, which for some
+            // orbits (e.g.polar orbits), will only be experienced periodically.
+            // NOTE: corrected via beta angle (mostly)
+
+            // The formula:
+            // Td = (2ab / h) (asin(R/b) + eR/b)
+            // a is the semi-major axis
+            // b the semi-minor axis
+            // h the specific angular momentum
+            // e the eccentricity
+            // R the radius of the planet or moon
+            // For reference these terms can be calculated by knowing the apoapsis(Ap), periapsis(Pe) and body to orbit:
+            // h = sqrt(lu)
+            // l = (2 * ra * rp) / (ra + rp)
+            // u = G * M, the gravitational parameter
+            // ra = Ap + R (apoapsis from center of the body)
+            // rp = Pe + R (periapsis from center of the body)
+            // TRANSFORMED by dividing by period sqrt(a^3/u) to report eclipse fraction rather than time in darkness.
+            // ab * (asin(R / b) + e * R / b) / (pi * sqrt(a^3 * l)
+
+            double b = v.orbit.semiMinorAxis;
+            // Just bail if we were going to report NaN, or we're in a weird state
+            // We've likely avoided this already due to the eccentricity check in the main call, though
+            if (a < b || b < R)
+                return 0d;
+
+            double betaStar = Math.Asin(R / a);
+            if (Math.Abs(betaAngle) >= betaStar)
+                return 0d;
+
+            double ra = v.orbit.ApR;
+            double rp = v.orbit.PeR;
+            double RdivB = R / b;
+            double l = (2d * ra * rp) / (ra + rp);
+
+            double frac = a * b * (Math.Asin(RdivB) + e * RdivB) / (Math.PI * Math.Sqrt(a * a * a * l));
+
+            // err on the light side:
+            // if more than half the orbit duration is in shadow, invert
+            // the result. this will be wrong when the apoapsis is near nadir.
+            // this is just... WRONG, I know, but it is wrong in a good way.
+            // f.i. a very eccentric orbit with the apoapsis above one of the
+            // poles won't be assumed to spend most of the time in shadow.
+            // NOTE: with the new beta angle code above, this should not be
+            // hit? Beta* for high-ecc (and therefore high-SMA) orbits will
+            // be very low, meaning we early-out in those cases.
+            if (frac > 0.5d)
+                frac = 1d - frac;
+            return frac;
         }
     }
 }

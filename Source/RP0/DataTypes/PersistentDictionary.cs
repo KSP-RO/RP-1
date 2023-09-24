e227fec3aa7b87aa33dce00b3b654703a84cf850
NathanKell <nathankell@gmail.com>
2023-09-24 03:07:15 -0700

    DataTypes.Persistent now support loading/saving arbitrary types that inherit or implement their generic

--- Source/RP0/DataTypes/PersistentDictionary.cs
+++ Source/RP0/DataTypes/PersistentDictionary.cs
@@ -6,6 +6,10 @@ namespace RP0.DataTypes
 {
     public abstract class PersistentDictionary<TKey, TValue> : Dictionary<TKey, TValue>, IConfigNode where TValue : IConfigNode
     {
+        private static readonly Type _type = typeof(TValue);
+        private static readonly string _typeName = typeof(TValue).Name;
+        private static readonly Dictionary<string, Type> _typeCache = new Dictionary<string, Type>();
+
         protected abstract TKey ParseKey(string key);
 
         protected abstract string WriteKey(TKey key);
@@ -19,22 +23,40 @@ namespace RP0.DataTypes
             for (int i = 0; i < keyNode.values.Count; ++i)
             {
                 TKey key = ParseKey(keyNode.values[i].value);
-                TValue value = Activator.CreateInstance<TValue>();
-                value.Load(valueNode.nodes[i]);
+
+                var n = valueNode.nodes[i];
+                TValue value;
+                if (n.name == "VALUE" || n.name == _typeName)
+                {
+                    value = Activator.CreateInstance<TValue>();
+                }
+                else
+                {
+                    if (!_typeCache.TryGetValue(n.name, out var type))
+                        type = HarmonyLib.AccessTools.TypeByName(n.name);
+                    if (type == null || !_type.IsAssignableFrom(type))
+                        type = _type;
+                    else
+                        _typeCache[n.name] = type;
+
+                    value = (TValue)Activator.CreateInstance(type);
+                }
+                value.Load(n);
                 Add(key, value);
             }
         }
 
         public void Save(ConfigNode node)
         {
-            node.AddValue("version", 1);
+            node.AddValue("version", 2);
             ConfigNode keyNode = node.AddNode("Keys");
             ConfigNode valueNode = node.AddNode("Values");
 
             foreach (var kvp in this)
             {
                 keyNode.AddValue("key", WriteKey(kvp.Key));
-                ConfigNode n = new ConfigNode("VALUE");
+                var type = kvp.Value.GetType();
+                ConfigNode n = new ConfigNode(type == _type ? _typeName : type.FullName);
                 kvp.Value.Save(n);
                 valueNode.AddNode(n);
             }
@@ -43,6 +65,10 @@ namespace RP0.DataTypes
 
     public class PersistentDictionaryNodeKeyed<TValue> : Dictionary<string, TValue>, IConfigNode where TValue : IConfigNode
     {
+        private static readonly Type _type = typeof(TValue);
+        private static readonly string _typeName = typeof(TValue).Name;
+        private static readonly Dictionary<string, Type> _typeCache = new Dictionary<string, Type>();
+
         private string _keyName = "name";
 
         public PersistentDictionaryNodeKeyed() {}
@@ -64,7 +90,23 @@ namespace RP0.DataTypes
                     RP0Debug.LogError("PersistentDictionaryNodeKeyed: null or empty key in node! Skipping. Node=\n" + n.ToString());
                     continue;
                 }
-                TValue value = Activator.CreateInstance<TValue>();
+
+                TValue value;
+                if (n.name == "VALUE" || n.name == _typeName)
+                {
+                    value = Activator.CreateInstance<TValue>();
+                }
+                else
+                {
+                    if (!_typeCache.TryGetValue(n.name, out var type))
+                        type = HarmonyLib.AccessTools.TypeByName(n.name);
+                    if (type == null || !_type.IsAssignableFrom(type))
+                        type = _type;
+                    else
+                        _typeCache[n.name] = type;
+
+                    value = (TValue)Activator.CreateInstance(type);
+                }
                 value.Load(n);
                 Add(key, value);
             }
@@ -72,10 +114,11 @@ namespace RP0.DataTypes
 
         public void Save(ConfigNode node)
         {
-            node.AddValue("version", 1);
+            node.AddValue("version", 2);
             foreach (var kvp in this)
             {
-                ConfigNode n = new ConfigNode("VALUE");
+                var type = kvp.Value.GetType();
+                ConfigNode n = new ConfigNode(type == _type ? _typeName : type.FullName);
                 kvp.Value.Save(n);
                 n.SetValue(_keyName, kvp.Key, true);
                 node.AddNode(n);

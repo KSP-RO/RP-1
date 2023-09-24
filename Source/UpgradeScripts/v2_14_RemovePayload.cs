d81296872d829f945fc9b3e31c1a73fa5ab5edc6
ryanc55 <54254625+ryanc55@users.noreply.github.com>
2023-09-24 00:53:27 -0700

    Contract Sats: Bugfixes and Balance Changes
    Add missing texture
    Decrease temperature tolerance
    Fix tank weights
    Decided to remove zombie resources from all locations.   This will cause ongoing nav/com/weather missions to fail, and affected KCT craft will have to be rebuilt or edited.  But I didn't like having those half-broken configs out there causing potential issues.

--- Source/UpgradeScripts/v2_14_RemovePayload.cs
+++ Source/UpgradeScripts/v2_14_RemovePayload.cs
@@ -18,7 +18,7 @@ namespace RP0.UpgradeScripts
         public override TestResult OnTest(ConfigNode node, LoadContext loadContext, ref string nodeName)
         {
             ConfigNode.Value v = null;
-            if (node.name == "RESOURCE" && (v = node.values.values.Find(s => s.name == "name")) != null)
+            if ((v = node.values.values.Find(s => s.name == "name")) != null)
             {
                 if (v.value == "ComSatPayload" || v.value == "NavSatPayload" || v.value == "WeatherSatPayload")
                 {
@@ -33,14 +33,16 @@ namespace RP0.UpgradeScripts
         public override void OnUpgrade(ConfigNode node, LoadContext loadContext, ConfigNode parentNode)
         {
             ConfigNode.Value v = null;
-            if (node.name == "RESOURCE" && (v = node.values.values.Find(s => s.name == "name")) != null)
+            if ((v = node.values.values.Find(s => s.name == "name")) != null)
             {
                 if (v.value == "ComSatPayload" || v.value == "NavSatPayload" || v.value == "WeatherSatPayload")
                 {
+                    node.SetValue("maxAmount", 0f);
+                    node.SetValue("amount", 0f);
                     Debug.Log($"[RP-1] UpgradePipeline removed {v.value} from {node.name}");
-                    node.ClearData();
                 }
             }
         }
     }
 }
+

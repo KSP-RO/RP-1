587823eb04bf7ab0357bcb4509daa103572385d3
NathanKell <nathankell@gmail.com>
2023-10-02 02:50:03 -0700

    Meh, this doesn't need to be unsafe.

--- Source/RP0/Utilities/KerbalismUtils.cs
+++ Source/RP0/Utilities/KerbalismUtils.cs
@@ -21,7 +21,7 @@ namespace RP0
             1d/(60d*60d),
             1d/(86400d * 365d) };
 
-        public static unsafe bool HumanRateToSI(ref string rate, string unit, double baseMult = 1d, int sigFigs = 3)
+        public static bool HumanRateToSI(ref string rate, string unit, double baseMult = 1d, int sigFigs = 3)
         {
             int numStart = -1, numStop = -1, rStart = -1, rEnd = -1;
             int len = rate.Length;
@@ -34,81 +34,78 @@ namespace RP0
                 _rateStrings[3] = Local.Generic_perDay;
                 _rateStrings[4] = Local.Generic_perYear;
             }
-            fixed (char* pszRate = rate)
+            for (int i = 0; i < len; ++i)
             {
-                for (int i = 0; i < len; ++i)
+                if (rate[i] == '/')
                 {
-                    if (pszRate[i] == '/')
+                    numStop = i - 1;
+                    rStart = i;
+                    for (int j = i + 1; j < len; ++j)
                     {
-                        numStop = i - 1;
-                        rStart = i;
-                        for (int j = i + 1; j < len; ++j)
+                        if (rate[j] == '<')
                         {
-                            if (pszRate[j] == '<')
-                            {
-                                rEnd = j - 1;
-                                break;
-                            }
-                        }
-                        if (rEnd == -1)
-                            rEnd = len - 1;
-
-                        for (int j = i; j-- > 0;)
-                        {
-                            if (pszRate[j] == '>')
-                            {
-                                numStart = j + 1;
-                                break;
-                            }
-                        }
-                        if (numStart == -1)
-                            numStart = 0;
-
-                        break;
-                    }
-                }
-
-                if (numStop == -1)
-                    return false;
-
-                int end = rEnd - rStart + 1;
-                for (int i = 0; i < end; ++i)
-                {
-                    char c = pszRate[i + rStart];
-                    for (int j = 0; j < 5; ++j)
-                    {
-                        if (_rateMatches[j])
-                        {
-                            if (c != _rateStrings[j][i])
-                                _rateMatches[j] = false;
+                            rEnd = j - 1;
+                            break;
                         }
                     }
+                    if (rEnd == -1)
+                        rEnd = len - 1;
+
+                    for (int j = i; j-- > 0;)
+                    {
+                        if (rate[j] == '>')
+                        {
+                            numStart = j + 1;
+                            break;
+                        }
+                    }
+                    if (numStart == -1)
+                        numStart = 0;
+
+                    break;
                 }
-                int rateIdx = 0;
-                for (; rateIdx < 5; ++rateIdx)
-                {
-                    if (_rateMatches[rateIdx])
-                        break;
-                }
-                // reset for next
-                _rateMatches[0] = _rateMatches[1] = _rateMatches[2] = _rateMatches[3] = _rateMatches[4] = true;
-
-                // we could parse in place but eh.
-                string numStr = rate.Substring(numStart, numStop - numStart + 1);
-                if (!double.TryParse(numStr, out var value))
-                    return false;
-
-                value *= _rateMults[rateIdx] * baseMult;
-
-                string result = KSPUtil.PrintSI(value, unit, sigFigs);
-                if (numStart > 0)
-                    result = rate.Substring(0, numStart) + result;
-                if (rEnd + 1 < len)
-                    result += rate.Substring(rEnd + 1, len - rEnd - 1);
-
-                rate = result;
-                return true;
             }
+
+            if (numStop == -1)
+                return false;
+
+            int end = rEnd - rStart + 1;
+            for (int i = 0; i < end; ++i)
+            {
+                char c = rate[i + rStart];
+                for (int j = 0; j < 5; ++j)
+                {
+                    if (_rateMatches[j])
+                    {
+                        if (c != _rateStrings[j][i])
+                            _rateMatches[j] = false;
+                    }
+                }
+            }
+            int rateIdx = 0;
+            for (; rateIdx < 5; ++rateIdx)
+            {
+                if (_rateMatches[rateIdx])
+                    break;
+            }
+            // reset for next
+            _rateMatches[0] = _rateMatches[1] = _rateMatches[2] = _rateMatches[3] = _rateMatches[4] = true;
+
+            // we could parse in place but eh.
+            string numStr = rate.Substring(numStart, numStop - numStart + 1);
+            if (!double.TryParse(numStr, out var value))
+                return false;
+
+            value *= _rateMults[rateIdx] * baseMult;
+
+            string result = KSPUtil.PrintSI(value, unit, sigFigs);
+            if (numStart > 0)
+                result = rate.Substring(0, numStart) + result;
+            if (rEnd + 1 < len)
+                result += rate.Substring(rEnd + 1, len - rEnd - 1);
+
+            rate = result;
+            return true;
         }
     }
 }

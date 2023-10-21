989d20334ca9eac5c3d36e9f0affb400a77ee9c8
NathanKell <nathankell@gmail.com>
2023-10-20 22:04:25 -0700

    Update Install Checker to check Kerbalism version

--- Source/InstallChecker/InstallChecker.cs
+++ Source/InstallChecker/InstallChecker.cs
@@ -7,8 +7,16 @@ namespace RP0InstallChecker
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     public class InstallChecker : MonoBehaviour
     {
-        protected void Start()
+        private bool _firstFrame = true;
+
+        protected void Update()
         {
+            if (_firstFrame)
+            {
+                _firstFrame = false;
+                return;
+            }
+
             Version minKSPCFVer = new Version(1, 30, 0);
             if (!AssemblyLoader.loadedAssemblies.Any(a => a.name.Equals("KSPCommunityFixes") && (new Version(a.versionMajor, a.versionMinor, a.versionRevision)) >= minKSPCFVer))
             {
@@ -22,7 +30,7 @@ namespace RP0InstallChecker
             if (assembliesToCheck.Any(an => !AssemblyLoader.loadedAssemblies.Any(a => a.name.Equals(an, StringComparison.OrdinalIgnoreCase))))
             {
                 string titleText = "Incorrect RP-1 Installation";
-                string contentText = "This could be caused by downloading the RP-1 repo or some specific branch directly from GitHub. " +
+                string contentText = "This could be caused by downloading the RP-1 repo or some specific branch directly from GitHub, or by not installing or not updating dependencies. " +
                     "Make sure to follow the install guide located in the RP-1 wiki.\n\n" +
                     "If the goal was to obtain the latest developmental version of RP-1, then please use the link at the top of the RP-1 readme.";
                 ShowErrorDialog(titleText, contentText);
@@ -32,7 +40,7 @@ namespace RP0InstallChecker
             if (AssemblyLoader.loadedAssemblies.Any(a => a.name.Equals("KerbalConstructionTime", StringComparison.OrdinalIgnoreCase)))
             {
                 string titleText = "Incorrect RP-1 Installation";
-                string contentText = "You still have a KerbalConstructionTime dll (RP0KCT.dll). Please uninstall RP-1 and properly reinstall it to remove this dll." +
+                string contentText = "You still have a KerbalConstructionTime dll (RP0KCT.dll). Please uninstall RP-1 and properly reinstall it to remove this dll. " +
                     "Make sure to follow the install guide located in the RP-1 wiki.\n\n" +
                     "If the goal was to obtain the latest developmental version of RP-1, then please use the link at the top of the RP-1 readme after uninstalling this RP-1 install.";
                 ShowErrorDialog(titleText, contentText);
@@ -40,11 +48,14 @@ namespace RP0InstallChecker
             }
 
             assembliesToCheck = new[] { "ToolbarController", "ClickThroughBlocker", "RealFuels", "ContractConfigurator", "ModularFlightIntegrator" };    // These are values of KSPAssembly attribute
-            if (assembliesToCheck.Any(an => !AssemblyLoader.loadedAssemblies.Any(a => a.name.Equals(an, StringComparison.OrdinalIgnoreCase))))
+            var kerbAssembly = AssemblyLoader.loadedAssemblies.FirstOrDefault(a => a.name == "Kerbalism");
+            if (assembliesToCheck.Any(an => !AssemblyLoader.loadedAssemblies.Any(a => a.name.Equals(an, StringComparison.OrdinalIgnoreCase)))
+                || kerbAssembly == null
+                || kerbAssembly.assembly.GetName().Version < new Version("3.17"))
             {
                 string titleText = "Incorrect RP-1 Installation";
-                string contentText = "You are missing dependencies for RP-1. This could be caused by manually installing RP-1, or by not updating dependencies." +
-                    "Make sure to follow the install guide located in the RP-1 wiki.\n\n" +
+                string contentText = "You are missing dependencies for RP-1. This could be caused by manually installing RP-1, or by not updating dependencies. " +
+                    "Make sure to follow the install guide located in the RP-1 wiki and make sure to update your mods in CKAN.\n\n" +
                     "If the goal was to obtain the latest developmental version of RP-1, then install normally through the guide and then use the link at the top of the RP-1 readme.";
                 ShowErrorDialog(titleText, contentText);
                 return;

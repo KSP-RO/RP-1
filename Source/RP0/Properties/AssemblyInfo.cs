e595d3172b173a5a259fed72679347498cac9630
siimav <siim.aaver@gmail.com>
2024-07-13 15:30:01 +0300

    Add KSPAssemblyDependency for RA

--- Source/RP0/Properties/AssemblyInfo.cs
+++ Source/RP0/Properties/AssemblyInfo.cs
@@ -45,6 +45,7 @@ using System.Runtime.InteropServices;
 
 [assembly: KSPAssemblyDependency("ModularFlightIntegrator", 1, 0)]
 [assembly: KSPAssemblyDependency("RealFuels", 15, 7)]
+[assembly: KSPAssemblyDependency("RealAntennas", 2, 4)]
 [assembly: KSPAssemblyDependency("ROUtils", 1, 0, 1)]
 [assembly: KSPAssemblyDependency("ClickThroughBlocker", 1, 8)]
 [assembly: KSPAssemblyDependency("ContractConfigurator", 2, 6)]

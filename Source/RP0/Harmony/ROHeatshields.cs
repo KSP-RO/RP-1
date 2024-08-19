a764bb324722b28bd4e6756330608afd4cd5be0c
siimav <siim.aaver@gmail.com>
2024-08-19 17:11:24 +0300

    Harmony-patch ROHS config can afford query to factor in unlock credit.
    Also change other Harmony patches to use the same format.

---
+++ Source/RP0/Harmony/ROHeatshields.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+
+namespace RP0.Harmony
+{
+    [HarmonyPatch]
+    internal class ROHSPartModulePatcher
+    {
+        internal static readonly Type _type = AccessTools.TypeByName("ROHeatshields.ModuleROHeatshield");
+        internal static MethodInfo _mi;
+
+        internal static MethodBase TargetMethod() => _mi;
+
+        [HarmonyPrepare]
+        internal static bool Prepare()
+        {
+            if (_type != null)
+            {
+                _mi ??= AccessTools.Method(_type, "CanAffordEntryCost", new Type[] { typeof(float) });
+            }
+            return _mi != null;
+        }
+
+        [HarmonyPrefix]
+        internal static bool Prefix_CanAffordEntryCost(float cost, ref bool __result)
+        {
+            var cmq = CurrencyModifierQueryRP0.RunQuery(TransactionReasonsRP0.PartOrUpgradeUnlock, -cost, 0d, 0d);
+            double postCMQcost = -cmq.GetTotal(CurrencyRP0.Funds, false);
+            double credit = UnlockCreditHandler.Instance.TotalCredit;
+            cmq.AddPostDelta(CurrencyRP0.Funds, credit, true);
+            __result = cmq.CanAfford();
+
+            return false;
+        }
+    }
+}

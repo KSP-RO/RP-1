fa0d04bcb98390c48115e93d28b1d9ace1f2538e
Clayell <125416952+Clayell@users.noreply.github.com>
2025-03-30 22:55:20 -0400

    Update GUI_Personnel.cs
    ok i see why mobile is a bad idea...

--- Source/RP0/UI/KCT/GUI_Personnel.cs
+++ Source/RP0/UI/KCT/GUI_Personnel.cs
@@ -436,7 +436,7 @@ namespace RP0
             bool b1 = int.TryParse(sNumStaff, out int numCrew);
             bool b2 = double.TryParse(sReserveFunds, out double reserveFunds);
 
-            string errorMessage = (!b1 ? "Failed to parse stadf count!\n" : "") + (!b2 ? "Failed to parse reserve funds!" : "");
+            string errorMessage = (!b1 ? "Failed to parse staff count!\n" : "") + (!b2 ? "Failed to parse reserve funds!" : "");
 
             if (!string.IsNullOrEmpty(errorMessage))
             {

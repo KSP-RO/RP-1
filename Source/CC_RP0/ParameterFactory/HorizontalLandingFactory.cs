c3f6999264fa88958996fca93d86f042d124e34b
siimav <1120038+siimav@users.noreply.github.com>
2025-10-01 20:22:38 +0300

    Add maxSrfVel option to HorizontalLanding parameter (#2642)

--- Source/CC_RP0/ParameterFactory/HorizontalLandingFactory.cs
+++ Source/CC_RP0/ParameterFactory/HorizontalLandingFactory.cs
@@ -5,6 +5,7 @@ namespace ContractConfigurator.RP0
     public class HorizontalLandingFactory : ParameterFactory
     {
         protected double descentAngle;
+        protected double? maxSrfVel;
         protected float updateFrequency;
 
         public override bool Load(ConfigNode configNode)
@@ -12,14 +13,15 @@ namespace ContractConfigurator.RP0
             bool valid = base.Load(configNode);
 
             valid &= ConfigNodeUtil.ParseValue<double>(configNode, "descentAngle", x => descentAngle = x, this, 2, x => Validation.GT(x, 0) && Validation.LT(x, 90));
-            valid &= ConfigNodeUtil.ParseValue<float>(configNode, "updateFrequency", x => updateFrequency = x, this, HorizontalLanding.DEFAULT_UPDATE_FREQUENCY, x => Validation.GT(x, 0.0f));
+            valid &= ConfigNodeUtil.ParseValue(configNode, "maxSrfVel", x => maxSrfVel = x, this, (double?)null);
+            valid &= ConfigNodeUtil.ParseValue(configNode, "updateFrequency", x => updateFrequency = x, this, HorizontalLanding.DEFAULT_UPDATE_FREQUENCY, x => Validation.GT(x, 0.0f));
 
             return valid;
         }
 
         public override ContractParameter Generate(Contract contract)
         {
-            return new HorizontalLanding(title, descentAngle, updateFrequency);
+            return new HorizontalLanding(title, descentAngle, maxSrfVel, updateFrequency);
         }
     }
 }

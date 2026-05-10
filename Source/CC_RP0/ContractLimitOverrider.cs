f35247cdd1776073f2b01a32a03a197163936bac
Clayel <videogamer0907@gmail.com>
2026-05-10 16:34:46 -0400

    remove the harmony patch, use CC customparams instead

---
+++ Source/CC_RP0/ContractLimitOverrider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace ContractConfigurator.RP0
+{
+    /// <summary>
+    /// ContractConfigurator sets Contract Limits based on the prestige level of the contract.
+    /// RP-1 doesn't want that, so we just override it to only use the limit that the trivial prestige level would have.
+    /// </summary>
+    [KSPAddon(KSPAddon.Startup.AllGameScenes, true)]
+    public class ContractLimitOverrider : MonoBehaviour
+    {
+        internal void Start()
+        {
+            HighLogic.CurrentGame.Parameters.CustomParams<ContractConfiguratorParameters>().contractLimitPrestigeOverride = ContractConfiguratorParameters.ContractLimitPrestigeOverride.Trivial;
+        }
+    }
+}

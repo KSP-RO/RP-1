734dd5ac21c992298ab7c06e5d76b8b8e4429873
Clayel <videogamer0907@gmail.com>
2026-05-11 16:58:02 -0400

    use CC's method of getting prestige from a contract type, use maxActive instead of the custom stuff, make ContractLimitOverrider start up more than once

--- Source/CC_RP0/ContractLimitOverrider.cs
+++ Source/CC_RP0/ContractLimitOverrider.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using RP0;
 using UnityEngine;
 
 namespace ContractConfigurator.RP0
@@ -7,21 +7,18 @@ namespace ContractConfigurator.RP0
     /// ContractConfigurator sets Contract Limits based on the prestige level of the contract.
     /// RP-1 doesn't want that, so we just override it to only use the limit that the trivial prestige level would have.
     /// </summary>
-    [KSPAddon(KSPAddon.Startup.AllGameScenes, true)]
+    [KSPAddon(KSPAddon.Startup.AllGameScenes, false)]
     public class ContractLimitOverrider : MonoBehaviour
     {
         internal void Start()
         {
-            int level = (int)Math.Round(ScenarioUpgradeableFacilities.GetFacilityLevel(SpaceCenterFacility.MissionControl) *
-                ScenarioUpgradeableFacilities.GetFacilityLevelCount(SpaceCenterFacility.MissionControl));
-            float rep = Reputation.Instance.reputation;
-            float mult = HighLogic.CurrentGame.Parameters.CustomParams<ContractConfiguratorParameters>().ActiveContractMultiplier;
+            int maxActive = GameVariables.Instance.GetActiveContractsLimit(ScenarioUpgradeableFacilities.GetFacilityLevel(SpaceCenterFacility.MissionControl));
 
-            int contractLimit = Math.Max(2, (int)Math.Round((rep + rep * level / 3) * mult / 200 + 6 + level));
+            RP0Debug.Log($"Setting trivial, significant, and exceptional contract limits to {maxActive}.");
 
-            HighLogic.CurrentGame.Parameters.CustomParams<ContractConfiguratorParameters>().trivialContractLimit = contractLimit;
-            HighLogic.CurrentGame.Parameters.CustomParams<ContractConfiguratorParameters>().significantContractLimit = contractLimit;
-            HighLogic.CurrentGame.Parameters.CustomParams<ContractConfiguratorParameters>().exceptionalContractLimit = contractLimit;
+            HighLogic.CurrentGame.Parameters.CustomParams<ContractConfiguratorParameters>().trivialContractLimit = maxActive;
+            HighLogic.CurrentGame.Parameters.CustomParams<ContractConfiguratorParameters>().significantContractLimit = maxActive;
+            HighLogic.CurrentGame.Parameters.CustomParams<ContractConfiguratorParameters>().exceptionalContractLimit = maxActive;
         }
     }
 }

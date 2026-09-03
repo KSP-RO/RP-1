a9702f2aeb98533def43f6bc18864dc7b88eb749
Clayel <videogamer0907@gmail.com>
2026-09-03 12:08:33 -0400

    extract to variable

--- Source/RP0/UI/ConfidenceWidget.cs
+++ Source/RP0/UI/ConfidenceWidget.cs
@@ -75,8 +75,10 @@ namespace RP0.UI
         }
         private static string GetTooltipTextConf()
         {
-            return Localizer.Format("#rp0_Widgets_Confidence_Tooltip", Confidence.AllConfidenceEarned.ToString("N0"),
-                CurrencyUtils.Conf(TransactionReasonsRP0.ScienceTransmission, Programs.ProgramHandler.Settings != null ? Programs.ProgramHandler.Settings.scienceToConfidence.Evaluate(System.Math.Max(0d, SpaceCenterManagement.Instance.SciPointsTotal)) : 2d).ToString("N2"));
+            double scienceToConfidence = CurrencyUtils.Conf(TransactionReasonsRP0.ScienceTransmission, Programs.ProgramHandler.Settings != null ? 
+                Programs.ProgramHandler.Settings.scienceToConfidence.Evaluate(System.Math.Max(0d, SpaceCenterManagement.Instance.SciPointsTotal)) : 2d);
+
+            return Localizer.Format("#rp0_Widgets_Confidence_Tooltip", Confidence.AllConfidenceEarned.ToString("N0"), scienceToConfidence.ToString("N2"));
         }
     }
 }
